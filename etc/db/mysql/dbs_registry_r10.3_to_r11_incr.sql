7a7755355b9cdc00478be05bef9c6baa77445747
Ben Greenwood <ben@ldcdc.net>
2013-11-18 11:14:15 +1100

    Remove database names and add tag tables

--- etc/db/mysql/dbs_registry_r10.3_to_r11_incr.sql
+++ etc/db/mysql/dbs_registry_r10.3_to_r11_incr.sql
@@ -1,8 +1,8 @@
-DROP TABLE dbs_registry.vocab_metadata;
-DROP TABLE dbs_registry.harvest_parameter;
+DROP TABLE vocab_metadata;
+DROP TABLE harvest_parameter;
 
 
-CREATE  TABLE `dbs_registry`.`registry_object_identifier_relationships` (
+CREATE  TABLE `registry_object_identifier_relationships` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
   `registry_object_id` MEDIUMINT UNSIGNED NOT NULL ,
   `related_object_key` VARCHAR(255) NULL ,
@@ -12,10 +12,17 @@ CREATE  TABLE `dbs_registry`.`registry_object_identifier_relationships` (
   `relation_type` VARCHAR(45) NULL ,
   PRIMARY KEY (`id`) );
 
-CREATE  TABLE `dbs_registry`.`registry_object_identifiers` (
+CREATE  TABLE `registry_object_identifiers` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
   `registry_object_id` MEDIUMINT UNSIGNED NOT NULL ,
   `identifier` VARCHAR(255) NOT NULL ,
   `identifier_type`  VARCHAR(45) NOT NULL ,
   PRIMARY KEY (`id`) );
+
+CREATE TABLE `registry_object_tags` (
+  `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
+  `key` varchar(255) NOT NULL,
+  `tag` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=14855 DEFAULT CHARSET=utf8
 
