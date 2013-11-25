4e73242161b011ce8179a41194abd39174462c98
Minh Duc Nguyen <dekarvn@gmail.com>
2013-11-25 11:55:21 +1100

    hide invisible page on theme page listing

--- applications/portal/theme_page/views/theme_page_listing.php
+++ applications/portal/theme_page/views/theme_page_listing.php
@@ -9,11 +9,13 @@
 		<div class="post clear">
 			<?php if(sizeof($index['items']) > 0): ?>
 			<?php foreach($index['items'] as $page): ?>
+			<?php if($page['visible']): ?>
 			<div class="theme-page-item">
 				<?php $img_src = ($page['img_src']?$page['img_src']:'http://placehold.it/350x150&text=No+Cover+Image'); ?>
 				<a href="<?php echo portal_url('theme/'.$page['slug']);?>"><img class="cover"src="<?php echo $img_src;?>" alt=""></a>
 				<h4 class="theme-page-title"><?php echo anchor('theme_page/view/'.$page['slug'], $page['title']); ?></h4>
 			</div>
+			<?php endif; ?>
 			<?php endforeach; ?>
 			<?php else: ?>
 			<b>There are no theme pages set up</b>
