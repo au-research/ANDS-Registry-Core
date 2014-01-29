74c929f31c3ff4397105d33bd25ed6eccd26c777
Minh Duc Nguyen <dekarvn@gmail.com>
2014-01-29 14:38:41 +1100

    fix nla pullback on nonobject

--- applications/registry/maintenance/controllers/nlaPullback.php
+++ applications/registry/maintenance/controllers/nlaPullback.php
@@ -140,7 +140,7 @@ class nlaPullback extends MX_Controller
 					{
 						// Get the matching element
 						$data = array_pop($data);
-						if ($data->registryObjects && !empty($data->registryObjects))
+						if (is_object($data) && $data->registryObjects && !empty($data->registryObjects))
 						{
 							return $data->registryObjects->asXML();
 						}
