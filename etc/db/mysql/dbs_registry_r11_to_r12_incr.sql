6542699de73938e0a4c8dc96ed9c73b20339400f
Minh Duc Nguyen <dekarvn@gmail.com>
2014-02-11 09:34:48 +1100

    change field id for logs for orcid

--- etc/db/mysql/dbs_registry_r11_to_r12_incr.sql
+++ etc/db/mysql/dbs_registry_r11_to_r12_incr.sql
@@ -11,3 +11,7 @@ ADD COLUMN `date_created` DATETIME NULL  AFTER `tag` ,
 ADD COLUMN `type` VARCHAR(45) NOT NULL DEFAULT 'public'  AFTER `date_created` , 
 ADD COLUMN `user` VARCHAR(256) NOT NULL  AFTER `type` , 
 ADD COLUMN `user_from` VARCHAR(45) NOT NULL  AFTER `user` ;
+
+ALTER TABLE `dbs_registry`.`logs` CHANGE COLUMN `id` `type_id` VARCHAR(128) NULL DEFAULT NULL  ;
+ALTER TABLE `dbs_registry`.`logs` ADD COLUMN `id` BIGINT NOT NULL AUTO_INCREMENT  AFTER `date_modified` , ADD PRIMARY KEY (`id`) ;
+
