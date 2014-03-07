22a5c21587504ea3e8357dbb8dd7d8724366b11c
Minh Duc Nguyen <dekarvn@gmail.com>
2014-03-07 12:00:38 +1100

    dept logo for RDA

--- applications/portal/core/views/rda_footer.php
+++ applications/portal/core/views/rda_footer.php
@@ -47,7 +47,7 @@
 				<a href="<?=base_url('home/about');?>">Read more about us...</a>
 			</p>
 			<p class="small">ANDS is supported by the Australian Government through the National Collaborative Research Infrastructure Strategy Program and the Education Investment Fund (EIF) Super Science Initiative.</p>
-			<a href="http://www.innovation.gov.au/" class="gov_logo"><img src="<?php echo asset_url('images/dept_ind_stacked.png', 'core');?>" alt="" /></a>
+			<a href="http://www.innovation.gov.au/" class="gov_logo"><img src="<?php echo asset_url('images/dept_white.png', 'core');?>" alt="" /></a>
 			<a href="http://www.ands.org.au/" class="footer_logo"><img src="<?php echo asset_url('images/footer_logo.jpg', 'core');?>" alt="" /></a>			
 		</div><!-- foot -->		
 	</div><!-- footer -->	
