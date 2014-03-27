8c0a996c079c98b7568f89283a416e1775f74925
Minh Duc Nguyen <dekarvn@gmail.com>
2014-03-27 11:25:20 +1100

    Fix - Fix for multi layered URI

--- system/core/URI.php
+++ system/core/URI.php
@@ -184,21 +184,20 @@ class CI_URI {
 
 		$uri = $_SERVER['REQUEST_URI'];
 
-		if (strpos($uri, $_SERVER['SCRIPT_NAME']) === 0)
-		{
+		if (strpos($uri, dirname($_SERVER['SCRIPT_NAME'])) === 0) {
+			$uri = substr($uri, strlen(dirname($_SERVER['SCRIPT_NAME'])));
+		} elseif (strpos($uri, $_SERVER['SCRIPT_NAME']) === 0) {
 			$uri = substr($uri, strlen($_SERVER['SCRIPT_NAME']));
 		}
-		elseif (strpos($uri, dirname($_SERVER['SCRIPT_NAME'])) === 0)
-		{
-			$uri = substr($uri, strlen(dirname($_SERVER['SCRIPT_NAME'])));
-		}
+
 
 		// This section ensures that even on servers that require the URI to be in the query string (Nginx) a correct
 		// URI is found, and also fixes the QUERY_STRING server var and $_GET array.
-		if (strncmp($uri, '?/', 2) === 0)
+		if (strncmp($uri, '?/', 3) === 0)
 		{
 			$uri = substr($uri, 2);
 		}
+
 		$parts = preg_split('#\?#i', $uri, 2);
 		$uri = $parts[0];
 		if (isset($parts[1]))
@@ -217,7 +216,10 @@ class CI_URI {
 			return '/';
 		}
 
+
+
 		$uri = parse_url($uri, PHP_URL_PATH);
+
 
 		// Do some final cleaning of the URI and return it
 		return str_replace(array('//', '../'), '/', trim($uri, '/'));
