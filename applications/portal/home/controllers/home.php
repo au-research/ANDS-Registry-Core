78998f73fd868f860b0bacda44fde44d975e2227
Minh Duc Nguyen <dekarvn@gmail.com>
2014-04-07 10:56:38 +1000

    Sitemap must not include data source if it has no records

--- applications/portal/home/controllers/home.php
+++ applications/portal/home/controllers/home.php
@@ -208,7 +208,7 @@ class Home extends MX_Controller {
 
 			for($i=0;$i<sizeof($dsfacet);$i+=2){
 				echo '<sitemap>';
-				echo '<loc>'.base_url().'home/sitemap/?ds='.$dsfacet[$i].'</loc>';
+				echo '<loc>'.base_url().'home/sitemap/?ds='.urlencode($dsfacet[$i]).'</loc>';
 				echo '<lastmod>'.date('Y-m-d').'</lastmod>';
 				echo '</sitemap>';
 			}
