9c85fdcace5eab6e55a45a13bf870f7ba17ca745
Minh Duc Nguyen <dekarvn@gmail.com>
2014-07-16 16:28:14 +1000

    hotfix

--- assets/lib/angularmod/portal-filters.js
+++ assets/lib/angularmod/portal-filters.js
@@ -8,15 +8,17 @@
 angular.module('portal-filters', []).
 	filter('truncate', function () {
 		return function (text, length, end) {
-			if (isNaN(length))
-				length = 10;
-			if (end === undefined)
-				end = "...";
-			if (text.length <= length || text.length - end.length <= length) {
-				return text;
-			}
-			else {
-				return String(text).substring(0, length-end.length) + end;
+			if(text){
+				if (isNaN(length))
+					length = 10;
+				if (end === undefined)
+					end = "...";
+				if (text.length <= length || text.length - end.length <= length) {
+					return text;
+				}
+				else {
+					return String(text).substring(0, length-end.length) + end;
+				}
 			}
 		};
 	}).
