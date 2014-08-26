a8651b0b8dda494e4c68fd4d80408376105dadfa
Minh Duc Nguyen <dekarvn@gmail.com>
2014-08-26 16:19:31 +1000

    text change for falling water

--- applications/portal/core/views/falling_water.php
+++ applications/portal/core/views/falling_water.php
@@ -3,7 +3,7 @@
 	<img src="<?php echo asset_url('img/ands-full.png','base')?>" alt="" style="float:right;margin:0 20px;">
   <h4> Help us to improve Research Data Australia. </h4>
   <p>
-  	We are currently considering a redesign of the Research Data Australia website. We are seeking your input to help us improve our website and provide the information you want. If you are willing to provide us with some of your time to help us understand how you believe content on this website should be organised, then please contact the ANDS Service Desk	<a href="mailto:services@ands.org.au">services@ands.org.au</a> with your contact details and availability from 1 September.
+  	We are currently considering a redesign of the Research Data Australia website. We are seeking your input to help us improve our website and provide the information you want. If you are willing to provide us with some of your time in September to help us understand how you believe content on this website should be organised, then please register your details below.
   </p>
   <hr>
   <form action="" id="fwform">
