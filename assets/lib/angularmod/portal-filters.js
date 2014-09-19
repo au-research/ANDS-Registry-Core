e1f49017957616ed82ef23dc0171ef3d496f6711
Minh Duc Nguyen <dekarvn@gmail.com>
2014-09-19 12:46:44 +1000

    cc980 roles minor changes

--- assets/lib/angularmod/portal-filters.js
+++ assets/lib/angularmod/portal-filters.js
@@ -236,5 +236,22 @@ angular.module('portal-filters', []).
 	        }
 	        return (time <= local && !isNaN(time)) ? span + ' ago' : 'in ' + span;
 	    }
-	})
+	}).
+	filter('orderObjectBy', function(){
+	 return function(input, attribute) {
+	    if (!angular.isObject(input)) return input;
+
+	    var array = [];
+	    for(var objectKey in input) {
+	        array.push(input[objectKey]);
+	    }
+
+	    array.sort(function(a, b){
+	        var alc = a[attribute].toLowerCase(),
+	        	blc = b[attribute].toLowerCase();
+	        return alc > blc ? 1 : alc < blc ? -1 : 0;
+	    });
+	    return array;
+	 }
+	});
 ;
