51369bcc76ec8bf27e106b688ee3ed458b27e10d
Minh Duc Nguyen <dekarvn@gmail.com>
2014-12-02 10:54:16 +1100

    search sidebar

--- applications/portal2/templates/omega/registry_object/search.blade.php
+++ applications/portal2/templates/omega/registry_object/search.blade.php
@@ -5,5 +5,30 @@
 @stop
 
 @section('sidebar')
-	stuff
+
+<div class="sidebar-widget widget_search">
+	<h3 class="sidebar-header">Refine search result</h3>
+	<form action="">
+		<div class="input-group">
+            <input type="text" value="" name="s" class="form-control" placeholder="Add more keywords">
+            <span class="input-group-btn">
+	            <button class="btn" type="submit" value="Search">
+	                <i class="fa fa-search"></i> Go
+	            </button>
+	        </span>
+        </div>
+	</form>
+</div>
+
+<div class="sidebar-widget widget_search">
+	<h3 class="sidebar-header">By Subject</h3>
+	<ul class="list-unstyled">
+		<li><input type="checkbox"> <a href="">Lorem ipsum dolor sit.</a></li>
+		<li><input type="checkbox"> <a href="">Lorem ipsum dolor sit.</a></li>
+		<li><input type="checkbox"> <a href="">Lorem ipsum dolor sit.</a></li>
+		<li><input type="checkbox"> <a href="">Lorem ipsum dolor sit.</a></li>
+		<li><a href="">View More...</a></li>
+	</ul>
+</div>
+
 @stop
