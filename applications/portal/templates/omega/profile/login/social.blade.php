362fdb724963b7697dbeec504dc5c22a111119dc
Minh Duc Nguyen <dekarvn@gmail.com>
2015-01-12 09:54:33 +1100

    added button for display on social login

--- applications/portal/templates/omega/profile/login/social.blade.php
+++ applications/portal/templates/omega/profile/login/social.blade.php
@@ -6,8 +6,14 @@
         @if($oauth_conf['providers']['Facebook']['enabled'])
             <a href="{{registry_url('auth/authenticate/facebook/?redirect=profile')}}" class="btn btn-primary btn-block btn-icon-left">Login with Facebook <span><i class="fa fa-facebook"></i></span></a>
         @endif
-        <p><?php if($oauth_conf['providers']['Twitter']['enabled']) echo anchor('auth/login/Twitter/?redirect='.current_url(),'Login With Twitter', array('class'=>'zocial twitter')); ?></p>
-        <p><?php if($oauth_conf['providers']['Google']['enabled']) echo anchor('auth/login/Google/?redirect='.current_url(),'Login With Google', array('class'=>'zocial google')); ?></p>
-        <p><?php if($oauth_conf['providers']['LinkedIn']['enabled']) echo anchor('auth/login/LinkedIn/?redirect='.current_url(),'Login With LinkedIn', array('class'=>'zocial linkedin')); ?></p>
+        @if($oauth_conf['providers']['Twitter']['enabled'])
+            <a href="{{registry_url('auth/authenticate/twitter/?redirect=profile')}}" class="btn btn-primary btn-block btn-icon-left">Login with Twitter <span><i class="fa fa-twitter"></i></span></a>
+        @endif
+        @if($oauth_conf['providers']['Google']['enabled'])
+            <a href="{{registry_url('auth/authenticate/google/?redirect=profile')}}" class="btn btn-primary btn-block btn-icon-left">Login with Google <span><i class="fa fa-google"></i></span></a>
+        @endif
+        @if($oauth_conf['providers']['LinkedIn']['enabled'])
+            <a href="{{registry_url('auth/authenticate/linkedin/?redirect=profile')}}" class="btn btn-primary btn-block btn-icon-left">Login with LinkedIn <span><i class="fa fa-linkedin"></i></span></a>
+        @endif
     </div>
 </div>
