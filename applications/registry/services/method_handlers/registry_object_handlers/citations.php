3fa936499fd6f4f2626380a67f3749f84a308d1a
Elizabeth Woods <u3485947@devl.ands.org.au>
2015-01-19 13:14:10 +1100

    abstracted out the citations handler into its own file

---
+++ applications/registry/services/method_handlers/registry_object_handlers/citations.php
@@ -0,0 +1,82 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+require_once(SERVICES_MODULE_PATH . 'method_handlers/registry_object_handlers/_ro_handler.php');
+
+/**
+ * Citations handler
+ * @author Liz Woods <liz.woods@ands.org.au>
+ * @param  string type
+ * @return array
+ */
+class Citations extends ROHandler {
+	function handle() {
+        $result = array();
+        if ($this->xml) {
+            $endNote = 'Provider: Australian National Data Service
+Database: Research Data Australia
+Content:text/plain; charset="utf-8"
+
+
+TY  - DATA';
+            foreach($this->xml->{$this->ro->class}->citationInfo as $citation){
+                foreach($citation->citationMetadata as $citationMetadata){
+                    $contributors = Array();
+                    foreach($citationMetadata->contributor as $contributor)
+                    {
+                        $nameParts = Array();
+                        foreach($contributor->namePart as $namePart)
+                        {
+                            $nameParts[] = array(
+                                'namePart_type' => (string)$namePart['type'],
+                                'name' => (string)$namePart
+                            );
+                        }
+                        $contributors[] =array(
+                            'name' => $nameParts,
+                            'seq' => (string)$contributor['seq'],
+                        );
+                    }
+                    usort($contributors,"seq");
+                    $displayNames ='';
+                    $contributorCount = 0;
+                    foreach($contributors as $contributor){
+                        $contributorCount++;
+                        $displayNames .= formatName($contributor['name']);
+                        if($contributorCount < count($contributors)) $displayNames .= "; ";
+                    }
+                    $identifierResolved = identifierResolution((string)$citationMetadata->identifier, (string)$citationMetadata->identifier['type']);
+
+                    $result[] = array(
+                        'type'=> 'metadata',
+                        'identifier' => (string)$citationMetadata->identifier,
+                        'identifier_type' => strtoupper((string)$citationMetadata->identifier['type']),
+                        'identifierResolved' => $identifierResolved,
+                        'version' => (string)$citationMetadata->version,
+                        'publisher' => (string)$citationMetadata->publisher,
+                        'url' => (string)$citationMetadata->url,
+                        'context' => (string)$citationMetadata->context,
+                        'placePublished' => (string)$citationMetadata->placePublished,
+                        'title' => (string)$citationMetadata->title,
+                        'date_type' => (string)$citationMetadata->date['type'],
+                        'date' => date("Y",strtotime((string)$citationMetadata->date)),
+                        'contributors' => $displayNames,
+                        'endNote' => $endNote
+                    );
+
+                }
+                foreach($citation->fullCitation as $fullCitation){
+                    $result[] = array(
+                        'type'=> 'fullCitation',
+                        'value' => (string)$fullCitation,
+                        'citation_type' => (string)$fullCitation['style'],
+                        'endNote' => $endNote
+                    );
+
+                }
+            }
+
+        }
+        return $result;
+	}
+}
+
+
--- applications/registry/services/method_handlers/registry_objects.php
+++ applications/registry/services/method_handlers/registry_objects.php
@@ -43,7 +43,6 @@ class Registry_objectsMethod extends MethodHandler {
                         case 'get':
                         case 'registry':
                         case 'relationships' :  $result[$m1] = $this->relationships_handler(); break;
-                        case 'citations' :      $result[$m1] = $this->citations_handler(); break;
                         case 'connectiontree' : $result[$m1] = $this->connectiontree_handler($id); break;
 
                         default :  $result[$m1] = $this->ro_handle($m1);  break;
@@ -156,83 +155,6 @@ class Registry_objectsMethod extends MethodHandler {
 
         //get results
         $result = $ci->solr->executeSearch(true);
-        return $result;
-    }
-
-
-
-    /**
-    * Citations handler
-    * @author Liz Woods <liz.woods@ands.org.au>
-    * @return array
-    */
-    private function citations_handler() {
-        $result = array();
-        if ($this->xml) {
-            $endNote = 'Provider: Australian National Data Service
-Database: Research Data Australia
-Content:text/plain; charset="utf-8"
-
-
-TY  - DATA';
-            foreach($this->xml->{$this->ro->class}->citationInfo as $citation){
-                 foreach($citation->citationMetadata as $citationMetadata){
-                     $contributors = Array();
-                     foreach($citationMetadata->contributor as $contributor)
-                     {
-                        $nameParts = Array();
-                        foreach($contributor->namePart as $namePart)
-                         {
-                                 $nameParts[] = array(
-                                 'namePart_type' => (string)$namePart['type'],
-                                 'name' => (string)$namePart
-                             );
-                         }
-                         $contributors[] =array(
-                             'name' => $nameParts,
-                             'seq' => (string)$contributor['seq'],
-                         );
-                     }
-                     usort($contributors,"seq");
-                     $displayNames ='';
-                     $contributorCount = 0;
-                     foreach($contributors as $contributor){
-                        $contributorCount++;
-                        $displayNames .= formatName($contributor['name']);
-                        if($contributorCount < count($contributors)) $displayNames .= "; ";
-                     }
-                     $identifierResolved = identifierResolution((string)$citationMetadata->identifier, (string)$citationMetadata->identifier['type']);
-
-                     $result[] = array(
-                         'type'=> 'metadata',
-                         'identifier' => (string)$citationMetadata->identifier,
-                         'identifier_type' => strtoupper((string)$citationMetadata->identifier['type']),
-                         'identifierResolved' => $identifierResolved,
-                         'version' => (string)$citationMetadata->version,
-                         'publisher' => (string)$citationMetadata->publisher,
-                         'url' => (string)$citationMetadata->url,
-                         'context' => (string)$citationMetadata->context,
-                         'placePublished' => (string)$citationMetadata->placePublished,
-                         'title' => (string)$citationMetadata->title,
-                         'date_type' => (string)$citationMetadata->date['type'],
-                         'date' => date("Y",strtotime((string)$citationMetadata->date)),
-                         'contributors' => $displayNames,
-                         'endNote' => $endNote
-                     );
-
-                 }
-                foreach($citation->fullCitation as $fullCitation){
-                    $result[] = array(
-                        'type'=> 'fullCitation',
-                        'value' => (string)$fullCitation,
-                        'citation_type' => (string)$fullCitation['style'],
-                        'endNote' => $endNote
-                    );
-
-                }
-            }
-
-        }
         return $result;
     }
 
