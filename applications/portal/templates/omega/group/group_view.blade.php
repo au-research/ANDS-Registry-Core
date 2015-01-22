575ff23a5fa17071df4e21d6700a5d8496cd562c
Minh Duc Nguyen <dekarvn@gmail.com>
2015-01-22 15:45:34 +1100

    contributor page

--- applications/portal/templates/omega/group/group_view.blade.php
+++ applications/portal/templates/omega/group/group_view.blade.php
@@ -1,55 +1,85 @@
 @extends('layouts/right-sidebar')
-@section('content')
-  <article class="post">
-    <header class="post-head">
-          <div class="post-title bordered">
-              <h1>{{$group['title']}}</h1>
-          </div>
-    </header>
-
-    <div class="post-body">
-      To date, {{$group['title']}} has {{$group['counts']}} collection records in Research Data Australia, which covers {{sizeof($group['facet']['subjects'])}} subjects areas including 
-      {{$group['facet']['subjects'][0]['name']}}, {{$group['facet']['subjects'][1]['name']}} and {{$group['facet']['subjects'][2]['name']}}, {{sizeof($group['groups'])}} research groups 
-      have been actively involved in collecting data and creating metadata records for the data.  All the Collections, Parties, Activities and Services associated with {{$group['title']}} 
-      can be accessed from the Registry Contents box on the right hand side of this page.
+@section('header')
+<section class="section swatch-white section-text-shadow section-innder-shadow element-short-top element-short-bottom">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <header>
+                    <h1 class="hairline bordered-normal">{{$group['title']}}</h1>
+                </header>
+            </div>
+        </div>
     </div>
-  </article>
+</section>
+@stop
+
+@section('content')
+    <?php
+        $subjects_list = '';
+        if (sizeof($group['facet']['subjects']) > 3) {
+            $subjects_list = 'including '.$group['facet']['subjects'][0]['name'].', '.$group['facet']['subjects'][1]['name'].' and '.$group['facet']['subjects'][2]['name'];
+        } elseif (sizeof($group['facet']['subjects']) > 0) {
+            $subjects_list = 'including ';
+            foreach ($group['facet']['subjects'] as $s) {
+                $subjects_list .= ', '. $s['name'] .'';
+            }
+        }
+    ?>
+
+    <div class="panel panel-primary swatch-white">
+        <div class="panel-body">
+            To date, {{$group['title']}} has {{$group['counts']}} collection records in Research Data Australia, which covers {{sizeof($group['facet']['subjects'])}} subjects areas {{$subjects_list}}, {{sizeof($group['groups'])}} research groups 
+            have been actively involved in collecting data and creating metadata records for the data.  All the Collections, Parties, Activities and Services associated with {{$group['title']}} 
+            can be accessed from the Registry Contents box on the right hand side of this page.
+        </div>
+    </div>
+
+    <div class="panel panel-primary panel-content swatch-white">
+        <div class="panel-heading">Subjects Covered</div>
+        <div class="panel-body widget_tag_cloud">
+            <div class="tag-cloud">
+                <ul>
+                    @foreach($group['facet']['subjects'] as $subject)
+                        <li><a href="">{{$subject['name']}} <span>({{$subject['num']}})</span></a></li>
+                    @endforeach
+                </ul>
+            </div>
+        </div>
+    </div>
 @stop
 
 @section('sidebar')
-<div class="sidebar-widget widget_archive">
-    <h3 class="sidebar-header">Registry Contents</h3>
-    <ul>
-      @foreach($group['facet']['class'] as $class)
-        <li class="clearfix"><a href="">{{$class['name']}} <small>{{$class['num']}}</small></a></li>
-      @endforeach
-    </ul>
+<div class="panel panel-primary panel-content swatch-white">
+    <div class="panel-heading">Registry Contents</div>
+    <div class="panel-body">
+        <ul>
+            @foreach($group['facet']['class'] as $class)
+                <li><a href="">{{readable($class['name'])}} <small>({{$class['num']}})</small></a></li>
+            @endforeach
+        </ul>
+    </div>
 </div>
 
-<div class="sidebar-widget widget_archive">
-    <h3 class="sidebar-header">Subjects Covered</h3>
-    <ul>
-      @foreach($group['facet']['subjects'] as $subject)
-        <li class="clearfix"><a href="">{{$subject['name']}} <small>{{$subject['num']}}</small></a></li>
-      @endforeach
-    </ul>
+<div class="panel panel-primary panel-content swatch-white">
+    <div class="panel-heading">Organisations & Groups</div>
+    <div class="panel-body">
+        <ul>
+            @foreach($group['groups'] as $gr)
+                <li><a href="">{{$gr['title']}}</a></li>
+            @endforeach
+        </ul>
+    </div>
 </div>
 
-<div class="sidebar-widget widget_archive">
-    <h3 class="sidebar-header">Organisations & Groups</h3>
-    <ul>
-      @foreach($group['groups'] as $gr)
-        <li class="clearfix"><a href="">{{$gr['title']}}</a></li>
-      @endforeach
-    </ul>
+<div class="panel panel-primary panel-content swatch-white">
+    <div class="panel-heading">Last 5 Collections Added</div>
+    <div class="panel-body">
+        <ul>
+            @foreach($group['latest_collections'] as $gr)
+                <li><a href="">{{$gr['title']}}</a></li>
+            @endforeach
+        </ul>
+    </div>
 </div>
 
-<div class="sidebar-widget widget_archive">
-    <h3 class="sidebar-header">Last 5 Collections Added</h3>
-    <ul>
-      @foreach($group['latest_collections'] as $gr)
-        <li class="clearfix"><a href="">{{$gr['title']}}</a></li>
-      @endforeach
-    </ul>
-</div>
 @stop
