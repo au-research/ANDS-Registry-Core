6474eabe4f3d7f214772e219b6c89ab9afe265fe
Elizabeth Woods <u3485947@devl.ands.org.au>
2015-02-03 12:20:39 +1100

    Finalised the export to Endnote string formation and created the CoIns string

--- applications/registry/services/method_handlers/registry_object_handlers/citations.php
+++ applications/registry/services/method_handlers/registry_object_handlers/citations.php
@@ -12,6 +12,7 @@ class Citations extends ROHandler {
         $result = array();
         if ($this->xml) {
             $endNote = $this->getEndnoteText();
+            $coins = $this->getCoinsSpan();
             foreach($this->xml->{$this->ro->class}->citationInfo as $citation){
                 foreach($citation->citationMetadata as $citationMetadata){
                     $contributors = Array();
@@ -54,7 +55,8 @@ class Citations extends ROHandler {
                         'date_type' => (string)$citationMetadata->date['type'],
                         'date' => date("Y",strtotime((string)$citationMetadata->date)),
                         'contributors' => $displayNames,
-                        'endNote' => $endNote
+                        'endNote' => $endNote,
+                        'coins' =>$coins
                     );
 
                 }
@@ -63,7 +65,8 @@ class Citations extends ROHandler {
                         'type'=> 'fullCitation',
                         'value' => (string)$fullCitation,
                         'citation_type' => (string)$fullCitation['style'],
-                        'endNote' => $endNote
+                        'endNote' => $endNote,
+                        'coins' => $coins
                     );
 
                 }
@@ -110,15 +113,14 @@ Y2  - '.date("Y-m-d")."
 ";
         }
         $funders = $this->getFunders();
-        if($funders!=''){
-            foreach($funders as $funder){
-                $endNote .= "A4  - ".$funder."
+        foreach($funders as $funder){
+            $endNote .= "A4  - ".$funder."
 ";
-            }
         }
+
         $endNote .= "TI  - ".$this->ro->title."
 ";
-        $sourceUrl = $this->getSourceUrl();
+        $sourceUrl = $this->getSourceUrl($output='endNote');
         if($sourceUrl!=''){
             $endNote .= "UR  - ".$sourceUrl."
 ";
@@ -143,11 +145,103 @@ Y2  - '.date("Y-m-d")."
 ";
         $rights = $this->ro->processLicence();
         foreach($rights as $right) {
-            if($right['value']!='') $endNote .="C1  - ".$right['value']."
+            if($right['value']!='') $endNote .="C5  - ".$right['value']."
 ";
         }
 
+        $keywords = $this->getKeywords();
+        foreach($keywords as $keyword) {
+            $endNote .="KW  - ".$keyword."
+";
+        }
+
+        $spatials = $this->getSpatial();
+        foreach($spatials as $spatial) {
+            $endNote .="RI  - ".$spatial."
+";
+        }
+
+        $notes = $this->getNotes();
+        foreach($notes as $note) {
+            $endNote .="N1  - ".$note."
+";
+        }
+        $dates = $this->getDates();
+        foreach($dates as $date) {
+            $endNote .="C1  - ".$date."
+";
+        }
+
+        $descriptions = $this->getDescriptions();
+        foreach($descriptions as $description) {
+            $endNote .="AB  - ".$description."
+";
+        }
+
+        $endNote .= "ER  - ";
+
         return $endNote;
+    }
+
+    private function getCoinsSpan()
+    {
+        $coins = '';
+        $rft_id =  $this->getSourceUrl($output='coins');
+        $rft_identifier = $this->getIdentifier();
+        $rft_publisher = $this->getPublisher();
+        $descriptions = $this->getDescriptions();
+        $rft_description = '';
+        foreach($descriptions as $description){
+            $rft_description .= $description;
+        }
+        $rft_creators = '';
+        $creators= $this->getContributors();
+        foreach($creators as $creator){
+            $rft_creators .= '&rft.creator='.$creator['name'];
+        }
+        $rft_date = $this->getPublicationdate();
+        $rights = $rights = $this->ro->processLicence();
+        $rft_rights = '';
+        foreach($rights as $right){
+            if($right['type'] == 'rightsStatement' || $right['type'] == 'licence'){
+                $rft_rights .= '&rft_rights='.$right['value']." ".$right['rightsUri'];
+            }
+        }
+        $subjects = $this->getKeywords();
+        $rft_subjects = '';
+        foreach($subjects as $subject){
+            $rft_subjects .= '&rft_subject='.$subject;
+        }
+        $rft_edition = $this->getVersion();
+        $coverages = $this->getSpatial();
+        $rft_coverages = '';
+        foreach($coverages as $coverage){
+            $rft_coverages .= '&rft.coverage='.$coverage;
+        }
+
+        $relations = $this->getRelation();
+        $rft_relations = '';
+        foreach($relations as $relation){
+            $rft_relations .= '&rft.relation='.$relation;
+        }
+        $rft_place = $this->getPlace();
+        $coins .=  '<span class="Z3988" title="ctx_ver=Z39.88-2004&rft_val_fmt=info%3Aofi%2Ffmt%3Akev%3Amtx%3Adc&rfr_id=info%3Asid%2FANDS';
+        if($rft_id) $coins .= '&rft_id='.$rft_id;
+        $coins .= '&rft.title='.$this->ro->title;
+        if($rft_identifier) $coins .= '&rft.identifier='.$rft_identifier;
+        if($rft_publisher) $coins .= '&rft.publisher='.$rft_publisher;
+        $coins .= '&rft.description='.$rft_description.$rft_creators;
+        if($rft_date) $coins .= '&rft.date='.$rft_date;
+        if($rft_edition) $coins .= '&rft.edition='.$rft_edition;
+        if($rft_relations) $coins .= $rft_relations;
+        if($rft_coverages) $coins .=  $rft_coverages;
+        if($rft_rights) $coins .= $rft_rights;
+        if($rft_subjects) $coins .= $rft_subjects;
+        if($rft_place) $coins .= '&rft_place='.$rft_place;
+        $coins .= '&rft.type=dataset&rft.language=English';
+        $coins .= '"><span class="Z3988"></span>';
+
+        return $coins;
     }
 
     private function getDoi(){
@@ -173,7 +267,26 @@ Y2  - '.date("Y-m-d")."
         return $doi;
 
     }
+    private function getIdentifier(){
 
+        $identifier = '';
+        $query = '';
+        if($this->gXPath->evaluate("count(//ro:citationInfo/ro:citationMetadata/ro:identifier)")>0) {
+            $query = "//ro:citationInfo/ro:citationMetadata/ro:identifier";
+        }
+        elseif($this->gXPath->evaluate("count(//ro:collection/ro:identifier/)")>0) {
+            $query = "//ro:collection/ro:identifier";
+        }
+
+        if($query!=''){
+            $identifiers = $this->gXPath->query($query);
+            foreach($identifiers as $identifiervalue) {
+                $identifier = $identifiervalue->nodeValue;
+            }
+                    }
+        return $identifier;
+
+    }
     private function getPublicationDate()
     {
         $publicationDate = '';
@@ -215,7 +328,7 @@ Y2  - '.date("Y-m-d")."
         return  $publicationDate;
     }
 
-    private function getSourceUrl()
+    private function getSourceUrl($output=null)
     {
         $sourceUrl = '';
         $query = '';
@@ -263,12 +376,18 @@ Y2  - '.date("Y-m-d")."
             $urls = $this->gXPath->query($query);
             foreach($urls as $url) {
                 $sourceUrl = $url->nodeValue;
-                $resolved = identifierResolution($sourceUrl,$type);
-                $sourceUrl = $resolved['href'];
+                if($output=='endNote'){
+                    $resolved = identifierResolution($sourceUrl,$type);
+                    $sourceUrl = $resolved['href'];
+                }elseif($output == 'coins'){
+                    if(strpos($sourceUrl,"doi.org/")) $sourceUrl ="info:doi".substr($sourceUrl,strpos($sourceUrl,"doi.org/")+8);
+                    elseif($type=='doi') $sourceUrl = "info:doi".$sourceUrl;
+                }
             }
         } else {
-
-            $sourceUrl = portal_url();
+            if($output=='endNote'){
+                $sourceUrl = portal_url();
+            }
         }
 
         return  $sourceUrl;
@@ -387,28 +506,119 @@ Y2  - '.date("Y-m-d")."
                 {
                     $grant_sxml = $grant_object->getSimpleXML(NULL, true);
 
-                    // Handle the researcher IDs (using the normalisation_helper.php)
-                    $grant_id = $grant_sxml->xpath("//ro:identifier[@type='arc'] | //ro:identifier[@type='nhmrc'] | //ro:identifier[@type='purl']");
-
-                    $related_party = $grant_object->getRelatedObjectsByClassAndRelationshipType(['party'] ,['isFunderOf','isFundedBy']);
-                    if (is_array($grant_id))
-                    {
-                        if (is_array($related_party) && isset($related_party[0]))
+                    if($grant_object == 'PUBLISHED'){
+                        $grant_id = $grant_sxml->xpath("//ro:identifier[@type='arc'] | //ro:identifier[@type='nhmrc'] | //ro:identifier[@type='purl']");
+                        $related_party = $grant_object->getRelatedObjectsByClassAndRelationshipType(['party'] ,['isFunderOf','isFundedBy']);
+                        if (is_array($grant_id))
                         {
-                            $funders[] = $related_party[0]['title'];
+                            if (is_array($related_party) && isset($related_party[0]))
+                            {
+                                $funders[] = $related_party[0]['title'];
+                            }
                         }
                     }
                 }
-
             }
-
         }
 
         return $funders;
-
     }
 
+    private function getKeywords(){
 
+        $keywords = Array();
+
+        if($this->index && isset($this->index['subject_value_resolved'])) {
+            foreach($this->index['subject_value_unresolved'] as $key=>$sub) {
+                $keywords[] = titleCase($this->index['subject_value_resolved'][$key]);
+            }
+        }
+
+        return $keywords;
+    }
+
+    private function getSpatial(){
+
+        $spatials = Array();
+        $ro_spatials = $this->gXPath->query("//ro:collection/ro:coverage/ro:spatial");
+
+        foreach($ro_spatials as $a_spatial) {
+            $spatials[] = $a_spatial->nodeValue;
+        }
+        return $spatials;
+    }
+
+    private function getNotes(){
+        $notes = Array();
+        $ro_notes = $this->gXPath->query("//ro:collection/ro:description[@type='note']");
+        foreach($ro_notes as $a_note) {
+            $notes[] = strip_tags(html_entity_decode($a_note->nodeValue));
+        }
+        return $notes;
+    }
+
+    private function getDates(){
+        $dates = Array();
+        foreach($this->xml->{$this->ro->class}->coverage->temporal->date as $date){
+            $type = '';
+            $type = (string)$date['type'];
+            if($type=='dateFrom') $type = 'From';
+            if($type=='dateTo') $type = 'To';
+            $dates[] = $type ." ".(string)($date);
+        }
+        return $dates;
+    }
+
+    private function getDescriptions(){
+        $descriptions = Array();
+        $ro_descriptions = $this->gXPath->query("//ro:collection/ro:description[@type='full']");
+        foreach($ro_descriptions as $a_description) {
+            $descriptions[] = strip_tags(html_entity_decode($a_description->nodeValue));
+        }
+        $ro_descriptions = $this->gXPath->query("//ro:collection/ro:description[@type='brief']");
+        foreach($ro_descriptions as $a_description) {
+            $descriptions[] = strip_tags(html_entity_decode($a_description->nodeValue));
+        }
+
+        $ro_descriptions = $this->gXPath->query("//ro:collection/ro:description[@type='significanceStatement']");
+        foreach($ro_descriptions as $a_description) {
+            $descriptions[] = strip_tags(html_entity_decode($a_description->nodeValue));
+        }
+
+        $ro_descriptions = $this->gXPath->query("//ro:collection/ro:description[@type='note']");
+        foreach($ro_descriptions as $a_description) {
+            $descriptions[] = strip_tags(html_entity_decode($a_description->nodeValue));
+        }
+
+        $ro_descriptions = $this->gXPath->query("//ro:collection/ro:description[@type='lineage']");
+        foreach($ro_descriptions as $a_description) {
+            $descriptions[] = strip_tags(html_entity_decode($a_description->nodeValue));
+        }
+
+        return $descriptions;
+    }
+
+    private function getPlace(){
+        $place = '';
+        $ro_place = $this->gXPath->query("//ro:citationInfo/ro:citationMetadata/ro:placePublished");
+        foreach($ro_place as $a_place) {
+            $place = $a_place->nodeValue;
+        }
+        return $place;
+    }
+
+    private function getRelation(){
+
+        $relations = Array();
+        foreach($this->xml->{$this->ro->class}->relatedInfo as $relation){
+
+          if($relation['type']=='publication'){
+              $relations[] =  (string) $relation->identifier;
+          }
+
+        }
+        return $relations;
+    }
 }
 
 
