76c519f1f5c05138f8e9fc447ede02f7e23ecb73
Richard Walker <u9002369@devl.ands.org.au>
2015-02-05 15:35:28 +1100

    Suggestors work in progress
    Fix suggest.php to cope with multiple suggestors.
    Improve subjects suggestor.
    New suggestors: related_object, shared_text.
    Copy/pasted for future work: temporal_coverage. NB: Not started!

---
+++ applications/registry/registry_object/models/suggestors/shared_text_suggestor.php
@@ -0,0 +1,55 @@
+<?php
+require_once(APP_PATH. 'registry_object/models/_GenericSuggestor.php');
+
+/**
+ * Class Shared text Suggestor
+ * @author Leo Monus <Leo.Monus@ands.org.au>
+ * @author Richard Walker <Richard.Walker@ands.org.au>
+ */
+class Shared_text_suggestor extends _GenericSuggestor {
+	
+    /**
+     * Suggest Records based on the title or description
+     * containing similar text, using the Solr's mlt search.
+     * Rely on Solr's score.
+     * @return array suggested_records
+     */
+    function suggest() {
+
+        //construct the query string
+        $str = 'id:'.$this->ro->id;
+		
+        //call SOLR library
+        $ci =& get_instance();
+        $ci->load->library('solr');
+        $ci->solr->init();
+        $ci->solr
+            ->init()
+            ->setOpt('q', $str)
+            ->setOpt('rows', '10')
+            ->setOpt('fl', 'id,key,slug,title,score')
+            ->setOpt('defType', 'edismax')
+            ->setOpt('mlt', 'true')
+            ->setOpt('mlt.fl', 'description,display_title')
+            ->setOpt('mlt.count', '50');
+        
+        $suggestions = array();
+
+        $result = $ci->solr->executeSearch(true);
+
+        if($result['moreLikeThis'][$this->ro->id]['numFound'] > 0) {
+            foreach($result['moreLikeThis'][$this->ro->id]['docs'] as $doc) {
+                if(!in_array_r($doc, $suggestions)){
+                    $suggestions[] = $doc;
+                }
+            }
+        }
+        
+        return $suggestions;
+    }
+
+    function __construct() {
+        parent::__construct();
+        set_exception_handler('json_exception_handler');
+    }
+}
