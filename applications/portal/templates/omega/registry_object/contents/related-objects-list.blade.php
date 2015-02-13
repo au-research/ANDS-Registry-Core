f1f49af0d539a46e362922b900e035e5860be056
Minh Duc Nguyen <dekarvn@gmail.com>
2015-02-13 12:57:35 +1100

    Portal View. fix bug where related publication is blank

--- applications/portal/templates/omega/registry_object/contents/related-objects-list.blade.php
+++ applications/portal/templates/omega/registry_object/contents/related-objects-list.blade.php
@@ -20,23 +20,31 @@
 		<div class="panel-body swatch-white">
 		
 			@if($ro->relatedInfo)
-			<h4>Related Publications</h4>
-			@foreach($ro->relatedInfo as $relatedInfo)
-				@if($relatedInfo['type']=='publication')
-	                <h5><a href="" class="ro_preview" identifier_doi="{{$relatedInfo['identifier']['identifier_value']}}"><img src="<?php echo base_url()?>assets/core/images/icons/publications.png" style="margin-top: -2px; height: 24px; width: 24px;"> {{$relatedInfo['title']}}</a></h5>
-	                <p>
-	                    <b>{{$relatedInfo['identifier']['identifier_type']}}</b> : 
-	                    @if($relatedInfo['identifier']['identifier_href'])
-	                        <a href="{{$relatedInfo['identifier']['identifier_href']['href']}}">{{$relatedInfo['identifier']['identifier_value']}}</a><br />
-	                    @else
-	                        {{$relatedInfo['identifier']['identifier_value']}}
-	                    @endif
-	                </p>
-	                @if($relatedInfo['relation']['url'])
-	                    <p>URI : <a href="{{$relatedInfo['relation']['url']}}">{{$relatedInfo['relation']['url']}}</a></p>
-	                @endif
-	            @endif
-            @endforeach
+			<?php
+				$hasRelatedPublication = false;
+				foreach($ro->relatedInfo as $relatedInfo) {
+					if ($relatedInfo['type']=='publication') $hasRelatedPublication = true;
+				}
+			?>
+				@if($hasRelatedPublication)
+					<h4>Related Publications</h4>
+					@foreach($ro->relatedInfo as $relatedInfo)
+						@if($relatedInfo['type']=='publication')
+			                <h5><a href="" class="ro_preview" identifier_doi="{{$relatedInfo['identifier']['identifier_value']}}"><img src="<?php echo base_url()?>assets/core/images/icons/publications.png" style="margin-top: -2px; height: 24px; width: 24px;"> {{$relatedInfo['title']}}</a></h5>
+			                <p>
+			                    <b>{{$relatedInfo['identifier']['identifier_type']}}</b> : 
+			                    @if($relatedInfo['identifier']['identifier_href'])
+			                        <a href="{{$relatedInfo['identifier']['identifier_href']['href']}}">{{$relatedInfo['identifier']['identifier_value']}}</a><br />
+			                    @else
+			                        {{$relatedInfo['identifier']['identifier_value']}}
+			                    @endif
+			                </p>
+			                @if($relatedInfo['relation']['url'])
+			                    <p>URI : <a href="{{$relatedInfo['relation']['url']}}">{{$relatedInfo['relation']['url']}}</a></p>
+			                @endif
+			            @endif
+		            @endforeach
+		        @endif
             @endif
 
 			<!-- Only display collections with the relationship of isDerivedFrom or hasDerivedCollection -->
