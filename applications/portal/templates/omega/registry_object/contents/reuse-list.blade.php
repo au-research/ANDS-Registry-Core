a05adf11ff1461d7dbf8a774baa0ef7c1568897e
Elizabeth Woods <u3485947@devl.ands.org.au>
2015-02-19 11:01:51 +1100

    fix to display of reuse info

--- applications/portal/templates/omega/registry_object/contents/reuse-list.blade.php
+++ applications/portal/templates/omega/registry_object/contents/reuse-list.blade.php
@@ -1,19 +1,26 @@
 @if($ro->relatedInfo)
-<?php $heading = '<div class="swatch-white">
+<?php
+$found = false;
+$heading = '<div class="swatch-white">
     <div class="panel panel-primary element-no-top element-short-bottom panel-content">
-        <div class="panel-heading">';
-$closure = '</div></div></div>';
-$found=false;
-?>
+        <div class="panel-heading">
+            <a href="">Reuse Information</a>
+        </div>
+        <div class="panel-body swatch-white">';
+$closure = '        </div>
+    </div>
+</div>'; ?>
 
 	@foreach($ro->relatedInfo as $relatedInfo)
+
         @if($relatedInfo['type']=='reuseInformation')
-        <?php if(!$found) {echo $heading; $found=true;} ?>
+
+        <?php if(!$found) {echo $heading; $found=true; }?>
         <p>
             {{$relatedInfo['title']}}<br />
             {{$relatedInfo['identifier']['identifier_type']}} :
             @if($relatedInfo['identifier']['identifier_href'])
-            <a href="{{$relatedInfo['identifier']['identifier_href']}}">{{$relatedInfo['identifier']['identifier_value']}}</a><br />
+            <a href="{{$relatedInfo['identifier']['identifier_href']['href']}}">{{$relatedInfo['identifier']['identifier_value']}}</a><br />
             @else
             {{$relatedInfo['identifier']['identifier_value']}}<br />
             @endif
