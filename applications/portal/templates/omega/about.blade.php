5259b5dfd6768ebf7816e85d29add62b025957f0
Elizabeth Woods <u3485947@devl.ands.org.au>
2015-03-03 13:43:45 +1100

    more formatting for the about page

--- applications/portal/templates/omega/about.blade.php
+++ applications/portal/templates/omega/about.blade.php
@@ -4,12 +4,16 @@
 	<section class="section swatch-white element-normal-bottom">
        <div class="container-fluid">
            <div class="row">
-               <div class="col-md-12 text-center not-condensed os-animation animated fadeInUp swatch-gray" style="-webkit-animation: 0s;padding-top:50px;padding-bottom:50px;">
+
+               <div class="col-md-12 text-center not-condensed os-animation animated fadeInUp" style="-webkit-animation: 0s;padding-top:50px;padding-bottom:50px;">
                        <h1 class="bigger hairline bordered os-animation animated fadeIn" data-os-animation="fadeIn" data-os-animation-delay="0s" style="-webkit-animation: 0s;">Find out more about us</h1>
                </div>
-               <div class="col-md-12 not-condensed os-animation animated fadeInUp" style="-webkit-animation: 0s;padding-top:30px;padding-bottom:50px">
+               <div class="col-md-12  swatch-gray">
+               <div class="col-md-2 not-condensed os-animation animated fadeInUp swatch-gray"> </div>
+               <div class="col-md-8 not-condensed os-animation animated fadeInUp swatch-gray" style="-webkit-animation: 0s;padding-top:30px;padding-bottom:50px">
                    <div class="text-center" style="-webkit-animation: 0s;">
-                        <p class="strong hairline bordered bordered-normal os-animation animated fadeIn" data-os-animation="fadeIn" data-os-animation-delay="0s" style="-webkit-animation: 0s;color:#000000"><strong>Why make data discoverable?</strong></p>
+                        <p class="strong hairline bordered bordered-normal os-animation animated fadeIn" data-os-animation="fadeIn" data-os-animation-delay="0s" style="-webkit-animation: 0s;color:#000000">
+                            <strong>Why make data discoverable?</strong></p>
                    </div>
                    <div style="padding-left:50px;padding-right:50px">
                        <p>Research is using and producing larger and more complex data than ever before.
@@ -20,7 +24,11 @@
                         </p>
                    </div>
                </div>
-               <div class="col-md-12 not-condensed os-animation animated fadeInUp  swatch-gray" style="-webkit-animation: 0s;padding-top:20px;padding-bottom:20px;text-decoration-color: #ffffff">
+               <div class="col-md-2 swatch-gray">  </div>
+               </div>
+               <div  class="col-md-12">
+                   <div class="col-md-2 not-condensed os-animation animated fadeInUp"> </div>
+               <div class="col-md-8 not-condensed os-animation animated fadeInUp " style="-webkit-animation: 0s;padding-top:20px;padding-bottom:20px;text-decoration-color: #ffffff">
 
                    <div class="text-left" style="-webkit-animation: 0s;padding-left:50px;padding-right:50px;">
                        <div style="float:right;padding-left:20px">
@@ -38,13 +46,15 @@
                         </p>
                    </div>
                </div>
-
-
-
-               <div class="col-md-8" style="float:left" >
+                   <div class="col-md-2 not-condensed os-animation animated fadeInUp"> </div>
+               </div>
+               <div class="col-md-12  swatch-gray">
+                   <div class="col-md-2 not-condensed os-animation animated fadeInUp swatch-gray"> </div>
+                <div class="col-md-8  swatch-gray">
+               <div class="col-md-8  swatch-gray" style="float:left" >
                        @include('includes/about-subjects-list')
                </div>
-               <div class="col-md-4" style="float:right;padding-left:20px;padding-top:100px" >
+               <div class="col-md-4  swatch-gray" style="float:right;padding-left:20px;padding-top:100px" >
                    <p class="strong hairline bordered bordered-normal os-animation animated fadeIn" data-os-animation="fadeIn" data-os-animation-delay="0s" style="-webkit-animation: 0s;color:#000000"><strong>What will I find here?</strong></p>
                    <p>Research Data Australia can be used by everyone from policy-makers to teachers,but it is targeted to researchers
                        as users and creators of research data. <br />You can search here for data descriptions that are contributed by Australian
@@ -52,24 +62,39 @@
                        interest to researchers - from the sciences to economics to humanities. Much of the data you can discover here is
                        immediately accessible online via our partners and free to use (subject to any licence conditions</p>
                </div>
-               <div class="col-md-12 not-condensed os-animation animated fadeInUp swatch-gray" style="-webkit-animation: 0s;padding-top:50px;padding-bottom:50px">
-                   <div class="text-left" style="-webkit-animation: 0s;padding-left:50px;padding-right:50px;">
-    <ul>
-                       <p class="strong hairline bordered os-animation animated fadeIn" data-os-animation="fadeIn" data-os-animation-delay="0s" style="-webkit-animation: 0s;color:#000000"><strong>Why use Research data Australia?</strong></p>
-    </ul>
+                </div>
+                   </div>
+               </div>
 
+               <div class="col-md-12">
+                   <div class="col-md-2"></div>
+                       <div class="col-md-8 not-condensed os-animation animated fadeInUp" style="-webkit-animation: 0s;padding-top:50px;padding-bottom:50px">
+                   <div class="text-left" style="-webkit-animation: 0s;padding-left:50px;padding-right:50px;">
+                    <ul>
+                       <p class="strong hairline bordered os-animation animated fadeIn" data-os-animation="fadeIn" data-os-animation-delay="0s" style="-webkit-animation: 0s;color:#000000"><strong>Why use Research data Australia?</strong></p>
+                    </ul>
                        <p>
-                       <ul  class="fa-ul" >
-                           <li ><i class="fa-li fa  fa-lightbulb-o" ></i>
+
+
+
+                       <ul  class="fa-ul features-list element-no-top element-no-bottom" >
+                           <li class="element-no-top element-short-bottom os-animation animated fadeInLeft" ><div class="features-list-icon box-animate" style="background-color:#e9e9e9;">
+
+                              <i class=" fa fa-lightbulb-o fa-4x" ></i> </div>
                                Research Data Australia brings together a broad range of data resources across almost all of the publicly-funded research and government organisations across Australia.
+
                            </li>
-                           <li ><i class="fa-li fa  fa-lightbulb-o"></i>
+                           <li class="element-no-top element-short-bottom os-animation animated fadeInLeft" ><div class="features-list-icon box-animate" style="background-color:#e9e9e9;">
+
+                                   <i class=" fa fa-lightbulb-o fa-4x" ></i> </div>
                               You can perform a single search in Research Data Australia to retrieve resources across a range of subjects and providers.
                                For example a search for the town of "Wagga Wagga" will return results for data from a number of fields of research:
                                Earth Sciences, Agriculture and Veterinary Sciences, Environmental Sciences, Built Environment and Design, Biological Sciences,
                                Technology, Studies in Human Society, Studies in Creative Arts, History and Archaeology, and Philosophy and Religious Studies.
                            </li>
-                           <li ><i class="fa-li fa  fa-lightbulb-o" ></i>
+                           <li class="element-no-top element-short-bottom os-animation animated fadeInLeft" ><div class="features-list-icon box-animate" style="background-color:#e9e9e9;">
+
+                                   <i class=" fa fa-lightbulb-o fa-4x" ></i> </div>
                                Unlike using a generic search engine or discipline portal, searching in Research Data Australia provides an increased likelihood of related
                                discovery, as well as the ability to conduct cross-disciplinary research and address broader challenges over a larger pool of resources.
                            </li>
@@ -77,9 +102,13 @@
 
                        </p>
                    </div>
-               </div>
+                           </div></div>
 
-               <div class="col-md-12 not-condensed os-animation animated fadeInUp" style="-webkit-animation: 0s;padding-top:30px;padding-bottom:50px">
+
+        </div>
+           <div class="col-md-12  swatch-gray">
+               <div class="col-md-2 not-condensed os-animation animated fadeInUp swatch-gray"> </div>
+               <div class="col-md-8 not-condensed os-animation animated fadeInUp swatch-gray" style="-webkit-animation: 0s;padding-top:30px;padding-bottom:50px">
                    <div class="text-center" style="-webkit-animation: 0s;">
                        <p class="strong hairline bordered bordered-normal os-animation animated fadeIn" data-os-animation="fadeIn" data-os-animation-delay="0s" style="-webkit-animation: 0s;color:#000000"><strong>Get Started</strong></p>
                    </div>
@@ -139,10 +168,10 @@
                        </div>
                    </div>
                </div>
+           </div>
+       </div>
+           </div>
 
-           </div>
-           <div class="row ">
-           </div>
        </div>
     </section>
 </article>
--- applications/portal/templates/omega/includes/about-subjects-list.blade.php
+++ applications/portal/templates/omega/includes/about-subjects-list.blade.php
@@ -1,11 +1,11 @@
 <div class="portfolio-container element-medium-top element-medium-bottom">
-    <div class="portfolio masonry isotope" data-padding="10" data-col-xs="1" data-col-sm="1" data-col-md="3" data-col-lg="3" data-layout="fitRows">
+    <div class="portfolio masonry isotope" data-padding="10" data-col-xs="1" data-col-sm="1" data-col-md="4" data-col-lg="4" data-layout="fitRows">
         @foreach($highlevel as $item)
         <div class="masonry-item portfolio-item isotope-item" data-menu-order="1" data-title="{{$item['display']}}">
             <div class="figure portfolio-os-animation element-no-top element-no-bottom text-center figcaption-middle normalwidth image-filter-sepia fade-in image-filter-onhover animated fadeIn" data-os-animation="fadeIn" data-os-animation-delay="0s">
                 <div class="element-no-top element-no-bottom text-center figcaption-middle normalwidth image-filter-sepia image-filter-onhover">
                    <a href="{{base_url('search').'#!'.$item['query']}}" class="figure-image magnific-vimeo" data-links="" target="_self">
-                       <img src="{{asset_url('images/subjects/'.$item['img'], 'core')}}" alt="" class="normalwidth" style="height:190px">
+                       <img src="{{asset_url('images/subjects/'.$item['img'], 'core')}}" alt="" class="normalwidth" style="height:120px">
                     </a>
                 </div>
                 <div class="figure-caption text-center">
