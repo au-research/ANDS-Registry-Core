51a97e4ba420507691a4c6ba24cc62b1f099ccbf
Elizabeth Woods <u3485947@devl.ands.org.au>
2015-03-06 10:17:37 +1100

    removed display of non existant href index

--- applications/portal/templates/omega/registry_object/contents/related-objects-list.blade.php
+++ applications/portal/templates/omega/registry_object/contents/related-objects-list.blade.php
@@ -73,7 +73,7 @@
                                     <h5><a href="" class="ro_preview" identifier_doi="{{$relatedInfo['identifier']['identifier_value']}}"><i class="fa fa-book icon-portal"></i> {{$relatedInfo['title']}}</a></h5>
                                     <p>
                                         <b>{{$relatedInfo['identifier']['identifier_type']}}</b> :
-                                        @if($relatedInfo['identifier']['identifier_href'])
+                                        @if($relatedInfo['identifier']['identifier_href']['href'])
                                             <a href="{{$relatedInfo['identifier']['identifier_href']['href']}}">{{$relatedInfo['identifier']['identifier_value']}}</a><br />
                                         @else
                                             {{$relatedInfo['identifier']['identifier_value']}}
