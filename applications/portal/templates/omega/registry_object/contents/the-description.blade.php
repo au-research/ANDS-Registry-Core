300c9fa389ea2c1ae230ab5dcc9d3f2d3f8a5a22
Leo Monus <leo.monus@anu.edu.au>
2015-03-06 15:46:24 +1100

    related preview popup no html and limited to 500 chars

--- applications/portal/templates/omega/registry_object/contents/the-description.blade.php
+++ applications/portal/templates/omega/registry_object/contents/the-description.blade.php
@@ -15,7 +15,11 @@
 			}
 		}
 		if(!$the_description) $the_description = $ro->descriptions[0]['description'];
+        $the_description = trim(strip_tags(html_entity_decode(html_entity_decode($the_description))));
+        if (strlen($the_description) > 500)
+            $the_description = substr($the_description, 0, 500) . '...';
 	}
+
 ?>
 @if($the_description)
 	{{$the_description}}
