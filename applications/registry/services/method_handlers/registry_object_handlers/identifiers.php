3757444cac1b6972de106ae3a9c249578d3b95a0
Leo Monus <leo.monus@anu.edu.au>
2015-03-12 13:34:32 +1100

    drafts need identifiers

--- applications/registry/services/method_handlers/registry_object_handlers/identifiers.php
+++ applications/registry/services/method_handlers/registry_object_handlers/identifiers.php
@@ -6,18 +6,34 @@ require_once(SERVICES_MODULE_PATH . 'method_handlers/registry_object_handlers/_r
  * @return array list of identifier from SOLR index
  */
 class Identifiers extends ROHandler {
-	function handle() {
-		$result = array();
-        if($this->index && isset($this->index['identifier_type'])) {
-            //identifier_type, identifier_value
-            foreach($this->index['identifier_type'] as $key=>$type) {
+    function handle() {
+        $result = array();
+        if($this->ro->status == 'PUBLISHED')
+        {
+            if($this->index && isset($this->index['identifier_type'])) {
+                //identifier_type, identifier_value
+                foreach($this->index['identifier_type'] as $key=>$type) {
+                    $result[] = array(
+                        'type' => $type,
+                        'value' => $this->index['identifier_value'][$key],
+                        'identifier' => identifierResolution($this->index['identifier_value'][$key],$type)
+                    );
+                }
+            }
+        }
+        else{
+            $identifiers = $this->ro->getIdentifiers();
+            foreach($identifiers as $identifier) {
                 $result[] = array(
-                    'type' => $type,
-                    'value' => $this->index['identifier_value'][$key],
-                    'identifier' => identifierResolution($this->index['identifier_value'][$key],$type)
+                    'type' => $identifier['identifier_type'],
+                    'value' => $identifier['identifier'],
+                    'identifier' => identifierResolution($identifier['identifier'],$identifier['identifier_type'])
                 );
             }
         }
+
         return $result;
-	}
+    }
+
 }
+
