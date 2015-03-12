eb81223673e2229c48ea8b3111bd0cd38f457280
Minh Duc Nguyen <dekarvn@gmail.com>
2015-03-12 17:13:33 +1100

    Activity View. fix empty researchers

--- applications/portal/templates/omega/registry_object/activity_contents/activity-people.blade.php
+++ applications/portal/templates/omega/registry_object/activity_contents/activity-people.blade.php
@@ -1,19 +1,31 @@
 <?php $researcherfound = false; ?>
 @if($ro->relationships)
-        @if(isset($ro->relationships['party_one']))
-            <strong>Researchers </strong>
-            <?php
+    @if(isset($ro->relationships['party_one']))
+        <strong>Researchers </strong>
+        <?php
             $peoplecount = 0;
-            $researcherfound = true; ?>
-            @foreach($ro->relationships['party_one'] as $col)
-                <?php
+            $researcherfound = true;
+        ?>
+
+        <?php 
+            //post process, making sure everyone has a title, a slug and a registry object id
+            $people = array();
+            foreach($ro->relationships['party_one'] as $col) {
+                if($col['title'] && $col['slug'] && $col['registry_object_id']) {
+                    array_push($people, $col);
+                }
+            }
+        ?>
+
+        @foreach($people as $col)
+            <?php
                 $peoplecount++;
-                $type = readable($col['relation_type']); ?>
+                $type = readable($col['relation_type']);
+            ?>
+            @if($col['title'])
                 <a href="<?php echo base_url()?>{{$col['slug']}}/{{$col['registry_object_id']}}" style="margin-right:5px;">{{$col['title']}}</a>({{$type}})
-                <?php if($peoplecount<count($ro->relationships['party_one'])) { echo ", ";} ?>
-            @endforeach
-        @endif
+            @endif
+            <?php if($peoplecount<count($people)) { echo ", ";} ?>
+        @endforeach
+    @endif
 @endif
-
-
-
