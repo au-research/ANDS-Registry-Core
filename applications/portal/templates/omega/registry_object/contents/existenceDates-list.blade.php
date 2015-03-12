a3ac3dac8e70f7c6ae2e3e4e3607f269aa579943
Elizabeth Woods <u3485947@devl.ands.org.au>
2015-03-13 09:58:58 +1100

    added a hyphen to the display of a singular existence date

--- applications/portal/templates/omega/registry_object/contents/existenceDates-list.blade.php
+++ applications/portal/templates/omega/registry_object/contents/existenceDates-list.blade.php
@@ -1,5 +1,11 @@
 @if($ro->existenceDates)
     @foreach($ro->existenceDates as $date)
-        {{str_replace("T00:00:00Z","",$date)}}
+        <?php
+        $date = str_replace("T00:00:00Z","",$date);
+        echo $date;
+        if(strlen($date)<11){
+            echo " - ";
+        }
+        ?>
     @endforeach
 @endif
