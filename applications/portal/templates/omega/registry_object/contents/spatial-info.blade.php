4c6292a2b1bef5956d4a113cb0b000e7136d6f99
Elizabeth Woods <u3485947@devl.ands.org.au>
2015-03-13 08:38:35 +1100

    added other all coverage spatial types to the spatial display block

--- applications/portal/templates/omega/registry_object/contents/spatial-info.blade.php
+++ applications/portal/templates/omega/registry_object/contents/spatial-info.blade.php
@@ -1,20 +1,40 @@
+
 @if($ro->spatial)
+<?php
+$needmap = false;
+foreach($ro->spatial as $a_coverage){
+    if(isset($a_coverage['polygon'])){
+        $needmap = true;
+    }
+}
+?>
+
+
 <div class="swatch-white">
     <div class="panel panel-primary element-no-top element-short-bottom panel-content">
         <div class="panel-heading">
             <a href="">Spatial Coverage And Location</a>
         </div>
         <div class="panel-body swatch-white">
+            @if($needmap)
 			<div id="spatial_coverage_map" class="map-canvas angular-google-map-container"></div>
-			<!-- @include('registry_object/facet/map') -->
-			@foreach($ro->spatial as $a_coverage)
-            @if($a_coverage['polygon'])
-			<p class="coverage hide" itemprop="spatial">{{$a_coverage['polygon']}}</p>
-            @else
-            <p class="coverage hide">{{$a_coverage['polygon']}}</p>
             @endif
-			<p class="spatial_coverage_center hide">{{$a_coverage['center']}}</p>
+			<!-- @include('registry_object/facet/map') -->
+
+			@foreach($ro->spatial as $a_coverage)
+            @if(isset($a_coverage['polygon']))
+			    <p class="coverage hide" itemprop="spatial">{{$a_coverage['polygon']}}</p>
+            @endif
+            @if(isset($a_coverage['center']))
+			    <p class="spatial_coverage_center hide">{{$a_coverage['center']}}</p>
+            @endif
 			@endforeach
+
+            @foreach($ro->spatial as $a_coverage)
+                @if(isset($a_coverage['type']))
+                    <p>{{$a_coverage['type']}}: {{$a_coverage['value']}}</p>
+                @endif
+            @endforeach
         </div>
     </div>
     </div>
