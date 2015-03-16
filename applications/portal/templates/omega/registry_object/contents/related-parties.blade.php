2153ddec35f5589fb1fae3c3b83b551d0fa1b9a5
Elizabeth Woods <u3485947@devl.ands.org.au>
2015-03-16 12:11:01 +1100

    re-instated the addition of a related group name to title area if no related people exist

--- applications/portal/templates/omega/registry_object/contents/related-parties.blade.php
+++ applications/portal/templates/omega/registry_object/contents/related-parties.blade.php
@@ -18,3 +18,22 @@
 	@endif
 @endif
 
+@if($ro->relationships && !(isset($ro->relationships['party_one'])) && isset($ro->relationships['party_multi']))
+    @foreach($ro->relationships['party_multi'] as $col)
+        @if($col['slug'] && $col['registry_object_id'])
+        <?php
+        $description = '';
+        if(isset($col['relation_description']) && $col['relation_description']!='')
+        {
+            $description = 'tip="'.$col['relation_description'].'"';
+        }
+        ?>
+        <a href="<?php echo base_url()?>{{$col['slug']}}/{{$col['registry_object_id']}}" {{$description}} class="ro_preview" ro_id="{{$col['registry_object_id']}}" style="margin-right:5px;">{{$col['title']}} <small>({{readable($col['relation_type'])}}) </small></a>
+        @elseif(isset($col['identifier_relation_id']))
+        <a href="<?php echo base_url()?>" class="ro_preview" identifier_relation_id="{{$col['identifier_relation_id']}}" ">{{$col['title']}} <small>({{readable($col['relation_type'])}}) </small></a>
+        @endif
+    @endforeach
+    @if(sizeof($ro->relationships['party_multi']) < $ro->relationships['party_multi_count_solr'])
+    <a href="{{portal_url()}}search/#!/related_{{$ro->core['class']}}_id={{$ro->core['id']}}/class=party/type=group">View all {{$ro->relationships['party_multi_count_solr']}} related parties</a></li>
+    @endif
+@endif
