203b3a84cc462536cd62a2680528bb22c03cd30b
Elizabeth Woods <u3485947@devl.ands.org.au>
2015-03-17 13:40:54 +1100

    removed the bug in desxcription display on related record tooltip

--- applications/portal/templates/omega/registry_object/contents/the-description.blade.php
+++ applications/portal/templates/omega/registry_object/contents/the-description.blade.php
@@ -3,7 +3,7 @@
 	$omit = array('logo');
 
 	$the_description = false;
-	if (isset($ro->descriptions)) {
+	if (isset($ro->descriptions[0])) {
 		foreach($ro->descriptions as $desc) {
 			if($desc['type']=='brief' && !$the_description) {
 				$the_description = $desc['description'];
@@ -15,7 +15,7 @@
 			}
 		}
 		if(!$the_description) $the_description = $ro->descriptions[0]['description'];
-        $the_description = trim(strip_tags(html_entity_decode(html_entity_decode($the_description))));
+        $the_description = trim(strip_tags(html_entity_decode(html_entity_decode(html_entity_decode($the_description)))));
         if (strlen($the_description) > 500)
             $the_description = substr($the_description, 0, 500) . '...';
 	}
