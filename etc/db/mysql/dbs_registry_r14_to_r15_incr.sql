30b15bcfd5416b56d83d26cd6e056ba3e90018c2
Richard Walker <Richard.Walker@ands.org.au>
2015-03-30 15:05:19 +1100

    First go at fetching citations from TR DCI
    Implement a system for fetching citation data from the TR DCI service.

--- etc/db/mysql/dbs_registry_r14_to_r15_incr.sql
+++ etc/db/mysql/dbs_registry_r14_to_r15_incr.sql
@@ -7,3 +7,23 @@ CREATE  TABLE `dbs_registry`.`tasks` (
   `priority` INT NULL ,
   `params` VARCHAR(512) NULL ,
   PRIMARY KEY (`id`) );
+
+CREATE TABLE `registry_object_citations` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `registry_object_id` mediumint(8) unsigned NOT NULL,
+  `data_source_id` mediumint(8) unsigned NOT NULL,
+  `service_provider` varchar(32) NOT NULL,
+  `query_terms` varchar(512) DEFAULT NULL,
+  `citation_data` text,
+  `status` varchar(32),
+  `Last_checked` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`),
+  KEY `idx_data_source_id_roc` (`data_source_id`),
+  KEY `idx_registry_object_id_roc` (`registry_object_id`),
+  CONSTRAINT `fk_roc_registry_object_id` FOREIGN KEY (`registry_object_id`)
+    REFERENCES `registry_objects` (`registry_object_id`)
+    ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_roc_data_source_id` FOREIGN KEY (`data_source_id`)
+    REFERENCES `data_sources` (`data_source_id`)
+    ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
