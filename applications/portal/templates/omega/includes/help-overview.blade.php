35246721fb4cf44b06e5679a0993d31cbc4023ff
Leo Monus <leo.monus@anu.edu.au>
2015-04-13 16:38:29 +1000

    Joel's bad typingos...

--- applications/portal/templates/omega/includes/help-overview.blade.php
+++ applications/portal/templates/omega/includes/help-overview.blade.php
@@ -1,4 +1,4 @@
-<p>Welcome to  Reserach Data Australia help. Watch the videos below to get yourself acquainted with site, or use the tabs displayed above to navigate to specific help sections.</p>
+<p>Welcome to Research Data Australia help. Watch the videos below to get yourself acquainted with site, or use the tabs displayed above to navigate to specific help sections.</p>
 <p>Note that the help can be accessed at any time by using the orange ‘Help’ button displayed in the bottom right hand corner of every page.</p>
 <!-- <iframe width="560" height="315" src="https://www.youtube.com/embed/spWa9ZQM21A?list=PLG25fMbdLRa5jt_fqievJtLbIMSJDb1-7" frameborder="0" allowfullscreen></iframe> -->
 
