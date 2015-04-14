a73888126efcffc0fac936c7ddbb9320326d14b9
Elizabeth Woods <u3485947@devl.ands.org.au>
2015-04-15 09:11:10 +1000

    added check of environment before displaying debug menu

--- applications/portal/templates/omega/registry_object/activity.blade.php
+++ applications/portal/templates/omega/registry_object/activity.blade.php
@@ -15,10 +15,12 @@
 
 @section('sidebar')
 
+@if(is_dev())
 <div class="panel panel-primary panel-content swatch-white">
-	<div class="panel-heading">Debug Menu</div>
-	<div class="panel-body">
-		<a href="{{$ro->api_url}}">API URL</a>
-	</div>
+    <div class="panel-heading">Debug Menu</div>
+    <div class="panel-body">
+        <a href="{{$ro->api_url}}">API URL</a>
+    </div>
 </div>
+@endif
 @stop
