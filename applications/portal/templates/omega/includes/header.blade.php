573861030fd8c88fa9e7a31c074815a051733aa5
Elizabeth Woods <u3485947@devl.ands.org.au>
2015-04-28 11:36:57 +1000

    facebook sharing for all non object pages

--- applications/portal/templates/omega/includes/header.blade.php
+++ applications/portal/templates/omega/includes/header.blade.php
@@ -2,11 +2,12 @@
 <head>
     <meta charset="utf-8">
     <title>{{ isset($ro->core['title']) ? $ro->core['title']: 'Research Data Australia'}}</title>
-    @if(isset($ro->core['title']))
-        <meta property="og:title" content="{{$ro->core['title']}}"/>
-    @endif
+
         <meta property="og:type" content="article"/>
     @if(isset($ro))
+        @if(isset($ro->core['title']))
+        <meta property="og:title" content="{{$ro->core['title']}}"/>
+        @endif
         @if(isset($logo) && $logo !== false)
             <meta property="og:image" content="{{$logo}}"/>
         @else
@@ -14,6 +15,8 @@
         @endif
         @if(isset($ro->core['description']))
             <meta property="og:description" content="{{$ro->core['description']}}"/>
+        @else
+            <meta property="og:description" content="Find, access, and re-use data for research - from over one hundred Australian research organisations, government agencies, and cultural institutions."/>
         @endif
         @if(isset($ro->core['url']) && get_config_item('default_base_url') == 'https://researchdata.ands.org.au/')
             <meta property="og:url" content="{{$ro->core['url']}}"/>
@@ -23,6 +26,15 @@
         @if(isset($ro->core['site_name']))
             <meta property="og:site_name" content="{{$ro->core['site_name']}}"/>
         @endif
+    @else
+        <meta property="og:title" content="Research Data Australia"/>
+
+        <meta property="og:image" content="{{get_config_item('default_base_url')}}assets/core/images/ANDS_logo.JPG"/>
+
+        <meta property="og:description" content="Find, access, and re-use data for research - from over one hundred Australian research organisations, government agencies, and cultural institutions."/>
+
+        <meta property="og:site_name" content="Research Data Australia"/>
+
     @endif
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta content="yes" name="apple-mobile-web-app-capable">
