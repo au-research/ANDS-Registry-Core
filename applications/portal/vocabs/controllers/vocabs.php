70c0b5cc75b61e57e4dc9f74ccd7faf76aeea65b
Minh Duc Nguyen <dekarvn@gmail.com>
2015-05-17 11:48:26 +1000

    added internal documentation, placeholder and stud pages for vocabs module

--- applications/portal/vocabs/controllers/vocabs.php
+++ applications/portal/vocabs/controllers/vocabs.php
@@ -1,14 +1,19 @@
 <?php
 /**
- * Vocab controller
+ * Vocabs controller
+ * This is the primary controller for the vocabulary module
+ * This module is meant as a standalone with all assets, views and models self contained
+ * within the applications/vocabs directory
+ * @version 1.0
  * @author  Minh Duc Nguyen <minh.nguyen@ands.org.au>
  */
 class Vocabs extends MX_Controller {
 
 	/**
 	 * Index / Home page
-	 * @author  Minh Duc Nguyen <minh.nguyen@ands.org.au>
+	 * Displaying the Home Page
 	 * @return view/html
+	 * @author  Minh Duc Nguyen <minh.nguyen@ands.org.au>
 	 */
 	function index(){
 		// header('Content-Type: text/html; charset=utf-8');
@@ -17,7 +22,8 @@ class Vocabs extends MX_Controller {
 
 	/**
 	 * Viewing a vocabulary by slug
-	 * @return view
+	 * @return view/html
+	 * @author  Minh Duc Nguyen <minh.nguyen@ands.org.au>
 	 */
 	public function view() {
 		//use test records for now
@@ -33,22 +39,69 @@ class Vocabs extends MX_Controller {
 		}
 	}
 
+	/**
+	 * Search
+	 * Displaying the search page
+	 * @ignore Not used for now. Home page is a search hybrid
+	 * @version 1.0
+	 * @return view/html
+	 * @author  Minh Duc Nguyen <minh.nguyen@ands.org.au>
+	 */
+	public function search() {
+		$this->blade->render('search');
+	}
+
+	/**
+	 * Adding a vocabulary
+	 * Displaying a view for adding a vocabulary
+	 * Using the same CMS as edit
+	 * @todo  ACL
+	 * @return view
+	 * @author  Minh Duc Nguyen <minh.nguyen@ands.org.au>
+	 */	
 	public function add() {
 		$this->blade
 			->set('scripts', array('vocabs_cms'))
 			->render('cms');
 	}
 
+	/**
+	 * Edit a vocabulary
+	 * Displaying a view for editing a vocabulary
+	 * Using the same CMS as add but directed towards a vocabulary
+	 * @todo ACL
+	 * @param  string $slug slug of the vocabulary, unique for a vocabulary
+	 * @return view
+	 * @author  Minh Duc Nguyen <minh.nguyen@ands.org.au>
+	 */
 	public function edit($slug=false) {
-
 		if (!$slug) throw new Exception('Require a Vocabulary Slug to edit');
-
 		// $vocab = $this->vocab->getBySlug($slug);
 		$this->blade
 			->set('scripts', array('vocabs_cms'))
 			->render('cms');
 	}
 
+	/**
+	 * Page Controller
+	 * For displaying static pages that belongs to the vocabs module
+	 * @param  $slug supported: [help|about|contribute]
+	 * @return view
+	 */
+	public function page($slug) {
+		$this->blade->render($slug);
+	}
+
+	/**
+	 * Services Controller
+	 * For allowing RESTful API against the Vocabs Portal Database / SOLR
+	 * @param  string $class  [vocabs] context
+	 * @param  string $id     [id] of the context
+	 * @param  string $method [method] description of the query
+	 * @return API response / JSON
+	 * @example services/vocabs/ , services/vocabs/anzsrc-for , services/vocabs/rifcs/versions
+	 * @author  Minh Duc Nguyen <minh.nguyen@ands.org.au>
+	 */
 	public function services($class='', $id='', $method='') {
 
 		//header
@@ -83,17 +136,11 @@ class Vocabs extends MX_Controller {
 		);
 	}
 
-
 	/**
-	 * About Page
-	 * @return view/html
-	 */
-	function about() {
-		$this->blade->render('about');
-	}
-
-	/**
-	 * Automated test functionality
+	 * Automated test tools
+	 * @version 1.0
+	 * @internal Used as internal testing before rolling out automated test cases
+	 * @author  Minh Duc Nguyen <minh.nguyen@ands.org.au>
 	 */
 	function test() {
 		//test getting the documents
