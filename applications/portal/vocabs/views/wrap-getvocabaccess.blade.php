d15a493632e3c30840270cbd6a1717f1d3de3a9f
Minh Duc Nguyen <dekarvn@gmail.com>
2015-05-27 10:54:03 +1000

    updated versions view and styling

--- applications/portal/vocabs/views/wrap-getvocabaccess.blade.php
+++ applications/portal/vocabs/views/wrap-getvocabaccess.blade.php
@@ -5,10 +5,28 @@ $cc=$vocab['licence'];
 
 <div class="panel swatch-white">
 	<div class="panel-body">
-		<h3>{{ $vocab['current_version']['title'] }}</h3>
+		<div class="version-licence">
+			@if($cc=='CC-BY')
+				<a href="http://creativecommons.org/licenses/by/3.0/au/" tip="Attribution"><img src="{{asset_url('images/icons/CC-BY.png', 'core')}}" class="img-cc" alt="CC-BY"></a> <br/>
+				@elseif($cc=='CC-BY-SA')
+				<a href="http://creativecommons.org/licenses/by-sa/3.0/au/" tip="Attribution-Shared Alike"><img src="{{asset_url('images/icons/CC-BY-SA.png', 'core')}}" class="img-cc" alt="CC-BY-SA"></a> <br/>
+				@elseif($cc=='CC-BY-ND')
+				<a href="http://creativecommons.org/licenses/by-nd/3.0/au/" tip="Attribution-No Derivatives"><img src="{{asset_url('images/icons/CC-BY-ND.png', 'core')}}" class="img-cc" alt="CC-BY-ND"></a> <br/>
+				@elseif($cc=='CC-BY-NC')
+				<a href="http://creativecommons.org/licenses/by-nc/3.0/au/" tip="Attribution-Non Commercial"><img src="{{asset_url('images/icons/CC-BY-NC.png', 'core')}}" class="img-cc" alt="CC-BY-NC"></a> <br/>
+				@elseif($cc=='CC-BY-NC-SA')
+				<a href="http://creativecommons.org/licenses/by-nc-sa/3.0/au/" tip="Attribution-Non Commercial-Shared Alike"><img src="{{asset_url('images/icons/CC-BY-NC-SA.png', 'core')}}" class="img-cc" alt="CC-BY-NC-SA"></a> <br/>
+				@elseif($cc=='CC-BY-NC-ND')
+				<a href="http://creativecommons.org/licenses/by-nc-nd/3.0/au/" tip="Attribution-Non Commercial-Non Derivatives"><img src="{{asset_url('images/icons/CC-BY-NC-ND.png', 'core')}}" class="img-cc" alt="CC-BY-NC-ND"></a> <br/>
+				@else
+				<span>Licence: {{sentenceCase($cc)}}</span>
+			@endif
+		</div>
+
+		<h3 class="element-short-bottom">{{ $vocab['current_version']['title'] }}</h3>
 		@foreach($vocab['current_version']['access_points'] as $ap)
 			<div class="current-version-ap-block">
-				<a class="btn btn-lg btn-block btn-primary" href="{{ $ap['uri'] }}">{{ $ap['type'] }}</a>
+				<a class="btn btn-lg btn-block btn-primary" href="{{ $ap['uri'] }}"><i class="fa fa-cube"></i> {{ $ap['type'] }}</a>
 				<div class="btn-group btn-group-justified element element-shorter-bottom element-no-top" role="group" aria-label="...">
 					<a class="btn btn-sm btn-default"><i class="fa fa-edit"></i> {{ $ap['format'] }}</a>
 				</div>
@@ -17,7 +35,7 @@ $cc=$vocab['licence'];
 		<div class="clearfix"></div>
 		
 		<p>
-			<h4>Note:</h4>
+			<h4>Note</h4>
 			{{ $vocab['current_version']['note'] }}
 		</p>
 	</div>
@@ -31,7 +49,7 @@ $cc=$vocab['licence'];
 					<ul>
 						@if(isset($version['access_points']))
 							@foreach($version['access_points'] as $ap)
-							<li><a href="{{ $ap['uri'] }}">{{ $ap['format'] }} - {{ $ap['type'] }}</a></li>
+							<li><a href="{{ $ap['uri'] }}" class="btn btn-default">{{ $ap['format'] }} - {{ $ap['type'] }}</a></li>
 							@endforeach
 						@endif
 					</ul>
