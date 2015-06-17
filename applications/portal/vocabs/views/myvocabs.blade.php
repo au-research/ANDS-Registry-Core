065990582e2a3973b44784b7d4efd75ad1c0400d
Minh Duc Nguyen <dekarvn@gmail.com>
2015-06-17 12:08:12 +1000

    fix editing published vocabulary

--- applications/portal/vocabs/views/myvocabs.blade.php
+++ applications/portal/vocabs/views/myvocabs.blade.php
@@ -23,10 +23,10 @@
 										@foreach($owned_vocabs as $vocab)
 											@if($vocab['status']=='published')
 											<tr>
-												<td><a href="{{ portal_url('vocabs/edit/'.$vocab['slug']) }}">{{ $vocab['title'] }}</a></td>
+												<td><a href="{{ portal_url('vocabs/edit/'.$vocab['id']) }}">{{ $vocab['title'] }}</a></td>
 												<td>
 													<div class="btn-group">
-														<a href="{{ portal_url($vocab['slug']) }}" class="btn btn-primary"><i class="fa fa-search"></i> View</a>
+														<a href="{{ portal_url($vocab['id']) }}" class="btn btn-primary"><i class="fa fa-search"></i> View</a>
 														<a href="{{ portal_url('vocabs/edit/'.$vocab['id']) }}" class="btn btn-primary"><i class="fa fa-edit"></i> Edit</a>
 														<a href="javascript:;" class="btn btn-primary deleteVocab" vocab_id="{{ $vocab['id'] }}"><i class="fa fa-trash"></i></a>
 													</div>
