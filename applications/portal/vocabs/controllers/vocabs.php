b840f9fd7242247dd8115b13f7f7383086959e26
Minh Duc Nguyen <dekarvn@gmail.com>
2015-06-22 14:51:34 +1000

    further fixes to download link

--- applications/portal/vocabs/controllers/vocabs.php
+++ applications/portal/vocabs/controllers/vocabs.php
@@ -638,8 +638,7 @@ class Vocabs extends MX_Controller {
 		$file = $this->input->get('file');
 		if (!$file) throw new Exception('File (required) not found');
 		if (!file_exists($file)) $file = vocab_uploaded_url($file);
-		if (!file_exists(vocab_uploaded_url($file))) throw new Exception('File not found');
-		$file = vocab_uploaded_url($file);
+		if (!file_exists($file)) throw new Exception('File not found');
 		header('Content-Description: File Transfer');
 		header('Content-Type: application/octet-stream');
 		header('Content-Disposition: attachment; filename='.basename($file));
