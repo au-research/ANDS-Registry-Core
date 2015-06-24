0ff37b2c0bff0859fca6790ddbf0a6e199d215b3
Minh Duc Nguyen <minh.nguyen@ands.org.au>
2015-06-24 10:47:34 +1000

    remove debug code

--- engine/libraries/User.php
+++ engine/libraries/User.php
@@ -57,7 +57,6 @@ class User {
 				$aff[] = $r['role_id'];
 			}
 			setcookie('ands_affiliations', json_encode($aff, true), 0, '/', '.ands.org.au', true);
-			dd(get_cookie('affiliations'));
 			// $this->appendAffiliation($aff);
 		}
 
