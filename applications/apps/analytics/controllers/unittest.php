20c07cc07c8701803832cca63a62225fc7f6f0b3
Minh Duc Nguyen <minh.nguyen@ands.org.au>
2015-07-27 11:32:25 +1000

    Analytics [tests]. Added automated test tool for Analytics

---
+++ applications/apps/analytics/controllers/unittest.php
@@ -0,0 +1,162 @@
+<?php if (!defined('BASEPATH')) {
+    exit('No direct script access allowed');
+}
+
+/**
+ * Unit Test Controller for Analytics Module
+ * @todo make official Test Class usecase
+ * @author Minh Duc Nguyen <minh.nguyen@ands.org.au>
+ */
+class Unittest extends MX_Controller
+{
+
+    /**
+     * Test Data
+     * @var array
+     */
+    private $testdata = array(
+        array(
+            'log' => 'portal',
+            'period' => ['startDate' => '2015-06-01', 'endDate' => '2015-06-06'],
+            'group' => [
+                'type' => 'group',
+                'value' => 'Australian Antarctic Data Centre',
+            ],
+            'dimensions' => ['portal_view', 'portal_search'],
+        ),
+        array(
+            'log' => 'portal',
+            'period' => ['startDate' => '2015-06-01', 'endDate' => '2015-06-06'],
+            'group' => [
+                'type' => 'group',
+                'value' => 'Griffith University',
+            ],
+            'dimensions' => ['portal_view', 'portal_search'],
+        ),
+    );
+
+    /**
+     * Index entry
+     * If CLI then just return pass or fail
+     * If Browser then return the entire test results
+     * @author Minh Duc Nguyen <minh.nguyen@ands.org.au>
+     * @return test results
+     */
+    public function index()
+    {
+
+        $this->run_tests();
+
+        if ($this->input->is_cli_request()) {
+            $this->output->set_header('Content-type: application/json');
+            set_exception_handler('json_exception_handler');
+            $result = $this->unit->result();
+            $pass = true;
+            foreach ($result as $res) {
+                if ($res['Result'] != 'Passed') {
+                    $pass = false;
+                }
+            }
+            echo $pass ? 'Unit Test Passed' . "\n" : "Unit Test Failed" . "\n";
+        } else {
+            echo $this->unit->report();
+        }
+
+    }
+
+    /**
+     * Actually run the tests
+     * @author Minh Duc Nguyen <minh.nguyen@ands.org.au>
+     * @return void
+     */
+    public function run_tests()
+    {
+        $this->testSummaryAPI($this->testdata[0]);
+        $this->testSummaryAPI($this->testdata[1]);
+        $this->testGetStatAPI('doi', $this->testdata[0]);
+        $this->testGetStatAPI('tr', $this->testdata[0]);
+        $this->testGetStatAPI('doi', $this->testdata[1]);
+        $this->testGetStatAPI('tr', $this->testdata[1]);
+    }
+
+    /**
+     * Test Summary API
+     * @param  data $testdata
+     * @return void
+     */
+    public function testSummaryAPI($testdata)
+    {
+        $result = $this->callAPI('summary', $testdata);
+        //check valid and is JSON
+        $this->unit->run($this->isJson($result), true, 'Valid Request');
+        $result = json_decode($result, true);
+        $this->unit->run($result, 'is_array', 'Array return');
+
+        $this->unit->run($result['result'], 'is_array', 'Has Result');
+        foreach ($result['result'] as $date => $res) {
+            $this->unit->run($res, 'is_array', 'Has result for ' . $date);
+            foreach ($res as $key => $data) {
+                $this->unit->run($data, 'is_int', 'Has data for ' . $key . ' ' . $data);
+            }
+        }
+    }
+
+    /**
+     * Test GetStat API
+     * @param  string $path
+     * @param  data $testdata
+     * @return void
+     */
+    public function testGetStatAPI($path, $testdata)
+    {
+        $result = $this->callAPI('getStat/' . $path, $testdata);
+        //check valid and is JSON
+        $this->unit->run($this->isJson($result), true, 'Valid Request');
+        $result = json_decode($result, true);
+        $this->unit->run($result, 'is_array', 'Array return');
+
+        if ($path == 'doi') {
+            $this->unit->run($result['total'], 'is_int', 'Has Total ' . $result['total']);
+            $this->unit->run($result['missing_doi'], 'is_int', 'Has Missing ' . $result['missing_doi']);
+            $this->unit->run($result['has_doi'], 'is_int', 'Has DOI ' . $result['has_doi']);
+        } elseif ($path == 'tr') {
+            //has doc count on all result
+            foreach ($result as $res) {
+                $this->unit->run($res['doc_count'], 'is_int', 'Has ' . $res['key'] . ' => ' . $res['doc_count']);
+            }
+        }
+    }
+
+    /**
+     * Helper function to determine if a string is a json
+     * @param  string  $string
+     * @return boolean         is a json or not
+     */
+    private function isJson($string)
+    {
+        json_decode($string);
+        return (json_last_error() == JSON_ERROR_NONE);
+    }
+
+    /**
+     * Helper function to POST data to the API
+     * @param  string $path
+     * @param  data $filters
+     * @return data
+     */
+    public function callAPI($path, $filters)
+    {
+        $filters['filters'] = $filters;
+        $result = curl_post(apps_url('analytics/' . $path), json_encode($filters));
+        return $result;
+    }
+
+    //boring constructor
+    public function __construct()
+    {
+        $this->load->model('summary');
+        $this->load->model('dois');
+        $this->load->library('unit_test');
+    }
+
+}
