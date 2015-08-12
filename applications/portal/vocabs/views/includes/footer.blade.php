74da1f7c25e8aaeaf0f744657b2c7902382bbafa
Minh Duc Nguyen <minh.nguyen@ands.org.au>
2015-08-12 11:54:41 +1000

    Vocabs [display] change of footer text

--- applications/portal/vocabs/views/includes/footer.blade.php
+++ applications/portal/vocabs/views/includes/footer.blade.php
@@ -4,8 +4,7 @@
             <div class="row element-normal-top element-normal-bottom">
                 <div class="col-md-8">
                     <p>
-                        Vocabulary Portal is the controlled vocabulary discovery service of the Australian National Data Service (ANDS).
-                        ANDS is supported by the Australian Government through the National Collaborative Research Infrastructure Strategy Program
+                        Research Vocabularies Australia is the controlled vocabulary discovery service of the Australian National Data Service (ANDS). ANDS is supported by the Australian Government through the National Collaborative Research Infrastructure Strategy Program.
                         <a href="http://www.ands.org.au/andsbrochurejan2015.pdf" target="_blank" style="color:#84A07B">Read more about ANDS...</a>
                     </p>
                 </div>
@@ -13,7 +12,7 @@
                    <a href="https://education.gov.au/national-collaborative-research-infrastructure-strategy-ncris" target="_blank" class="gov_logo"><img style="height:75px;" src="<?php echo asset_url('images/NCRIS_PROVIDER_rev.png','core');?>" alt="National Collaborative Research Infrastructure Strategy (NCRIS)" /></a>
                 </div>
                 <div class="col-md-2">
-                    <a href="http://www.ands.org.au/" class="footer_logo"><img src="{{asset_url('images/footer_logo_rev.png', 'core')}}" alt="" style="height:75px;"/></a>    
+                    <a href="http://www.ands.org.au/" class="footer_logo"><img src="{{asset_url('images/footer_logo_rev.png', 'core')}}" alt="" style="height:75px;"/></a>
                 </div>
             </div>
             <div class="row element-normal-top element-normal-bottom">
