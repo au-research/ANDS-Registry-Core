c94f19a2929466a7c0faccfbdd45635e1aeea07e
Liz Woods <u3485947@devl.ands.org.au>
2015-09-04 11:01:09 +1000

    amended the senctence extract to check each sentance for the required snippet and to append or prepend continuation chars as required - also sentence extract will only be called against descriptions

--- applications/portal/templates/omega/registry_object/search.blade.php
+++ applications/portal/templates/omega/registry_object/search.blade.php
@@ -62,7 +62,8 @@
 
                         <div ng-repeat="(index, content) in getHighlight(doc.id)" class="element-shorter-bottom">
                             <div ng-repeat="c in content track by $index" class="element-shortest-bottom">
-                                <span ng-bind-html="c | extractSentence | trustAsHtml"></span> <span class="muted">(in [[index | highlightreadable]])</span>
+                                <span ng-bind-html="c | extractSentence | trustAsHtml" ng-if="index=='description_value'"> </span>
+                                <span ng-bind-html="c | trustAsHtml" ng-if="index!='description_value'"> </span> <span class="muted">(in [[index | highlightreadable]])</span>
                             </div>
                         </div>
 
