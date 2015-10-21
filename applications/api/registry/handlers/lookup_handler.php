951c65f599ee14de7c3f22cd4ad619470c81c3de
Minh Duc Nguyen <minh.nguyen@ands.org.au>
2015-10-22 09:12:10 +1100

    API . Files for psr-4 autoloader

---
+++ applications/api/registry/handlers/lookup_handler.php
@@ -0,0 +1,57 @@
+<?php
+namespace ANDS\Registry\Handler;
+
+use \Exception as Exception;
+
+/**
+ * Handles registry/lookup
+ * Lookup a registry object based on anything
+ * Used mainly for registry widget
+ * @author Minh Duc Nguyen <minh.nguyen@ands.org.au>
+ */
+class LookupHandler extends Handler
+{
+
+    /**
+     * Handles registry/lookup
+     * @return array
+     */
+    public function handle()
+    {
+        $this->ci->load->model('registry/registry_object/registry_objects', 'ro');
+
+        $query = $this->ci->input->get('q');
+        $query = urldecode($query);
+
+        $ro = $this->ci->ro->getByID($query);
+        if (!$ro) {
+            $ro = $this->ci->ro->getBySlug($query);
+        }
+
+        if (!$ro) {
+            $ro = $this->ci->ro->getPublishedByKey($query);
+        }
+
+        if (!$ro) {
+            throw new Exception('No Registry Object Found');
+        }
+
+        $result = array(
+            'id' => $ro->id,
+            'rda_link' => portal_url($ro->slug),
+            'key' => $ro->key,
+            'slug' => $ro->slug,
+            'title' => $ro->title,
+            'class' => $ro->class,
+            'type' => $ro->type,
+            'group' => $ro->group,
+        );
+        if ($ro->getMetadata('the_description')) {
+            $result['description'] = $ro->getMetadata('the_description');
+        } else {
+            $result['description'] = '';
+        }
+
+        return $result;
+    }
+}
