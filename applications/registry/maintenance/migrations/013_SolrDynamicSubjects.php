5ab8b61fd3b4a570eb3a4cf773d3d2d26504fd71
Minh Duc Nguyen <minh.nguyen@ands.org.au>
2016-01-13 11:27:55 +1100

    Solr Migration. Add tsubject dynamic field

---
+++ applications/registry/maintenance/migrations/013_SolrDynamicSubjects.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Class:  SolrDynamicSubjects
+ * @author: Minh Duc Nguyen <minh.nguyen@ands.org.au>
+ */
+
+namespace ANDS;
+
+
+class SolrDynamicSubjects extends GenericSolrMigration
+{
+    public function up(){
+        $this->ci->load->library('solr');
+        $this->ci->solr->setCore('portal');
+        return $this->ci->solr->schema([
+            'add-dynamic-field' => [
+                ['name' => 'tsubject_*', 'type' => 'string', 'stored' => 'true', 'indexed' => true]
+            ]
+        ]);
+    }
+
+    public function down(){
+        $this->ci->load->library('solr');
+        $this->ci->solr->setCore('portal');
+        return $this->ci->solr->schema([
+            'delete-dynamic-field' => [
+                ['name' => 'tsubject_*']
+            ]
+        ]);
+    }
+}
