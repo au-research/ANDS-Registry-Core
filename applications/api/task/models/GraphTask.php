98010a067f240bc048afce878dd09ee62d69a508
Minh Duc Nguyen <minh.nguyen@ands.org.au>
2016-02-01 11:15:10 +1100

    Sync. Graph. CC-1571 CC-1579. Adding graph generation task upon syncing a ds

--- applications/api/task/models/GraphTask.php
+++ applications/api/task/models/GraphTask.php
@@ -18,57 +18,137 @@ use Neoxygen\NeoClient\ClientBuilder;
 class GraphTask extends Task
 {
     private $client;
+    private $chunkSize = 100;
+
     public function run_task()
     {
+        $this->ci =& get_instance();
+        $neo4jConf = get_config_item('neo4j');
+
+        try {
+            $this->loadParams();
+            $this->constructClient($neo4jConf);
+
+            $this->ci->load->model('registry/registry_object/registry_objects', 'ro');
+            $this->ci->load->model('registry/data_source/data_sources', 'ds');
+
+            switch ($this->target) {
+                case 'ds':
+                    $list = explode(',', $this->target_id);
+                    foreach ($list as $dsID) {
+                        if ($dsID) {
+                            $this->syncDS($dsID);
+                        } else {
+                            throw new Exception("No valid Data Source ID found");
+                        }
+                    }
+                    break;
+                case 'ro':
+                    $list = explode(',', $this->target_id);
+                    $this->syncRo($list);
+                    break;
+            }
+
+        } catch (Exception $e) {
+            throw new Exception($e);
+        }
+    }
+
+    private function syncDS($dsID){
+        if (!$this->chunkSize) {
+            throw new Exception("No chunk defined for this sync task");
+        }
+        if (!$dsID) {
+            throw new Exception("Data Source ID required");
+        }
+        $offset = ($this->chunkPos - 1) * $this->chunkSize;
+        $limit = $this->chunkSize;
+        $ids = $this->ci->ro->getIDsByDataSourceID($dsID, false, 'PUBLISHED', $offset, $limit);
+
+        if (sizeof($ids) > 0) {
+            $this->log('Generating Graph for chunk ' . $this->chunkPos . ' of Data Source ' . $dsID . ' for ' . sizeof($ids) . ' records');
+            try {
+                $this->syncRO($ids);
+            } catch (Exception $e) {
+                throw new Exception('Error DSID:' . $dsID . ' Message: ' . $e->getMessage());
+            }
+        } else {
+            $this->log('No records for data source: ' . $dsID);
+        }
+    }
+
+    private function syncRO($ids){
+        foreach ($ids as $id) {
+            try {
+                $record = $this->ci->ro->getByID($id);
+                $this->addNode($record);
+                $relatedObjects = $record->getAllRelatedObjects(false, false, true);
+                foreach ($relatedObjects as $related) {
+                    $relatedObject = $this->ci->ro->getByID($related['registry_object_id']);
+                    $this->addNode($relatedObject);
+                    $this->addRelationship($record, $related);
+                    unset($relatedObject);
+                }
+                unset($record);
+            } catch (Exception $e) {
+                $this->log("Error generating graph for RO: ".$id.' -> '.$e->getMessage());
+            }
+        }
+    }
+
+    public function loadParams(){
+        parse_str($this->params, $params);
+        $this->target = isset($params['type']) ? $params['type'] : false;
+        $this->target_id = isset($params['id']) ? $params['id'] : false;
+
+        if (isset($params['chunkPos'])) {
+            $this->chunkPos = $params['chunkPos'];
+        }
+    }
+
+    /**
+     * Returns a Neo4JClient that connects with the default config
+     *
+     * @param $neo4jConf
+     */
+    private function constructClient($neo4jConf)
+    {
+        $url = parse_url($neo4jConf['url']);
         $this->client = ClientBuilder::create()
-            ->addConnection('default', 'http', 'minhdev.ands.org.au', 7474, true, 'neo4j', 'abc123')
+            ->addConnection('default',
+                $url['scheme'], $url['host'], $url['port'], true,
+                $neo4jConf['username'], $neo4jConf['password'])
             ->setDefaultTimeout(20)
             ->setAutoFormatResponse(true)
             ->build();
-
-        $this->ci =& get_instance();
-        $this->ci->load->model('registry/registry_object/registry_objects', 'ro');
-        $this->ci->load->model('registry/data_source/data_sources', 'ds');
-
-
-        $ids = $this->ci->ro->getIDsByDataSourceID(162, false, 'PUBLISHED');
-        foreach ($ids as $id) {
-            $this->sync($id);
-        }
-
     }
 
-    public function sync($recordID)
+
+    public function addNode($record)
     {
-        $record = $this->ci->ro->getByID($recordID);
-
-        $this->addNode($record);
-
-        $relatedObjects = $record->getAllRelatedObjects(false, false, true);
-        foreach ($relatedObjects as $related) {
-            $relatedObject = $this->ci->ro->getByID($related['registry_object_id']);
-            $this->addNode($relatedObject);
-            $this->addRelationship($record, $related);
-            unset($relatedObject);
-        }
-    }
-
-    public function addNode($record){
         $recordTitle = addslashes($record->title);
-        $encodedRecordData = "{id: '$record->id', key: '$record->key', title: '$recordTitle'}";
+        $recordKey = addslashes($record->key);
+        $encodedRecordData = "{id: '$record->id', key: '$recordKey', title: '$recordTitle'}";
         $recordClass = ucfirst($record->class);
         $q = "MERGE (n:RegistryObject:$recordClass $encodedRecordData)";
         $q .= " RETURN n";
+        if ($this->ci->input->get('debug')) {
+            $this->log($q);
+        }
         return $this->client->sendCypherQuery($q);
     }
 
-    public function addRelationship($record, $related){
+    public function addRelationship($record, $related)
+    {
         $relatedID = $related['registry_object_id'];
         $relationType = $related['relation_type'];
         $relationType = $this->camelCase($relationType);
         $q = "MATCH (n1:RegistryObject {id:'$record->id'}), (n2:RegistryObject {id:'$relatedID'})";
         $q .= " CREATE UNIQUE (n1)-[r:$relationType]-(n2)";
         $q .= " RETURN r";
+        if ($this->ci->input->get('debug')) {
+            $this->log($q);
+        }
         $result = $this->client->sendCypherQuery($q);
         return $result;
     }
