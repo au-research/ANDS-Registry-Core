2e52878d48a749052e9a997c0a1bb54aa2c4db12
Liz Woods <u3485947@devl.ands.org.au>
2016-02-16 11:45:34 +1100

    added the request_uri as the path to include logging of passed variables

--- applications/registry/services/controllers/oai.php
+++ applications/registry/services/controllers/oai.php
@@ -80,22 +80,25 @@ class Oai extends MX_Controller
 		$this->_header();
 
 
-        // logging of calls to legacy version of our services and widgets to flat file logging - added 09/02/2016
-
-        require_once (API_APP_PATH.'core/helpers/api_helper.php');
-        $terms = array(
-            'event' => 'api_hit',
-            'api_key' => 'public',
-            'api_version' => 'legacy',
-            'path' => 'services/oai'
-        );
-        api_log_terms($terms);
 
 		$verb = $this->input->get_post('verb', TRUE);
 		if (!empty($verb) and
 		    array_key_exists($verb, $this->verbs))
 		{
-			try
+
+           // logging of calls to legacy version of our services and widgets to flat file logging - added 09/02/2016
+
+            require_once (API_APP_PATH.'core/helpers/api_helper.php');
+            $terms = array(
+                'event' => 'api_hit',
+                'api_key' => 'public',
+                'api_version' => 'legacy',
+                'path' => $_SERVER['REQUEST_URI']
+            );
+            api_log_terms($terms);
+
+
+            try
 			{
 				#is this operation resumable?
 				if (array_key_exists('resume', $this->verbs[$verb]))
