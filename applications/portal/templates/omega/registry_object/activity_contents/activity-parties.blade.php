939334f373b4cb56c0e8d5a0cc90b7cd20da1ca4
Minh Duc Nguyen <minh.nguyen@ands.org.au>
2016-02-24 14:43:19 +1100

    Portal. Restore display of funders in activity title page

--- applications/portal/templates/omega/registry_object/activity_contents/activity-parties.blade.php
+++ applications/portal/templates/omega/registry_object/activity_contents/activity-parties.blade.php
@@ -1,51 +1,26 @@
 <?php
-$order = Array('isFundedBy','isManagedBy','isAdministeredBy');
-$prev_out ='';
+    $order = Array('isFundedBy','isManagedBy','isAdministeredBy');
 ?>
-
-@if($ro->relationships)
-    @foreach($order as $o)
-
-        @if(isset($ro->relationships['party_multi']))
-        @foreach($ro->relationships['party_multi'] as $col)
-            @if($col['relation_type']==$o)
-            <?php  if($o=='isFundedBy'){
-                        if($prev_out=='Funded by'){
-                            $output='';
-                        }
-                        else{
-                            $output = 'Funded by';
-                        }
-                        $prev_out = 'Funded by';
-                    } elseif($o=='isManagedBy') {
-                        if($prev_out=='Managed by'){
-                            $output='';
-                        }
-                        else{
-                            $output = 'Managed by';
-                        }
-                        $prev_out = 'Managed by';
-
-                    }else{
-                        if($prev_out=='Administered by'){
-                            $output='';
-                        }
-                        else{
-                            $output = 'Administered by';
-                        }
-                        $prev_out = 'Administered by';
+@if($related['organisations'] && $related['organisations']['count'] > 0)
+    @foreach ($related['organisations']['docs'] as $col)
+        @foreach($order as $o)
+            @if(in_array($o, $col['relation']))
+                <br/>
+                <?php
+                    $outputRelation = $o;
+                    switch($o) {
+                        case "isFundedBy": $outputRelation = "Funded by"; break;
+                        case "isManagedBy": $outputRelation = "Managed by"; break;
+                        case "isAdministeredBy": $outputRelation = "Administered by"; break;
                     }
-                if($output==''){
-                    echo ",";
-                }else{
-                    echo "<br />";
-                }
-                echo "<strong>".$output;?></strong> &nbsp; <a href="<?php echo base_url()?>{{$col['slug']}}/{{$col['registry_object_id']}}" class="ro_preview" ro_id="{{$col['registry_object_id']}}">{{trim($col['title'])}}</a>
+                ?>
+                <strong>{{ $outputRelation }}</strong>
+                <a href="{{ base_url($col['to_slug'].'/'.$col['to_id']) }}"
+                   class="ro_preview"
+                   ro_id="{{ $col['to_id'] }}">
+                    {{ trim($col['to_title']) }}
+                </a>
             @endif
         @endforeach
-        @endif
-@endforeach
-
+    @endforeach
 @endif
-
-
