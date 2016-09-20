au-research/ANDS-Registry-Core

commit 44caee77d65fb35f3ecf7f7248f0a84d949ba2ea

Vocabs: Have Solr only return the fields we need
Add an fl parameter to the Solr search query that includes
only the fields that are needed for displaying the search
results.

applications/portal/vocabs/controllers/vocabs.php
@@ -399,6 +399,13 @@ class Vocabs extends MX_Controller
                  ->setOpt('defType', 'edismax')
                  ->setOpt('rows', $pp)
                  ->setOpt('q.alt', '*:*')
+                // see views/includes/search-view.blade.php
+                // for the fields that must be returned.
+                // NB: highlighting can/does also return snippets
+                // from other fields not listed in fl (which is good!).
+                 ->setOpt('fl',
+                     'id,slug,status,title,acronym,publisher,'
+                          . 'description,widgetable')
                  ->setOpt(
                      'qf',
                      'title_search^1 subject_search^0.5 '

commit 7adcd5ebd0baa86291e651393c752caac245570f

Vocabs CC-1814: make sure sissvoc_end_point is fetched from Solr
Commit 44caee77d65fb35f3ecf7f7248f0a84d949ba2ea broke the
Widget Explorer, because it limited the Solr search results
too much, i.e., by omitting the sissvoc_end_point values.
Now, explicitly request that field, and document that
the fields used by the Widget Explorer must be requested
from Solr.

applications/portal/vocabs/assets/js/vocabDisplayDirective.js
@@ -83,6 +83,9 @@
 
                     if (scope.vocab) {
                         var subjectValueInput = $('#sampleWidgetInput');
+                        // NB Use of Solr field sissvoc_end_point.
+                        // Must be included in the fields given in the
+                        // "fl" option. See controllers/vocabs.php/filter().
                         var sissvoc_end_point = scope.vocab.sissvoc_end_point;
 
                         if (clearField) {

applications/portal/vocabs/assets/templates/widgetDirective.html
@@ -10,6 +10,8 @@
         <ui-select-match placeholder="Select a vocabulary">{{$select.selected.title}}</ui-select-match>
         <ui-select-choices
            repeat="vocab in vocabList | filter: $select.search "> <!--  | filter: {title: $select.search} -->
+          <!-- NB: fields vocab.xyz need to have been returned by Solr
+          search. -->
           <div>
             [[ vocab.title ]]
             <br />

applications/portal/vocabs/controllers/vocabs.php
@@ -399,13 +399,19 @@ class Vocabs extends MX_Controller
                  ->setOpt('defType', 'edismax')
                  ->setOpt('rows', $pp)
                  ->setOpt('q.alt', '*:*')
-                // see views/includes/search-view.blade.php
-                // for the fields that must be returned.
+                // see (1) views/includes/search-view.blade.php
+                // for the fields that must be returned for the
+                // "main" search function,
+                // (2) assets/templates/widgetDirective.html and
+                // assets/js/vocabDisplayDirective.js for the
+                // fields needed for the Widget Explorer.
+                // The Widget Explorer's needs add "sissvoc_end_point"
+                // to the list required by the "main" search.
                 // NB: highlighting can/does also return snippets
                 // from other fields not listed in fl (which is good!).
                  ->setOpt('fl',
                      'id,slug,status,title,acronym,publisher,'
-                          . 'description,widgetable')
+                          . 'description,widgetable,sissvoc_end_point')
                  ->setOpt(
                      'qf',
                      'title_search^1 subject_search^0.5 '