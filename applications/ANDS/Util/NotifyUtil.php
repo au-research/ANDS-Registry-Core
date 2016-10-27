7984e5fb4fd2a46b4280bc2cc9203405fd8397ec
Minh Duc Nguyen <minh.nguyen@ands.org.au>
2016-10-27 16:30:31 +1100

    Import Pipeline. Enable Notification after debugging

--- applications/ANDS/Util/NotifyUtil.php
+++ applications/ANDS/Util/NotifyUtil.php
@@ -11,6 +11,6 @@ class NotifyUtil
     public static function notify($channel, $content)
     {
         $redis = new PredisClient();
-//        return $redis->publish($channel, $content);
+        return $redis->publish($channel, $content);
     }
 }
