d1918cb0d2ed921372b2f27cadc862a956641c05
Leo Monus <leo.monus@anu.edu.au>
2016-11-30 12:32:40 +1100

    identifier provider in task

--- applications/api/task/models/ImportSubTask/ProcessIdentifiers.php
+++ applications/api/task/models/ImportSubTask/ProcessIdentifiers.php
@@ -2,7 +2,8 @@
 
 
 namespace ANDS\API\Task\ImportSubTask;
-use ANDS\Repository\DataSourceRepository;
+use ANDS\Registry\Providers\IdentifierProvider;
+use ANDS\Repository\RegistryObjectsRepository;
 
 class ProcessIdentifiers extends ImportSubTask
 {
@@ -12,16 +13,13 @@ class ProcessIdentifiers extends ImportSubTask
 
     public function run_task()
     {
-        // TODO: Refactor to RIFCS\IdentifierProvider
-        $this->parent()->getCI()->load->model('registry/registry_object/registry_objects', 'ro');
         $importedRecords = $this->parent()->getTaskData("importedRecords");
         $total = count($importedRecords);
         debug("Processing Identifiers for $total records");
         foreach ( $importedRecords as $index=>$roID) {
-            $ro = $this->parent()->getCI()->ro->getByID($roID);
-            $ro->processIdentifiers();
-            $this->updateProgress($index, $total, "Processed ($index/$total) $ro->title($roID)");
-        }
-        debug("Processing Identifiers for $total records");
+                $record = RegistryObjectsRepository::getRecordByID($roID);
+                IdentifierProvider::process($record);
+                $this->updateProgress($index, $total, "Processed ($index/$total) $record->title($roID)");
+            }
     }
 }
