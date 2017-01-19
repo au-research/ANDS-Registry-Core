b55d8bca964f87c08da75a89356767ee01be97e0
Liz Woods <u3485947@devl.ands.org.au>
2017-01-19 12:26:42 +1100

    removed bug of displaying an empty div if none of the tagged themes are visible

--- applications/portal/templates/omega/registry_object/contents/theme.blade.php
+++ applications/portal/templates/omega/registry_object/contents/theme.blade.php
@@ -1,13 +1,22 @@
 
-@if(isset($ro->core['theme_page']) && isset($theme_page) && $theme_page !== false)
+@if(isset($ro->core['theme_page']) && isset($theme_page))
+    <?php
+    $displayTheme = false;
+    for($i=0;$i<count($theme_page);$i++) {
+        if($theme_page[$i] !== false)   $displayTheme = true;
+    }
+
+    if($displayTheme ) { ?>
 <div class="panel swatch-white">
     <div class="panel-heading"></div>
-        <?php for($i=0;$i<count($theme_page);$i++) {
-                if($theme_page[$i] !== false) { ?>
-    <div class="panel-body">
-        <a href="{{portal_url('theme/'.$ro->core['theme_page'][$i])}}"  style="margin-right:5px;"><img src="{{$theme_page[$i]['img_src']}}"/>See more {{$theme_page[$i]['title']}} data</a>
-    </div>
-        <?php       }
-                } ?>
+        <?php
+        for($i=0;$i<count($theme_page);$i++) {
+            if($theme_page[$i] !== false) { ?>
+            <div class="panel-body">
+             <a href="{{portal_url('theme/'.$ro->core['theme_page'][$i])}}"  style="margin-right:5px;"><img src="{{$theme_page[$i]['img_src']}}"/>See more {{$theme_page[$i]['title']}} data</a>
+            </div>
+        <?php }
+         } ?>
 </div>
+    <?php } ?>
 @endif
