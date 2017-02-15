15568afd97f38f525ac4e0ba1c726c2868e3eb0e
Minh Duc Nguyen <dekarvn@gmail.com>
2017-02-16 09:32:38 +1100

    CC-1874. Added PHPUnit and code coverage engine

---
+++ tests/ANDS/DataSourceTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace ANDS;
+use PHPUnit_Framework_TestCase;
+
+class DataSourceTest extends PHPUnit_Framework_TestCase
+{
+    /** @test **/
+    public function it_should_be_true_sample_test()
+    {
+        $this->assertTrue(true);
+    }
+}
