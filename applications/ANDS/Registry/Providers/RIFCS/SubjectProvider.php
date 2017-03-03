fee2b946b9e3b8d69d2b7ce355aeea43b2248fd4
Liz Woods <u3485947@devl.ands.org.au>
2017-03-03 13:11:23 +1100

    initial cut of subject resolution through solr concepts schema

---
+++ applications/ANDS/Registry/Providers/RIFCS/SubjectProvider.php
@@ -0,0 +1,137 @@
+<?php
+
+
+namespace ANDS\Registry\Providers\RIFCS;
+
+
+use ANDS\RecordData;
+use ANDS\RegistryObject;
+use ANDS\Registry\Providers\MetadataProvider;
+use ANDS\Registry\Providers\RIFCSProvider;
+use MinhD\SolrClient\SolrClient;
+use MinhD\SolrClient\SolrSearchResult;
+use ANDS\Util\XMLUtil;
+
+
+/**
+ * Class RelationshipProvider
+ * @package ANDS\Registry\Providers
+ */
+class SubjectProvider implements RIFCSProvider
+{
+
+
+    public static function process(RegistryObject $record)
+    {
+        return;
+    }
+
+    /**
+     * get subjects
+     *
+     * @param $record
+     * @return array
+     */
+    public static function get(RegistryObject $record)
+    {
+        $data = MetadataProvider::get($record);
+        return [
+            'subjects' => self::getSubjects($record, $data)
+        ];
+    }
+
+    /**
+     * get subjects
+     *
+     * @param $record
+     * @return array
+     */
+    public static function getSubjects(
+        RegistryObject $record,
+        $data = null
+    ) {
+        if (!$data) {
+            $data = MetadataProvider::getSelective($record, ['recordData']);
+        }
+
+        $subjects = [];
+        foreach (XMLUtil::getElementsByXPath($data['recordData'],
+            'ro:registryObject/ro:' . $record->class . '/ro:subject') AS $subject) {
+            $subjects[] = array(
+            'type' => (string)$subject["type"],
+            'value' => (string)$subject,
+            'uri' => (string)$subject["termIdentifier"]);
+            };
+        return $subjects;
+    }
+
+    /**
+     * process subjects
+     *
+     * @param $record
+     * @return array
+     */
+    function processSubjects(RegistryObject $record)
+    {
+        $subjects = self::getSubjects($record);
+
+        $solrClient = new SolrClient('devl.ands.org.au', '8983');
+        $solrClient->setCore('concepts');
+        $subjectsResolved = array();
+
+        foreach ($subjects AS $subject)
+        {
+            $type = $subject["type"];
+            $value = (string)($subject['value']);
+            $uri = $subject['uri'];
+            $positive_hit = false;
+            $score = 0;
+
+            if(!array_key_exists($value, $subjectsResolved))
+            {
+                $solrResult = $solrClient->search([
+                    'q' => '"'.$value.'" ^10',
+                    'fl' => '* , score',
+                    'sort' => 'score desc',
+                ]);
+
+                if ($solrResult->getNumFound() > 0){
+                    $result = $solrResult->getDocs();
+                    $top_response = $result[0];
+                    $resolved_type= $top_response->type[0];
+                    $resolved_value =  $top_response->label[0];
+                    $uri = $top_response->iri[0];
+                    $score = $top_response->score;
+                    isset($top_response->notation_s)? $new_value = $top_response->notation_s : $new_value = $value;
+                    $positive_hit = self::checkResult($top_response,$subject);
+                }
+
+                if($positive_hit) {
+                    $subjectsResolved[$value] = array('type' => $resolved_type, 'value' => $new_value, 'resolved' => $resolved_value, 'uri' => $uri, 'score' => $score);
+                    if($top_response->broader_labels_ss) {
+                        isset($top_response->broader_notations_ss) ? $index = $top_response->broader_notations_ss: $index = $top_response->broader_labels_ss;
+                        for($i=0;$i<count($top_response->broader_labels_ss);$i++) {
+                             $subjectsResolved[$index[$i]] = array(
+                                'type'=>$resolved_type,
+                                'value'=>$index[$i],
+                                'resolved'=>$top_response->broader_labels_ss[$i],
+                                'uri'=>$top_response->broader_iris_ss[$i]
+                            );
+                        }
+                    }
+                }else{
+                    $subjectsResolved[$value] = array('type' => $type, 'value' => $value, 'resolved' => $value, 'uri' => $uri);
+                }
+
+            }
+        }
+        return $subjectsResolved;
+    }
+
+    function checkResult($resolved, $subject){
+        if((string)($subject['value']==$resolved->label[0])) return true;
+        if($resolved->score<0.8) return false;
+        return true;
+    }
+
+}
---
+++ tests/ANDS/Registry/Providers/RIFCS/SubjectsProviderTest.php
@@ -0,0 +1,47 @@
+<?php
+
+
+namespace ANDS\Providers\RIFCS;
+
+
+use ANDS\Registry\Providers\RIFCS\SubjectProvider;
+use ANDS\Repository\RegistryObjectsRepository;
+
+class SubjectsProviderTest extends \RegistryTestClass
+{
+    protected $requiredKeys = [
+        "AUTCollectionToTestSearchFields37",
+        "AODN/073fde5a-bff3-1c1f-e053-08114f8c5588"
+    ];
+
+    /** @test **/
+    public function it_should_get_the_subjects()
+    {
+        $record = RegistryObjectsRepository::getPublishedByKey("AUTCollectionToTestSearchFields37");
+        $subjects = SubjectProvider::getSubjects($record);
+        $this->assertArraySubset(['0' => ['type' => 'local','value' => 'localSubject','uri' => '']], $subjects);
+    }
+
+    /** @test **/
+    public function it_should_get_the_resolved_subjects()
+    {
+        $record = RegistryObjectsRepository::getPublishedByKey("AUTCollectionToTestSearchFields37");
+        $resolved_subjects = SubjectProvider::processSubjects($record);
+        $this->assertArraySubset(['03'=>
+            ['type' => 'anzsrc-for',
+            'value' => '03',
+            'resolved' => 'CHEMICAL SCIENCES',
+            'uri' => 'http://purl.org/au-research/vocabulary/anzsrc-for/2008/03']], $resolved_subjects);
+    }
+    /** @test **/
+    public function it_should_get_the_non_notation_subjects()
+    {
+        $record = RegistryObjectsRepository::getPublishedByKey("AODN/073fde5a-bff3-1c1f-e053-08114f8c5588");
+        $resolved_subjects = SubjectProvider::processSubjects($record);
+        $this->assertArraySubset(['Earth Science | Atmosphere | Atmospheric Radiation | Solar Radiation'=>
+            ['type' => 'GCMD',
+                'value' => 'Earth Science | Atmosphere | Atmospheric Radiation | Solar Radiation',
+                'resolved' => 'SOLAR RADIATION',
+                'uri' => 'http://gcmdservices.gsfc.nasa.gov/kms/concept/a0f3474e-9a54-4a82-97c4-43864b48df4c']], $resolved_subjects);
+    }
+}
