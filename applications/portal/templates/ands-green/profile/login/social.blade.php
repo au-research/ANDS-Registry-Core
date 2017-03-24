5173eeda23d94295deaefb6ada62203b7a390c87
Minh Duc Nguyen <minh.nguyen@ands.org.au>
2017-03-24 13:53:06 +1100

    Updated oauth configuration to config/oauth and .env

--- applications/portal/templates/ands-green/profile/login/social.blade.php
+++ applications/portal/templates/ands-green/profile/login/social.blade.php
@@ -1,7 +1,7 @@
 <div class="tab-content" ng-show="tab=='social'">
     <div class=" element-no-top element-no-bottom" data-os-animation="none" data-os-animation-delay="0s">
         <?php
-            $oauth_conf = $this->config->item('oauth_config');
+            $oauth_conf = \ANDS\Util\Config::get('oauth');
         ?>
         @if($oauth_conf['providers']['Facebook']['enabled'])
             <a href="{{registry_url('auth/authenticate/facebook')}}" class="btn btn-primary btn-block btn-icon-left">Login with Facebook <span><i class="fa fa-facebook"></i></span></a>
