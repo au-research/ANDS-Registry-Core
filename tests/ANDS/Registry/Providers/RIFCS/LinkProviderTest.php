38a8d94ce936adb0a35488cb57f62a0efa7a2f82
Leo Monus <leo.monus@anu.edu.au>
2017-04-10 10:32:21 +1000

    link provider tested

--- tests/ANDS/Registry/Providers/RIFCS/LinkProviderTest.php
+++ tests/ANDS/Registry/Providers/RIFCS/LinkProviderTest.php
@@ -10,30 +10,30 @@ class LinkProviderTest extends \RegistryTestClass
 {
 
     protected $requiredKeys = [
-        'http://hdl.handle.net/10536/DRO/DU:30036766'
+        'Collection2_demo'
     ];
 
     /** @test **/
     public function test_it_sould_have_3_links() {
         // $collectionkey = 'AUTestingRecords3RelatedCollectionDatasetRelObj1';
 
-        $collectionkey = 'http://hdl.handle.net/10536/DRO/DU:30036766';
+        $collectionkey = 'Collection2_demo';
         $record = RegistryObjectsRepository::getPublishedByKey($collectionkey);
 
         $existringLinks = Links::where('registry_object_id', $record->registry_object_id)->get();
 
-        $this->assertEquals(count($existringLinks), 2);
+        $this->assertEquals(count($existringLinks), 13);
     }
 
     /** @test **/
     public function test_it_sould_create_Links() {
         // $collectionkey = 'AUTestingRecords3RelatedCollectionDatasetRelObj1';
-        $collectionkey = 'http://hdl.handle.net/10536/DRO/DU:30042980';
+        $collectionkey = 'Collection2_demo';
         $record = RegistryObjectsRepository::getPublishedByKey($collectionkey);
         LinkProvider::process($record);
 
         $links = Links::where('registry_object_id', $record->registry_object_id)->get();
-        $this->assertEquals(count($links), 2);
+        $this->assertEquals(count($links), 13);
     }
 
 }
