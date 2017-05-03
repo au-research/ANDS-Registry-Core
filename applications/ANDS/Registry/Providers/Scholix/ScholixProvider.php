8b2854b3ef6597adfb153533dc12dbbc70ccdc62
Minh Duc Nguyen <minh.nguyen@ands.org.au>
2017-05-04 09:31:06 +1000

    DOIBulkTask. Added configuration look up

--- applications/ANDS/Registry/Providers/Scholix/ScholixProvider.php
+++ applications/ANDS/Registry/Providers/Scholix/ScholixProvider.php
@@ -136,7 +136,7 @@ class ScholixProvider implements RegistryContentProvider
             ->pluck('scholix_identifier')->toArray();
         $shouldBeDeleted = array_diff($exist, $new);
         Scholix::whereIn('scholix_identifier', $shouldBeDeleted)->delete();
-        
+
         return $report;
     }
 
