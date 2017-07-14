b78e3f0bcb746f4847b50595ccd1e3a03ea0067a
Minh Duc Nguyen <minh.nguyen@ands.org.au>
2017-07-14 14:31:17 +1000

    SlugProvider class and tests

---
+++ applications/ANDS/Registry/Providers/RIFCS/SlugProvider.php
@@ -0,0 +1,59 @@
+<?php
+
+
+namespace ANDS\Registry\Providers\RIFCS;
+
+
+use ANDS\Registry\Providers\RIFCSProvider;
+use ANDS\RegistryObject;
+use ANDS\Util\StrUtil;
+
+class SlugProvider implements RIFCSProvider
+{
+    const maxLength = 200;
+    const maxNumWords = 5;
+
+    public static function process(RegistryObject $record)
+    {
+        // TODO: Implement process() method.
+    }
+
+    public static function get(RegistryObject $record)
+    {
+        $title = $record->title;
+
+        $simplifiedTitle = StrUtil::removeStopWords($title);
+        $result = strtolower($simplifiedTitle);
+
+        if (trim($simplifiedTitle) == "") {
+            $result = strtolower($title);
+        }
+
+        $result = preg_replace("/[^a-z0-9\s-]/", "", $result);
+        $result = trim(preg_replace("/[\s-]+/", " ", $result));
+        // $result = trim(substr($result, 0, self::maxLength));
+
+        $result = explode(' ', $result);
+
+        $possibleSlug = implode('-', $result);
+
+        // if the slug still consist of emptiness
+        // group-id
+        if (count($result) == 0 || trim($possibleSlug) == "") {
+            $result = [
+                url_title(strtolower($record->group)), $record->id
+            ];
+        }
+
+        if (sizeof($result) > self::maxNumWords) {
+            $first_three = array_slice($result, 0, 3);
+            $last_two = array_slice($result, -2, 2, true);
+            $result = array_merge($first_three, $last_two);
+            $result = implode('-', $result);
+        } else {
+            $result = implode('-', $result);
+        }
+
+        return $result;
+    }
+}
