0fc489948ecd6fdcf35b597e07c431705071e092
Elizabeth Margaret Woods <liz.woods@ands.org.au>
2017-10-27 12:29:52 +1100

    added Program Highlights to explore menu CC-2116

--- applications/portal/templates/ands-green/includes/top-menu.blade.php
+++ applications/portal/templates/ands-green/includes/top-menu.blade.php
@@ -25,6 +25,7 @@
                         <li><a href="{{portal_url('themes')}}"><i class="fa fa-folder-open icon-portal"></i> Themed collections</a></li>
                         <li><a href="{{portal_url('theme/services')}}"><i class="fa fa-wrench icon-portal"></i> Services and Tools</a></li>
                         <li><a href="{{portal_url('theme/open-data')}}"><i class="fa fa-unlock icon-portal"></i> Open data</a></li>
+                        <li><a href="{{portal_url('theme/highlighting-ands-projects')}}"><i class="fa fa-sun-o icon-portal"></i> Program Highlights</a></li>
                         <li><a href="{{portal_url('grants')}}"><i class="fa fa-flask icon-portal"></i> Grants and Projects</a></li>
                     </ul>
                 </li>
