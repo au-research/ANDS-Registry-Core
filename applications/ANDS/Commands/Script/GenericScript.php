f4c383cbea07c44c377c8b296b822eebca1446bd
Minh Duc Nguyen <minh.nguyen@ands.org.au>
2017-10-31 15:30:43 +1100

    Registry. CC-2115. Added NLAPullBack script
    This script fetches the NLA party records and insert the RIFCS into
    the registry. This is a refactor for the nlaPullback functionality.
    
    * Added create data source for repository. This will default all the parameters
    * Fixed an issue with XMLUtil where REGISTRY_APP_PATH is not defined. Autoload
    outside of CodeIgniter
    * Added progress functionality for ANDS GenericScript
    * Added config/nla to house all NLA related configuration

--- applications/ANDS/Commands/Script/GenericScript.php
+++ applications/ANDS/Commands/Script/GenericScript.php
@@ -4,6 +4,9 @@
 namespace ANDS\Commands\Script;
 
 
+use ANDS\Commands\ANDSCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\ProgressBar;
 use Symfony\Component\Console\Helper\Table;
 use Symfony\Component\Console\Input\Input;
 
@@ -17,7 +20,7 @@ class GenericScript implements GenericScriptRunnable
      * GenericScript constructor.
      * @param $command
      */
-    public function __construct($command, $input, $output)
+    public function __construct(ANDSCommand $command, $input, $output)
     {
         $this->command = $command;
         $this->input = $input;
@@ -30,6 +33,41 @@ class GenericScript implements GenericScriptRunnable
         $table->setHeaders($headers)
             ->setRows($rows)
             ->render();
+    }
+
+    public function progressStart($total)
+    {
+        $this->progress = new ProgressBar($this->output, $total);
+    }
+
+    public function progressAdvance($count)
+    {
+        $this->progress->advance();
+    }
+
+    public function progressFinish()
+    {
+        $this->progress->finish();
+        $this->log("\n");
+    }
+
+    public function info($message)
+    {
+        $this->command->log($message, "info");
+    }
+
+    public function error($message)
+    {
+        $this->log("\n");
+        $this->command->log($message, "error");
+        $this->log("\n");
+    }
+
+    public function debug($message)
+    {
+        if ($this->command->isDebug()) {
+            $this->command->log("[DEBUG] ". $message, "info");
+        }
     }
 
     public function log($message, $wrapper = null)
--- applications/ANDS/Helpers.php
+++ applications/ANDS/Helpers.php
@@ -19,3 +19,53 @@ if (!function_exists('request')) {
         return ANDS\Registry\API\Request::value($key, $default);
     }
 }
+
+/**
+ * Create URL Title
+ *
+ * Takes a "title" string as input and creates a
+ * human-friendly URL string with a "separator" string
+ * as the word separator.
+ *
+ * @access	public
+ * @param	string	the string
+ * @param	string	the separator
+ * @return	string
+ */
+if ( ! function_exists('url_title'))
+{
+    function url_title($str, $separator = '-', $lowercase = FALSE)
+    {
+        if ($separator == 'dash')
+        {
+            $separator = '-';
+        }
+        else if ($separator == 'underscore')
+        {
+            $separator = '_';
+        }
+
+        $q_separator = preg_quote($separator);
+
+        $trans = array(
+            '&.+?;'                 => '',
+            '[^a-z0-9 _-]'          => '',
+            '\s+'                   => $separator,
+            '('.$q_separator.')+'   => $separator
+        );
+
+        $str = strip_tags($str);
+
+        foreach ($trans as $key => $val)
+        {
+            $str = preg_replace("#".$key."#i", $val, $str);
+        }
+
+        if ($lowercase === TRUE)
+        {
+            $str = strtolower($str);
+        }
+
+        return trim($str, $separator);
+    }
+}
