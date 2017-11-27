594ea99292fc2716b40ae866dff0537718dc63f3
Elizabeth Margaret Woods <liz.woods@ands.org.au>
2017-11-27 13:09:09 +1100

    added service and method handler to output script to portal

--- applications/portal/registry_object/models/registry_objects.php
+++ applications/portal/registry_object/models/registry_objects.php
@@ -7,8 +7,7 @@
 class Registry_objects extends CI_Model {
 
     //array of properties required from the Registry point for RDA purpose
-    public $rdaProperties = array('core', 'descriptions', 'relationships', 'subjects', 'spatial', 'temporal','citations','dates','relatedInfo', 'identifiers','rights', 'contact','directaccess', 'suggest', 'logo', 'tags','existenceDates', 'identifiermatch', 'accessPolicy', 'grantsNetwork', 'connectiontrees');
-
+    public $rdaProperties = array('core', 'descriptions', 'relationships', 'subjects', 'spatial', 'temporal','citations','dates','relatedInfo', 'identifiers','rights', 'contact','directaccess', 'suggest', 'logo', 'tags','existenceDates', 'identifiermatch', 'accessPolicy', 'grantsNetwork', 'connectiontrees','jsonld'
 	/**
 	 * get an _ro by ID
 	 * @param  int $id registry object id
