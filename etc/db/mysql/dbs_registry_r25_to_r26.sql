7d57436a32464d791c9b79b7820e6d2bf56e2f16
Minh Duc Nguyen <minh.nguyen@ands.org.au>
2017-12-05 10:12:16 +1100

    Registry. Update db migration r25 to r26

--- etc/db/mysql/dbs_registry_r25_to_r26.sql
+++ etc/db/mysql/dbs_registry_r25_to_r26.sql
@@ -12,16 +12,17 @@ CREATE TABLE `orcid_records` (
   UNIQUE KEY `id_UNIQUE` (`orcid_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-CREATE TABLE `orcid_records` (
+CREATE TABLE `orcid_exports` (
+  `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
+  `registry_object_id` mediumint(8) unsigned NOT NULL,
   `orcid_id` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `full_name` varchar(256) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `record_data` text COLLATE utf8_unicode_ci,
+  `put_code` varchar(512) COLLATE utf8_unicode_ci NOT NULL,
+  `data` text COLLATE utf8_unicode_ci,
+  `response` text COLLATE utf8_unicode_ci,
   `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
   `updated_at` datetime DEFAULT CURRENT_TIMESTAMP,
-  `refresh_token` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `access_token` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
-  PRIMARY KEY (`orcid_id`),
-  UNIQUE KEY `id_UNIQUE` (`orcid_id`)
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 ALTER TABLE `dbs_registry`.`registry_object_identifier_relationships`
