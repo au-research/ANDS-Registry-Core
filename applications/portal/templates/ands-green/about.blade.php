8574e6fcfe21c4941a992105e67f24854faea107
Leo Monus <u4187959@ands3-new.anu.edu.au>
2018-02-27 14:47:44 +1100

    CC-2147

--- applications/portal/templates/ands-green/about.blade.php
+++ applications/portal/templates/ands-green/about.blade.php
@@ -155,7 +155,7 @@
                     </div>
                     <div class="row " data-os-animation="" data-os-animation-delay="">
                         <div class="col-md-4 ">
-                            <a href="http://www.ands.org.au/online-services/research-data-australia/collections-registry/providing-collection-descriptions" target="_blank">
+                            <a href="http://www.ands.org.au/online-services/research-data-australia/rda-registry/providing-metadata-records" target="_blank">
                                 <div class="element-medium-top element-medium-bottom text-center os-animation animated fadeInUp"
                                      data-os-animation="fadeInUp" data-os-animation-delay=".0s"
                                      style="-webkit-animation: 0s;">
