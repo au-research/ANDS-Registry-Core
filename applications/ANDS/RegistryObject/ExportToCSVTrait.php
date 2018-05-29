86344ffca9cfc5d1ccae04fec2b29ac27db84a45
Minh Duc Nguyen <minh.nguyen@ands.org.au>
2018-05-29 12:33:18 +1000

    CC-2168. Fixed export to `researchgraph` functionality to provide TYPE and fixed research graph id

--- applications/ANDS/RegistryObject/ExportToCSVTrait.php
+++ applications/ANDS/RegistryObject/ExportToCSVTrait.php
@@ -4,9 +4,68 @@
 namespace ANDS\RegistryObject;
 
 
+use ANDS\Registry\Providers\RIFCS\DatesProvider;
+
 trait ExportToCSVTrait
 {
-    public function toCSV()
+    public static $CSV_NEO_GRAPH = "graph";
+    public static $CSV_RESEARCH_GRAPH = "researchgraph";
+
+    public function toCSV($type = null)
+    {
+        switch ($type) {
+            case static::$CSV_RESEARCH_GRAPH:
+                return $this->researchGraph();
+            case static::$CSV_NEO_GRAPH:
+            default:
+                return $this->graph();
+        }
+    }
+
+    /**
+     * Exportable array for researchgraph
+     *
+     * @return array
+     */
+    private function researchGraph()
+    {
+        if ($this->type === "publication") {
+            $type = 'publication';
+        } elseif ($this->type === "person") {
+            $type = 'researcher';
+        } elseif ($this->type === 'grant') {
+            $type = 'grant';
+        } else {
+            $type = 'auxilary';
+        }
+
+        return [
+            'key:ID' => $this->getResearchGraphID(),
+            'source' => 'ands.org.au',
+            'local_id' => $this->key,
+            'title' => $this->sanitizeTitle($this->title),
+            'author_list' => '',
+            'last_updated' => $this->getRegistryObjectAttributeValue('updated'),
+            'publication_year' => DatesProvider::getPublicationDate($this),
+            'url' => $this->portal_url,
+            'type' => $type,
+            'ands_class' => $this->class,
+            'ands_type' => $this->type,
+            'ands_data_source_id' => $this->data_source_id
+        ];
+    }
+
+    public function getResearchGraphID()
+    {
+        return 'researchgraph.org/ands/'.$this->id;
+    }
+
+    /**
+     * Exportable array for standard internal neo4j consumption
+     *
+     * @return array
+     */
+    private function graph()
     {
         return [
             "roId:ID" => $this->id,
