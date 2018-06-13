467821d15122a34e6458076dc8eb9554e5e1643b
Minh Duc Nguyen <minh.nguyen@ands.org.au>
2018-06-13 15:49:18 +1000

    CC-2167. Updated wording

--- applications/portal/templates/ands-green/registry_object/contents/related-researchers.blade.php
+++ applications/portal/templates/ands-green/registry_object/contents/related-researchers.blade.php
@@ -1,5 +1,5 @@
 <div class="related-researchers">
-    <h4>Related Researchers</h4>
+    <h4>Related People</h4>
     <ul class="list-unstyled">
         @foreach($related['researchers']['docs'] as $col)
             <li>
@@ -14,7 +14,7 @@
             </li>
         @endforeach
         @if($related['researchers']['count'] > 5)
-            <li><a href="{{ $related['researchers']['searchUrl'] }}">View all {{ $related['researchers']['count'] }} related researchers</a></li>
+            <li><a href="{{ $related['researchers']['searchUrl'] }}">View all {{ $related['researchers']['count'] }} related people</a></li>
         @endif
     </ul>
 </div>
