0ce5828ea1edb68455354eaf1a3782904a087f45
Minh Duc Nguyen <minh.nguyen@ands.org.au>
2018-06-13 15:48:52 +1000

    CC-2167. Updated visuals on related object lists to include graphs and related researchers

--- applications/portal/templates/ands-green/registry_object/contents/related-objects-list.blade.php
+++ applications/portal/templates/ands-green/registry_object/contents/related-objects-list.blade.php
@@ -1,13 +1,48 @@
-<?php
-    $hasRelated = false;
-    foreach ($related as $rr) {
-        if ($rr != $related['researchers'] && sizeof($rr['docs']) > 0) $hasRelated = true;
-    }
-?>
-@if($hasRelated)
+@if(count($related) > 0)
     <div class="panel panel-primary element-no-top element-short-bottom panel-content">
-        <div class="panel-body swatch-white" >
-            <div id="graph-viz" style="height:450px;"></div>
+
+        <div class="swatch-white" style="position:relative;">
+            <div id="visualisation-overlay"></div>
+            <div id="graph-viz"></div>
+            <a href="" id="toggle-visualisation"><i class="fa fa-bars"></i></a>
+            <div id="visualisation-notice">Click to explore visualisation graph</div>
+        </div>
+
+        <div class="panel-body swatch-white" style="padding-top:0;">
+
+            {{--Related Publications--}}
+            @if (isset($related['publications']) && sizeof($related['publications']['docs']) > 0)
+                @include('registry_object/contents/related-publications')
+            @endif
+
+            @if (isset($related['data']) && sizeof($related['data']['docs']) > 0)
+                @include('registry_object/contents/related-data')
+            @endif
+
+            @if (isset($related['organisations']) && sizeof($related['organisations']['docs']) > 0)
+                @include('registry_object/contents/related-organisation')
+            @endif
+
+            @if (isset($related['researchers']) && sizeof($related['researchers']['docs']) > 0)
+                @include('registry_object/contents/related-researchers')
+            @endif
+
+            @if (isset($related['programs']) && sizeof($related['programs']['docs']) > 0)
+                @include('registry_object/contents/related-program')
+            @endif
+
+            @if (isset($related['grants_projects']) && sizeof($related['grants_projects']['docs']) > 0)
+                @include('registry_object/contents/related-grants_projects')
+            @endif
+
+            @if (isset($related['services']) && sizeof($related['services']['docs']) > 0)
+                @include('registry_object/contents/related-service')
+            @endif
+
+            @if (isset($related['websites']) && sizeof($related['websites']['docs']) > 0)
+                @include('registry_object/contents/related-website')
+            @endif
+
         </div>
     </div>
 
@@ -15,7 +50,7 @@
         function init() {
             var roID = $('#ro_id')[0].value;
 //            console.log(base_url + 'registry_object/graph/' + roID)
-            var neo4jd3 = new Neo4jd3('#graph-viz', {
+            window.neo4jd3 = new Neo4jd3('#graph-viz', {
                 icons: {
                     'collection': 'folder-open',
                     'activity' : 'flask',
@@ -57,38 +92,4 @@
 
         window.onload = init;
     </script>
-
-    <div class="panel panel-primary element-no-top element-short-bottom panel-content">
-        <div class="panel-body swatch-white">
-            {{--Related Publications--}}
-            @if (isset($related['publications']) && sizeof($related['publications']['docs']) > 0)
-                @include('registry_object/contents/related-publications')
-            @endif
-
-            @if (isset($related['data']) && sizeof($related['data']['docs']) > 0)
-                @include('registry_object/contents/related-data')
-            @endif
-
-            @if (isset($related['organisations']) && sizeof($related['organisations']['docs']) > 0)
-                @include('registry_object/contents/related-organisation')
-            @endif
-
-            @if (isset($related['programs']) && sizeof($related['programs']['docs']) > 0)
-                @include('registry_object/contents/related-program')
-            @endif
-
-            @if (isset($related['grants_projects']) && sizeof($related['grants_projects']['docs']) > 0)
-                @include('registry_object/contents/related-grants_projects')
-            @endif
-
-            @if (isset($related['services']) && sizeof($related['services']['docs']) > 0)
-                @include('registry_object/contents/related-service')
-            @endif
-
-            @if (isset($related['websites']) && sizeof($related['websites']['docs']) > 0)
-                @include('registry_object/contents/related-website')
-            @endif
-
-        </div>
-    </div>
 @endif
