c9dc8b5c41e9cc58e4f46a3c6f6ea544f4ae3234
Minh Duc Nguyen <minh.nguyen@ands.org.au>
2018-06-19 12:43:03 +1000

    CC-2167. Added plural names to cluster nodes

--- applications/ANDS/Util/StrUtil.php
+++ applications/ANDS/Util/StrUtil.php
@@ -38,6 +38,51 @@ class StrUtil
         return str_replace(self::stopWords, "", $str);
     }
 
+    /**
+     * Takes a singular word and makes it plural
+     *
+     * @param $str
+     * @param bool $force
+     * @return mixed|string
+     */
+    public static function plural($str, $force = FALSE)
+    {
+        $result = strval($str);
+
+        $plural_rules = array(
+            '/^(ox)$/'                 => '\1\2en',     // ox
+            '/([m|l])ouse$/'           => '\1ice',      // mouse, louse
+            '/(matr|vert|ind)ix|ex$/'  => '\1ices',     // matrix, vertex, index
+            '/(x|ch|ss|sh)$/'          => '\1es',       // search, switch, fix, box, process, address
+            '/([^aeiouy]|qu)y$/'       => '\1ies',      // query, ability, agency
+            '/(hive)$/'                => '\1s',        // archive, hive
+            '/(?:([^f])fe|([lr])f)$/'  => '\1\2ves',    // half, safe, wife
+            '/sis$/'                   => 'ses',        // basis, diagnosis
+            '/([ti])um$/'              => '\1a',        // datum, medium
+            '/(p)erson$/'              => '\1eople',    // person, salesperson
+            '/(m)an$/'                 => '\1en',       // man, woman, spokesman
+            '/(c)hild$/'               => '\1hildren',  // child
+            '/(buffal|tomat)o$/'       => '\1\2oes',    // buffalo, tomato
+            '/(bu|campu)s$/'           => '\1\2ses',    // bus, campus
+            '/(alias|status|virus)/'   => '\1es',       // alias
+            '/(octop)us$/'             => '\1i',        // octopus
+            '/(ax|cris|test)is$/'      => '\1es',       // axis, crisis
+            '/s$/'                     => 's',          // no change (compatibility)
+            '/$/'                      => 's',
+        );
+
+        foreach ($plural_rules as $rule => $replacement)
+        {
+            if (preg_match($rule, $result))
+            {
+                $result = preg_replace($rule, $replacement, $result);
+                break;
+            }
+        }
+
+        return $result;
+    }
+
     /* From Joel Benn, 2014 */
     const stopWords = [
         ' a ',
