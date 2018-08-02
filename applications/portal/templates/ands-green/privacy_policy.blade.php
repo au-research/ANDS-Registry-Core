74bd78f6ac5d908c33bf831a70ffebcf6e6052e6
Liz Woods <liz.woods@ands.org.au>
2018-08-02 12:13:28 +1000

    amended the privacy policy text CC-2248

--- applications/portal/templates/ands-green/privacy_policy.blade.php
+++ applications/portal/templates/ands-green/privacy_policy.blade.php
@@ -8,21 +8,67 @@
                    <header class="text-center element-normal-top element-medium-bottom not-condensed os-animation animated fadeInUp" data-os-animation="fadeInUp" data-os-animation-delay="0s" style="-webkit-animation: 0s;">
                        <h1 class="bigger hairline bordered bordered-normal os-animation animated fadeIn" data-os-animation="fadeIn" data-os-animation-delay="0s" style="-webkit-animation: 0s;">Privacy Policy</h1>
                    </header>
-                   <p>This following document sets forth the Privacy Policy for the ANDS website Research Data Australia, <a href="http://researchdata.ands.org.au">http://researchdata.ands.org.au</a>.</p>
-                   <p>ANDS is bound by the <a href="http://www.comlaw.gov.au/Details/C2013C00482">Privacy Act 1988</a> (Privacy Act), which sets out a number of principles concerning the privacy of individuals.</p>
+                   <p>
+                       The following document sets forth the Privacy Policy for the ANDS website Research Data Australia,
+                       <a href="http://researchdata.ands.org.au">http://researchdata.ands.org.au</a>.
+                   </p>
+
+                   <p>
+                       ANDS is bound by the <a href="http://www.comlaw.gov.au/Details/C2013C00482">Privacy Act 1988</a> (Privacy Act),
+                       which sets out a number of principles concerning the privacy of individuals.
+                   </p>
+
                    <h3>Non-personally identifiable information</h3>
-                   <p>For each visitor to reach the site, we expressively collect the following non-personally identifiable information, including but not limited to browser type, version and language, operating system, pages viewed while browsing the Site, page access times and referring website address. This collected information is used solely internally for the purpose of gauging visitor traffic, trends and delivering personalized content to you while you are at this site.</p>
-                   <p>From time to time, we may use customer information for new, unanticipated uses not previously disclosed in our privacy notice. If our information practices change at some time in the future we will use for these new purposes only, data collected from the time of the policy change forward will adhere to our updated practices.</p>
+                   <p>
+                       For each visitor to reach the site, we collect non-personally identifiable information, including
+                       but not limited to browser type, version and language, operating system, pages viewed while
+                       browsing the site, page access times, search terms used and referring website address.
+                   </p>
+                   <h3>Use of non-personally identifiable information</h3>
+                   <p>
+                       The non-personally identifiable information collected will be used for the purpose of gauging
+                       visitor traffic, trends analysis and delivering personalised content to you while you are at the
+                       site.  We will also use the aggregated data, in its entirety or in a time-based scale, to provide
+                       statistics to our users and our partners. These statistics can either be displayed in Research
+                       Data Australia (e.g. views, access) or as a report shared with our partners and stakeholders.
+                   </p>
                    <h3>Collection of your personal information</h3>
-                   <p>There are many aspects of the site which can be viewed without providing personal information, however, for access to certain features you are required to login via supported 3rd party authorisation providers.  Upon your approval, these 3rd party providers share profile information about you with ANDS and Research Data Australia. From this information ANDS will collect and store your name, unique provider ID and if available a link to your profile image.</p>
+                   <p>There are many aspects of the site which can be viewed without providing personal information,
+                       however, for access to certain features you are required to login via supported 3rd party
+                       authorisation providers. Upon your approval, these 3rd party providers share profile information
+                       about you with ANDS and Research Data Australia. From this information ANDS will collect and
+                       store your name, unique provider ID and if available a link to your profile image.
+                   </p>
+                   <p>
+                       From time to time, we may use customer information for new, unanticipated uses not previously
+                       disclosed in our privacy notice. If our information practices change at some time in the future,
+                       we will update this policy, and  only data collected from the time of the policy change will be
+                       used  for those new purposes.
+                   </p>
                    <h3>Sharing of your personal information</h3>
-                   <p>ANDS will not disclose or on sell any personal information obtained through your login and use of Research Data Australia. Data collected will only be used in the context of your logged in Research Data Australia session and to improve our website and services.</p>
+                   <p>
+                       ANDS will not disclose or on sell any personal information obtained through your login and use of
+                       Research Data Australia. Data collected will only be used in the context of your logged in
+                       Research Data Australia session and to improve our website and services.
+                   </p>
                    <h3>Changes to this Privacy Policy</h3>
-                   <p>ANDS reserves the right to make amendments to this Privacy Policy at any time. If you have objections to the Privacy Policy, you should not access or use the site.</p>
+                   <p>
+                       ANDS reserves the right to make amendments to this Privacy Policy at any time. If you have
+                       objections to the Privacy Policy, you should not access or use the site.
+
+                   </p>
                    <h3>Accessing Your Personal Information</h3>
-                   <p>You have a right to access your personal information, subject to exceptions allowed by law. If you would like to do so, please let us know. You may be required to put your request in writing for security reasons.</p>
+                   <p>
+                       You have a right to access your personal information, subject to exceptions allowed by law. If
+                       you would like to do so, please let us know. You may be required to put your request in writing
+                       for security reasons.
+
+                   </p>
                    <h3>Contacting us</h3>
-                   <p>ANDS welcomes your comments regarding this Privacy Policy. If you have any questions or concerns about this Privacy Policy and would like further information, please contact us by any of the following means during business hours Monday to Friday.</p>
+                   <p>ANDS welcomes your comments regarding this Privacy Policy. If you have any questions or concerns
+                       about this Privacy Policy and would like further information, please contact us by any of the
+                       following means during business hours Monday to Friday.
+                   </p>
                    <p>
                        ANDS Office<br/>
                        Monash University,<br/>
