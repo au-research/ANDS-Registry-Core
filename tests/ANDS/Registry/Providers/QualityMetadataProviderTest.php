13a8ba6a7075d1b67ff603c4cfcf19005ef71f63
Minh Duc Nguyen <minh.nguyen@ands.org.au>
2018-08-13 10:20:01 +1000

    CC-2015. Validates records without title and description on collections

---
+++ tests/ANDS/Registry/Providers/QualityMetadataProviderTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace ANDS\Registry\Providers;
+
+
+use ANDS\File\Storage;
+
+class QualityMetadataProviderTest extends \RegistryTestClass
+{
+    /** @test
+     * @throws \Exception
+     */
+    function it_validates_a_well_described_record()
+    {
+        $xml = Storage::disk('test')->get('rifcs/collection_all_elements.xml');
+        $actual = QualityMetadataProvider::validate($xml);
+        $this->assertTrue($actual);
+    }
+
+    /** @test
+     * @throws \Exception
+     */
+    public function validates_records_without_title()
+    {
+        $this->setExpectedException('InvalidArgumentException');
+        $xml = Storage::disk('test')->get('rifcs/collection_no_title.xml');
+        QualityMetadataProvider::validate($xml);
+    }
+
+    /** @test
+     * @throws \Exception
+     */
+    function validates_collections_without_description()
+    {
+        $this->setExpectedException('InvalidArgumentException');
+        $xml = Storage::disk('test')->get('rifcs/collection_no_description.xml');
+        QualityMetadataProvider::validate($xml);
+    }
+
+    /** @test
+     * @throws \Exception
+     */
+    function it_validates_non_collection_without_description()
+    {
+        $xml = Storage::disk('test')->get('rifcs/party_no_description.xml');
+        $this->assertTrue(QualityMetadataProvider::validate($xml));
+    }
+}
