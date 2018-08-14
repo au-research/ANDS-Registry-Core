9c227e5ab6286e5f98c1db5ceacba10900d0affa
Minh Duc Nguyen <minh.nguyen@ands.org.au>
2018-08-14 11:12:33 +1000

    CC-2251. Removed links to data source reports page

--- applications/registry/data_source/views/detailed_quality_report.php
+++ applications/registry/data_source/views/detailed_quality_report.php
@@ -12,7 +12,6 @@
 		<ul class="nav nav-pills">
 			<li class=""><?php echo anchor('data_source/manage#!/view/'.$ds->id,'Dashboard');?></li>
 			<li class=""><?php echo anchor('data_source/manage_records/'.$ds->id,'Manage Records');?></li>
-			<li class="active"><?php echo anchor('data_source/report/'.$ds->id,'Reports');?></li>
 			<li class=""><?php echo anchor('data_source/manage#!/settings/'.$ds->id,'Settings');?></li>
 		</ul>
 	</div>
