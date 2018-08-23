9a5ec8b7ed4af285cd4d243fa5a7602ae0bca6a5
Liz Woods <liz.woods@ands.org.au>
2018-08-23 10:28:16 +1000

    update merge function to set merged test account to inactive and added status attribute to doi client CC-2263

--- etc/db/mysql/dbs_dois_r28.1_to_r29_incr.sql
+++ etc/db/mysql/dbs_dois_r28.1_to_r29_incr.sql
@@ -3,3 +3,6 @@ ADD COLUMN `test_app_id` VARCHAR(255) CHARACTER SET 'utf8' NULL AFTER `shared_se
 
 ALTER TABLE `dbs_dois`.`doi_client`
 ADD COLUMN `test_shared_secret` VARCHAR(64) CHARACTER SET 'utf8' NULL AFTER `test_app_id`;
+
+ALTER TABLE `dbs_dois`.`doi_client`
+ADD COLUMN `status` VARCHAR(45) NULL DEFAULT 'ACTIVE' AFTER `test_shared_secret`;
