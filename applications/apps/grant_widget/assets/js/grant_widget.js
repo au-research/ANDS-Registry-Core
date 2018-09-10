7a44f09806550da222266bdedcc6499a8568df50
Liz Woods <liz.woods@ands.org.au>
2018-09-10 11:29:47 +1000

    updated the grant widget to allow for a custom function to be called on a successful hit CC-2259

--- applications/apps/grant_widget/assets/js/grant_widget.js
+++ applications/apps/grant_widget/assets/js/grant_widget.js
@@ -68,6 +68,7 @@
 		    lookup_error_handler: false,
 		    lookup_success_handler: false,
 		    post_lookup_success_handler: false,
+			custom_select_handler:false,
 
 		    //auto close the search box once a value is chosen
 		    auto_close_search: true,
@@ -480,14 +481,18 @@
 							$('.grant_search_result', p).html(html);
 						}
 					}
-					$('.select_grant_search_result', p).on('click', function(){
-						obj.val($(this).attr('grant-id'));
-						_lookup(obj, settings);
-						if(settings.auto_close_search) $('#'+settings._wid).slideUp();
+					if(settings.custom_select_handler && (typeof settings.custom_select_handler === 'function')){
+						settings.custom_select_handler(data, obj, settings);
+					}else {
+						$('.select_grant_search_result', p).on('click', function () {
+							obj.val($(this).attr('grant-id'));
+							_lookup(obj, settings);
+							if (settings.auto_close_search) $('#' + settings._wid).slideUp();
 
-                        $(document.body).scrollTop($('#return'+settings._wid).offset().top -100);
+							$(document.body).scrollTop($('#return' + settings._wid).offset().top - 100);
 
-					});
+						});
+					}
 					if(settings.tooltip){
 						$('.preview').each(function(){
    								$(this).qtip({
