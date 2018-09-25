2ffc577de671cc7c62fcc889ccd03a9977366b13
Minh Duc Nguyen <minh.nguyen@ands.org.au>
2018-09-25 10:52:43 +1000

    Registry. Refactored AAFRapidConnectAuthenticator

---
+++ applications/ANDS/Util/URLUtil.php
@@ -0,0 +1,34 @@
+<?php
+
+
+namespace ANDS\Util;
+
+
+class URLUtil
+{
+
+    /**
+     * @param string $input A base64 encoded string
+     *
+     * @return string A decoded string
+     */
+    public static function urlsafeB64Decode($input)
+    {
+        $remainder = strlen($input) % 4;
+        if ($remainder) {
+            $padlen = 4 - $remainder;
+            $input .= str_repeat('=', $padlen);
+        }
+        return base64_decode(strtr($input, '-_', '+/'));
+    }
+
+    /**
+     * @param string $input Anything really
+     *
+     * @return string The base64 encode of what you passed in
+     */
+    public static function urlsafeB64Encode($input)
+    {
+        return str_replace('=', '', strtr(base64_encode($input), '+/', '-_'));
+    }
+}
