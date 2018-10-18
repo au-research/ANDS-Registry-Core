a458a5a7f74ffc1f1600955d4f1c080e7b1cb42c
Minh Duc Nguyen <minh.nguyen@ands.org.au>
2018-10-19 10:39:35 +1100

    Added records controller endpoint for scholix and dci

--- applications/ANDS/Registry/API/Controller/RecordsMiscController.php
+++ applications/ANDS/Registry/API/Controller/RecordsMiscController.php
@@ -2,6 +2,8 @@
 namespace ANDS\Registry\API\Controller;
 
 
+use ANDS\Registry\API\Request;
+use ANDS\Registry\Providers\DCI\DataCitationIndexProvider;
 use ANDS\Registry\Providers\ORCID\ORCIDProvider;
 use ANDS\Registry\Providers\ORCID\ORCIDRecord;
 use ANDS\Repository\RegistryObjectsRepository;
@@ -19,6 +21,33 @@ class RecordsMiscController
         $record = RegistryObjectsRepository::getRecordByID($id);
         $xml = ORCIDProvider::getORCIDXML($record, new ORCIDRecord());
         $this->printXML($xml);
+    }
+
+    public function scholix($id)
+    {
+        $record = RegistryObjectsRepository::getRecordByID($id);
+        $scholix = \ANDS\Registry\Providers\ScholixProvider::get($record);
+
+        $wt = Request::get('wt', 'json');
+        switch ($wt) {
+            case "json":
+                return $scholix->toJson();
+            case "xml":
+                $this->printXML($scholix->toXML());
+//                return $scholix->toXML();
+            case "oai":
+                $this->printXML($scholix->toOAI());
+//                return $scholix->toOAI();
+            default:
+                return $scholix->toArray();
+        }
+    }
+
+    public function dci($id)
+    {
+        $record = RegistryObjectsRepository::getRecordByID($id);
+        $dci = DataCitationIndexProvider::get($record);
+        $this->printXML($dci);
     }
 
     public function orcidValidate($id)
