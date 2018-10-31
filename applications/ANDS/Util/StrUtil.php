1c75b032afd8da2804bb65f757ae51f2843601ab
Liz Woods <liz.woods@ands.org.au>
2018-11-01 09:36:23 +1100

    added software icon to graph node and relation text and changed data to software for preview popup

--- applications/ANDS/Util/StrUtil.php
+++ applications/ANDS/Util/StrUtil.php
@@ -18,7 +18,11 @@ class StrUtil
     public static function getIconFor($class, $type = null)
     {
         if ($class === 'collection') {
-            return "fa-folder-open";
+            if($type === 'software'){
+                return "fa-file-code-o";
+            }else {
+                return "fa-folder-open";
+            }
         }
 
         if ($class === "activity") {
