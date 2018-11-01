00e7bb43c36e8e0bd1b7c2b8ad232945f5c53c0b
Minh Duc Nguyen <minh.nguyen@ands.org.au>
2018-11-01 11:03:03 +1100

    Registry. Fixed an issue where file caching do not honor the config set out in .env. Updated Config class to have static caching as to not load a config file multiple time

--- applications/ANDS/Util/Config.php
+++ applications/ANDS/Util/Config.php
@@ -7,6 +7,13 @@ namespace ANDS\Util;
 class Config
 {
     /**
+     * Cached data.
+     *
+     * @var array
+     */
+    protected static $data = [];
+
+    /**
      * Get a configuration
      * Usage: Config::get('database'), Config::get('database.registry')
      *
@@ -20,7 +27,6 @@ class Config
         if (strpos($name, ".") > 0) {
             $parts = explode('.', $name);
             $name = $parts[0];
-            $config = $parts[1];
         }
 
         // name should now be the config name
@@ -31,18 +37,37 @@ class Config
             return null;
         }
 
-        $configuration = include(dirname(__DIR__) . "/../../config/$name.php");
+        // check if the config is already statically cached during run time
+        if (!array_key_exists($name, self::$data)) {
+            self::load($name);
+        }
 
+        // fetch the configuration that is already loaded (should)
+        $configuration = self::$data[$name];
+
+        // dot notation
+        // TODO cater for multiple dot app.config.cache.something
         if ($config && array_key_exists($config, $configuration)) {
             return $configuration[$config];
         }
 
         return $configuration;
     }
+
+    /**
+     * Load the configuration file into static caching
+     *
+     * @param $name
+     */
+    public static function load($name)
+    {
+        $configuration = include(dirname(__DIR__) . "/../../config/{$name}.php");
+        self::$data[$name] = $configuration;
+    }
+
 }
 
-if (!function_exists('config'))
-{
+if (!function_exists('config')) {
     /**
      * Helper function for Config class
      * Usage: config('database')
@@ -50,7 +75,8 @@ if (!function_exists('config'))
      * @param $name
      * @return mixed
      */
-    function config($name) {
+    function config($name)
+    {
         return Config::get($name);
     }
 }
