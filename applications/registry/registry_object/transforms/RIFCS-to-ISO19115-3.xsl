935d028aa5f5a4a3e9484ae1d7e79f481caba709
Leo Monus <u4187959@ands-devl.anu.edu.au>
2019-01-15 16:16:09 +1100

    ISO19115-3 provider

--- applications/registry/registry_object/transforms/RIFCS-to-ISO19115-3.xsl
+++ applications/registry/registry_object/transforms/RIFCS-to-ISO19115-3.xsl
@@ -1,5 +1,5 @@
 <?xml version="1.0"?>
-<xsl:stylesheet version="2.0"
+<xsl:stylesheet version="1.0"
                 xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
                 xmlns:ro="http://ands.org.au/standards/rif-cs/registryObjects"
                 xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
---
+++ etc/db/mysql/dbs_registry_r30_to_r30.1_incr.sql
@@ -0,0 +1,17 @@
+USE dbs_registry;
+
+
+CREATE TABLE `alt_schema_versions` (
+  `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
+  `registry_object_id` mediumint(8) NOT NULL,
+  `registry_object_key` varchar(255) NOT NULL,
+  `registry_object_data_source_id` mediumint(8) NOT NULL,
+  `schema` varchar(45) NOT NULL,
+  `data` blob NOT NULL,
+  `hash` varchar(45) NOT NULL,
+  `created_at` datetime DEFAULT NOW(),
+  `updated_at` datetime DEFAULT NOW(),
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  INDEX (`registry_object_id`, `registry_object_key`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
