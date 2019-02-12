977c4716e534b404bd7438b7d9cfe41cf336e9bd
Leo Monus <u4187959@ands-devl.anu.edu.au>
2019-02-13 10:02:52 +1100

    missing files

--- config/pids.php
+++ config/pids.php
@@ -3,6 +3,6 @@
 return [
     'identifier_suffix' => env('PIDS_IDENTIFIER_SUFFIX', 'https://researchdata.ands.org.au'),
     'server_base_url'=>  env('PIDS_SERVER_BASE_URL', "https://handle.ands.org.au/pids/"),
-    'server_app_id'=> env('PIDS_SERVER_APP_ID', "b50d89cbeeabb59c9679271b06fd5ba44234feb"),
+    'server_app_id'=> env('PIDS_SERVER_APP_ID', null),
     'url_prefix'=> env('PIDS_URL_PREFIX', "https://handle.ands.org.au/pids")
 ];
