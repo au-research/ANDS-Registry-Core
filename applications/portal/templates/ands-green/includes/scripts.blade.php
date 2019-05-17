6468580272d0d30c1b74a6b731f0069a12f249d9
Liz Woods <lwoods@andss-mbp.uds.anu.edu.au>
2019-05-17 12:37:04 +1000

    another attempt to delay load of javascript to imporve performance CC 2510

--- applications/portal/templates/ands-green/includes/scripts.blade.php
+++ applications/portal/templates/ands-green/includes/scripts.blade.php
@@ -38,7 +38,28 @@
     <script src="{{ dist_url('lib.js') }}"></script>
 @endif
 
-
+@if(isset($lib))
+	@foreach($lib as $l)
+		@if($l=='jquery-ui')
+			<script type="text/javascript" src="{{asset_url('vendor/jquery-ui/jquery-ui.js', 'core')}}"></script>
+		@elseif($l=='dynatree')
+			<script type="text/javascript" src="{{asset_url('vendor/dynatree/dist/jquery.dynatree.js', 'core')}}"></script>
+        @elseif($l=='textAngular')
+        	<link rel='stylesheet' href="{{asset_url('vendor/textAngular/src/textAngular.css', 'core')}}">
+            <script src="{{asset_url('vendor/textAngular/dist/textAngular-rangy.min.js', 'core')}}"></script>
+            <script src="{{asset_url('vendor/textAngular/dist/textAngular-sanitize.min.js', 'core')}}"></script>
+            <script src="{{asset_url('vendor/textAngular/dist/textAngular.min.js', 'core')}}"></script>
+        @elseif($l=='colorbox')
+            <script src="{{asset_url('vendor/colorbox/jquery.colorbox-min.js', 'core')}}"></script>
+        @elseif($l=='mustache')
+            <script src="{{asset_url('vendor/mustache/mustache.min.js', 'core')}}"></script>
+        @elseif($l=='map')
+            <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?{{ \ANDS\Util\Config::get('app.google_api_key') ? 'key='.\ANDS\Util\Config::get('app.google_api_key') : ''  }}&libraries=drawing&amp;sensor=false"></script>
+        @elseif($l=='ngupload')
+            <script type="text/javascript" src="{{asset_url('vendor/ng-file-upload/angular-file-upload-all.min.js','core')}}"></script>
+        @endif
+	@endforeach
+@endif
 
 @if(is_dev())
     <script src="{{ dist_url('portal_lib.js') }}"></script>
@@ -46,7 +67,17 @@
     <script src="{{ dist_url('portal_lib.js') }}"></script>
 @endif
 
+<script type="text/javascript" src="https://jira.ands.org.au/s/d41d8cd98f00b204e9800998ecf8427e/en_AUc8oc9c-1988229788/6265/77/1.4.7/_/download/batch/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector.js?collectorId=d9610dcf"></script>
 
+<script>
+    _.contains = _.includes;
+</script>
+
+@if(isset($scripts))
+	@foreach($scripts as $script)
+		<script src="{{asset_url('js/'.$script.'.js')}}"></script>
+	@endforeach
+@endif
 
 <?php if(isset($ro->jsonld[0])){
     print('<script type="application/ld+json">'.$ro->jsonld[0].'</script>');
