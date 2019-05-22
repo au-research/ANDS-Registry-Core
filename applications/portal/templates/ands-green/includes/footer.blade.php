43d400dd4c4abd8637868022fd040a7d3436575a
Leo Monus <u4187959@ands-devl.anu.edu.au>
2019-05-22 14:21:10 +1000

    logo changed to ARDC

--- applications/portal/templates/ands-green/includes/footer.blade.php
+++ applications/portal/templates/ands-green/includes/footer.blade.php
@@ -10,11 +10,12 @@
                     </p>
                 </div>
                 <div class="col-md-2">
-                   <a href="https://education.gov.au/national-collaborative-research-infrastructure-strategy-ncris" target="_blank" class="gov_logo"><img style="height:75px;" src="<?php echo asset_url('images/NCRIS_PROVIDER_rev.png','core');?>" alt="National Collaborative Research Infrastructure Strategy (NCRIS)" /></a>
+                    <a href="https://www.ardc.edu.au/" class="footer_logo"><img src="{{ asset_url(\ANDS\Util\config::get('app.environment_logo'), 'base')}}" alt=""/></a>
                 </div>
                 <div class="col-md-2">
-                    <a href="http://www.ands.org.au/" class="footer_logo"><img src="{{asset_url('images/footer_logo_rev.png', 'core')}}" alt="" style="height:75px;"/></a>
+                   <a href="https://education.gov.au/national-collaborative-research-infrastructure-strategy-ncris" target="_blank" class="gov_logo"><img src="<?php echo asset_url('images/NCRIS_PROVIDER_rev.png','core');?>" alt="National Collaborative Research Infrastructure Strategy (NCRIS)" /></a>
                 </div>
+
             </div>
             <div class="row element-normal-top element-normal-bottom">
                 <div class="col-md-3">
