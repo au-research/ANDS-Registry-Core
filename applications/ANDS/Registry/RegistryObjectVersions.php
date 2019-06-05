ba7d66af2292fa3d716da7fa3485de12adf91e7a
Minh Duc Nguyen <minh.nguyen@ands.org.au>
2019-06-05 11:06:16 +1000

    Fixed an issue where the registry object view page often takes an extended amount of time Often on the first load, subsequent load are fast (because the db query is cached). This fix is a db query optimization

---
+++ applications/ANDS/Registry/RegistryObjectVersions.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace ANDS\Registry;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class RegistryObjectVersions extends Model
+{
+    protected $table = "registry_object_versions";
+    public $timestamps = false;
+}
