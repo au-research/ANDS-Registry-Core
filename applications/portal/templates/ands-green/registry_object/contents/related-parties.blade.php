56d1fe8c0477f5fe50f59951aebdbd544aa5cb06
Liz Woods <lwoods@andss-mbp.uds.anu.edu.au>
2019-09-26 10:29:50 +1000

    detect duplicate related parties by to_title and display the one that is from the same group if there is one, else display the first one CC-2595. Remove contributor relationship from title bar CC-2647

--- applications/portal/templates/ands-green/registry_object/contents/related-parties.blade.php
+++ applications/portal/templates/ands-green/registry_object/contents/related-parties.blade.php
@@ -2,22 +2,83 @@
     <?php
     $contributors = '';
     ?>
-    @if($ro->citations)
+   @if($ro->citations)
         @foreach($ro->citations as $citation)
             @if($citation['type']=='metadata' && $citation['contributors']!='')
              <?php  $contributors = explode(";",$citation['contributors']); ?>
             @endif
         @endforeach
 
+
     @endif
+    <?php
+    /*
+    Set up an array which detects how many times a related party is linked.
+    Currently this is based on exact match of to_title as we do not index identifiers of the related parties
+    This is needed to avoid displaying the same party multiple times when aggregators
+    have registered the same party maultiple times as separate objects
+    */
+
+    foreach($related['organisations']['docs'] as $col){
+        $arrayNames[] = $col['to_title'];
+    }
+    foreach($related['researchers']['docs'] as $col){
+        $arrayNames[] = $col['to_title'];
+    }
+    //count each to_title occurrence
+    $multi = array_count_values($arrayNames);
+    ?>
+
     @if($contributors!='')
-        <span id="contributors">
+       <?php  $contributorString = ''; ?>
         @foreach($contributors as $contributor)
-            {{$contributor}} <small>( Contributor )</small>
+            <?php $contributorString .= ' <span id="contributor">'.$contributor."</span>, "; ?>
         @endforeach
-        </span>
+            <?php $contributorString = trim($contributorString, ", ") ;?>
+            {{$contributorString}}
+
     @elseif (is_array($related['researchers']) && sizeof($related['researchers']['docs']) > 0)
         @foreach($related['researchers']['docs'] as $col)
+            <?php
+            // we need to detect if the related party occurs more than once,
+            // and if so let's check if one of those occurrences has the same group as the current object's
+            ?>
+            @if ($multi[$col['to_title']]>1 AND $multi[$col['to_title']] != 'found')
+                <?php
+                $same_group_found = false;
+                //now loop through all the organisations to get the one from the same group
+                // if it exists, else just out put the first one
+                foreach($related['researchers']['docs'] as $col2){
+                if($col2['to_title'] == $col['to_title'] && $col2['to_group'] == $col['from_group']){
+                $same_group_found = true;
+                ?>
+                <a href="<?php echo base_url()?>{{$col2['to_slug']}}/{{$col2['to_id']}}"
+                   tip="{{ $col2['display_description'] }}"
+                   class="ro_preview"
+                   ro_id="{{$col2['to_id']}}"
+                   style="margin-right:5px;">
+                    {{ $col2['to_title'] }}
+                    <small>({{ $col2['display_relationship'] }})</small>
+                </a>
+                <?php
+                }
+                }
+                //if we have relationships to duplicate party but we can't find one from the same group, then just output the first one
+                if(!$same_group_found){ ?>
+                <a href="<?php echo base_url()?>{{$col['to_slug']}}/{{$col['to_id']}}"
+                   tip="{{ $col['display_description'] }}"
+                   class="ro_preview"
+                   ro_id="{{$col['to_id']}}"
+                   style="margin-right:5px;">
+                    {{ $col['to_title'] }}
+                    <small>({{ $col['display_relationship'] }})</small>
+                </a>
+                <?php
+                }
+                $multi[$col['to_title']] = 'found';
+                ?>
+            @elseif($multi[$col['to_title']] != 'found')
+
             <a href="<?php echo base_url()?>{{$col['to_slug']}}/{{$col['to_id']}}"
                tip="{{ $col['display_description'] }}"
                class="ro_preview"
@@ -30,12 +91,52 @@
                  {{ $col['to_title'] }}
                 <small>({{ $col['display_relationship'] }})</small>
             </a>
+            @endif
         @endforeach
         @if($related['researchers']['count'] > 5)
             <a href="{{ $related['researchers']['searchUrl'] }}">View all {{ $related['researchers']['count'] }} related researchers</a>
         @endif
     @elseif(is_array($related['organisations']) && sizeof($related['organisations']['docs']) > 0)
         @foreach($related['organisations']['docs'] as $col)
+            <?php
+            // we need to detect if the related party occurs more than once,
+            // and if so let's check if one of those occurrences has the same group as the current object's
+            ?>
+            @if ($multi[$col['to_title']]>1 AND $multi[$col['to_title']] != 'found')
+                <?php
+                $same_group_found = false;
+                //now loop through all the organisations to get the one from the same group
+                // if it exists, else just out put the first one
+                foreach($related['organisations']['docs'] as $col2){
+                    if($col2['to_title'] == $col['to_title'] && $col2['to_group'] == $col['from_group']){
+                        $same_group_found = true;
+                        ?>
+                        <a href="<?php echo base_url()?>{{$col2['to_slug']}}/{{$col2['to_id']}}"
+                           tip="{{ $col2['display_description'] }}"
+                           class="ro_preview"
+                           ro_id="{{$col2['to_id']}}"
+                           style="margin-right:5px;">
+                            {{ $col2['to_title'] }}
+                            <small>({{ $col2['display_relationship'] }})</small>
+                        </a>
+                        <?php
+                    }
+                }
+               //if we have relationships to duplicate party but we can't find one from the same group, then just output the first one
+                if(!$same_group_found){ ?>
+                         <a href="<?php echo base_url()?>{{$col['to_slug']}}/{{$col['to_id']}}"
+                       tip="{{ $col['display_description'] }}"
+                       class="ro_preview"
+                       ro_id="{{$col['to_id']}}"
+                       style="margin-right:5px;">
+                         {{ $col['to_title'] }}
+                        <small>({{ $col['display_relationship'] }})</small>
+                        </a>
+                       <?php
+                }
+                $multi[$col['to_title']] = 'found';
+                ?>
+            @elseif($multi[$col['to_title']] != 'found')
             <a href="<?php echo base_url()?>{{$col['to_slug']}}/{{$col['to_id']}}"
                tip="{{ $col['display_description'] }}"
                class="ro_preview"
@@ -44,6 +145,7 @@
                  {{ $col['to_title'] }}
                 <small>({{ $col['display_relationship'] }})</small>
             </a>
+            @endif
         @endforeach
         @if($related['organisations']['count'] > 5)
             <a href="{{ $related['organisations']['searchUrl'] }}">View all {{ $related['organisations']['count'] }} related organisations</a>
