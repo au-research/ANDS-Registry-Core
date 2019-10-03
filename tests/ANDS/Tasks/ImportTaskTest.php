61e39d644ed578d6ebc1671ccac372d9f23851f3
Leo Monus <leo.monus@anu.edu.au>
2019-10-03 15:49:41 +1000

    testless test will give a warning

--- tests/ANDS/Tasks/ImportTaskTest.php
+++ tests/ANDS/Tasks/ImportTaskTest.php
@@ -6,7 +6,17 @@ use ANDS\API\Task\ImportTask;
 
 class ImportTaskTest extends \RegistryTestClass
 {
-    /** @not_test */
+
+    /** @test */
+    function test_for_the_sake_of_test()
+    {
+        $batchID = 'FBEA060C6FABCAD76FB3E10286E49543A845B008';
+        $this->assertEquals('FBEA060C6FABCAD76FB3E10286E49543A845B008',$batchID);
+
+    }
+
+
+    /** @not_test but we should start migration the old CI tasks tests here*/
     function not_test_native_pipeline()
     {
         $batchID = 'FBEA060C6FABCAD76FB3E10286E49543A845B008';
