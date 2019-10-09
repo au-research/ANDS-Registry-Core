e8eb1f497359cc9672f38fcaaacb6d3ff7b64d38
Leo Monus <leo.monus@anu.edu.au>
2019-10-09 15:35:17 +1100

    CC-2653 added the use of configurable (.env) sissvoc url (typo)

--- config/vocab.php
+++ config/vocab.php
@@ -1,13 +1,13 @@
 <?php
 
-$sisscovUrl = env('SISSVOC_URL', "https://vocabs.ands.org.au/repository/api/lda/");
+$sissvocUrl = env('SISSVOC_URL', "https://vocabs.ands.org.au/repository/api/lda/");
 
 return [
-    'sissvoc_url' => $sisscovUrl,
+    'sissvoc_url' => $sissvocUrl,
     'vocab_resolving_services' =>[
-        'anzsrc-seo' => ['resolvingService' => $sisscovUrl.'anzsrc-seo/', 'uriprefix' => 'http://purl.org/au-research/vocabulary/anzsrc-seo/2008/'],
-        'anzsrc-for' => ['resolvingService' => $sisscovUrl.'anzsrc-for/', 'uriprefix' => 'http://purl.org/au-research/vocabulary/anzsrc-for/2008/'],
-        'GCMD' => ['resolvingService' => $sisscovUrl.'gcmd-sci/', 'uriprefix' => 'http://gcmdservices.gsfc.nasa.gov/kms/concept/']
+        'anzsrc-seo' => ['resolvingService' => $sissvocUrl.'anzsrc-seo/', 'uriprefix' => 'http://purl.org/au-research/vocabulary/anzsrc-seo/2008/'],
+        'anzsrc-for' => ['resolvingService' => $sissvocUrl.'anzsrc-for/', 'uriprefix' => 'http://purl.org/au-research/vocabulary/anzsrc-for/2008/'],
+        'GCMD' => ['resolvingService' => $sissvocUrl.'gcmd-sci/', 'uriprefix' => 'http://gcmdservices.gsfc.nasa.gov/kms/concept/']
     ]
 ];
 
