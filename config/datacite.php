47ca76c402cecc8d5d679bd82868c0ca29ceae64
Liz Woods <lwoods@andss-mbp.uds.anu.edu.au>
2019-12-17 12:56:32 +1100

    updated datacite status function to login into mds and do a get metadata request of a know doi CC-CC-2687

--- config/datacite.php
+++ config/datacite.php
@@ -9,6 +9,7 @@ return [
     'testPassword' =>  env('DATACITE_TEST_PASSWORD', null),
     'contact-name' => env('DATACITE_CONTACT_NAME', 'ANDS Service'),
     'contact-email' => env('DATACITE_CONTACT_EMAIL','services@ands.org.au'),
+    'known_doi' =>env('DATACITE_KNOWN_DOI',null),
     'fabrica' => [
         'api_url' => env('DATACITE_FABRICA_API_URL', 'https://api.datacite.org'),
         'url' => env('DATACITE_FABRICA_URL', 'https://doi.test.datacite.org'),
