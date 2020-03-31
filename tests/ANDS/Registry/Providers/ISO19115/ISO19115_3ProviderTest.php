37c3c1da34e0f3ad8e794efb96c7d85c4c5b92a9
Leo Monus <leo.monus@anu.edu.au>
2020-03-31 16:44:57 +1100

    Renamed transformer getter function CC-2714

--- tests/ANDS/Registry/Providers/ISO19115/ISO19115_3ProviderTest.php
+++ tests/ANDS/Registry/Providers/ISO19115/ISO19115_3ProviderTest.php
@@ -3,23 +3,29 @@
 namespace ANDS\Registry\Providers\ORCID;
 use \ANDS\Registry\Providers\ISO19115\ISO19115_3Provider;
 use ANDS\Util\XMLUtil;
+use ANDS\RegistryObject;
+use ANDS\File\Storage;
+use ANDS\RecordData;
+use Exception;
 
 class ISO19115_3ProviderTest extends \RegistryTestClass
 {
     /** @test */
     public function test_iso_transform()
     {
-        //$record = $this->ensureKeyExist("c126e9f1-b072-9fee-6e04-d131383d9bea");
-        $record = $this->ensureKeyExist("6ac30542-4805-f2bf-e5e4-45b73b15221d");
-        $provider = new ISO19115_3Provider();
-      //  $result = $provider->process($record);
-      //  $this->assertTrue($result);
-        $success =  $provider->process($record);
-        $this->assertTrue($success);
-        $iso = $provider->get($record);
-        $rootNode = XMLUtil::getElementsByName($iso,'MD_Metadata',"http://standards.iso.org/iso/19115/-3/mdb/1.0");
-        $this->assertEquals(sizeof($rootNode), 1);
-        //$provider->validateContent($iso);
+        try{
+            $record = $this->stub(RegistryObject::class, ['class' => 'service', 'type' => "OGC:WMS"]);
+            $this->stub(RecordData::class, [
+                'registry_object_id' => $record->id,
+                'data' => Storage::disk('test')->get('servicesDiscovery/rifcs_service.xml')
+            ]);
+            $provider = new ISO19115_3Provider();
+            $result =  $provider->process($record);
+            $this->assertContains("<gco:CharacterString>6ac30542-4805-f2bf-e5e4-45b73b15221d</gco:CharacterString>", $result);
+        }
+        catch(Exception $e){
+            $this->fail("Test [test_iso_transform] failed:" .$e->getMessage());
+        }
     }
 
     /** @test */
