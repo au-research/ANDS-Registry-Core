8cb63d20495b263c27cf2a5e42ef9788ef719310
Liz Woods <lwoods@ANDSs-MacBook-Pro.local>
2020-04-07 08:47:32 +1000

    RDA styling updates to header and footer and link colours RDA-65

--- applications/portal/templates/ands-green/includes/footer.blade.php
+++ applications/portal/templates/ands-green/includes/footer.blade.php
@@ -1,27 +1,18 @@
 <footer id="footer" role="contentinfo">
     <section class="section swatch-black">
         <div class="container">
-            <div class="row element-normal-top element-normal-bottom footer-banner">
-                <div class="col-md-8">
-                    <p>
-                        Research Data Australia is the data discovery service of the Australian Research Data Commons (ARDC).
-                        The ARDC is supported by the Australian Government through the National Collaborative Research Infrastructure Strategy Program.
-                        <a href="https://ardc.edu.au/about/" target="_blank" style="color:#84A">Read more about the ARDC...</a>
-                    </p>
-                </div>
-                <div class="col-md-2 edu-logo-footer">
-                   <a href="https://education.gov.au/national-collaborative-research-infrastructure-strategy-ncris" target="_blank" class="gov_logo">
-                       <img src="<?php echo asset_url('images/NCRIS_PROVIDER_rev.png','core');?>" alt="National Collaborative Research Infrastructure Strategy (NCRIS)" />
-                   </a>
-                </div>
-                <div class="col-md-2 ardc-logo-footer">
-                    <a href="https://www.ardc.edu.au/" class="footer_logo"><img src="{{ asset_url(\ANDS\Util\config::get('app.environment_logo'), 'base')}}" alt=""/></a>
-                </div>
-            </div>
+
             <div class="row element-normal-top element-normal-bottom">
-                <div class="col-md-3">
+
+                    <div class="col-md-2 ardc-logo-footer">
+                        <a href="https://www.ardc.edu.au/" class="footer_logo">
+                          <!--   <img src="{{ asset_url(\ANDS\Util\config::get('app.environment_logo'), 'base')}}" alt=""/></a> -->
+                        <img src="https://ardc.edu.au/wp-content/themes/ardc/img/ardc_logo.svg" alt="" /></a>
+                    </div>
+
+                <div class="col-md-2">
                     <div id="categories-3" class="sidebar-widget  widget_categories">
-                        <h3 class="sidebar-header">Quick Links</h3>
+                        <h5 class="sidebar-header">Quick Links</h5>
                         <ul>
                             <li class="cat-item"> <a href="{{portal_url()}}" title="">Home</a> </li>
                             <li class="cat-item"> <a href="{{portal_url('page/about')}}" title="">About</a> </li>
@@ -32,9 +23,9 @@
                         </ul>
                     </div>
                 </div>
-                <div class="col-md-3">
+                <div class="col-md-2">
                     <div id="categories-3" class="sidebar-widget  widget_categories">
-                        <h3 class="sidebar-header">Explore</h3>
+                        <h5 class="sidebar-header">Explore</h5>
                         <ul>
                             <li class="cat-item"> <a href="{{portal_url('themes')}}" title="">Themed Collections</a> </li>
                             <li class="cat-item"> <a href="{{portal_url('theme/open-data')}}#!/access_rights=open" title="">Open Data</a> </li>
@@ -43,7 +34,19 @@
                         </ul>
                     </div>
                 </div>
-                <div class="col-md-3">
+                <div class="col-md-2">
+                    <div id="categories-4" class="sidebar-widget  widget_categories">
+                        <h5 class="sidebar-header">External Resources</h5>
+                        <ul>
+                            <li class="cat-item"> <a href="https://ardc.edu.au" title="" target="_blank">ARDC Website</a> </li>
+                            <li class="cat-item"> <a href="{{base_url('registry/')}}" title="">ARDC Online Services</a> </li>
+                            @if(isset($ro) && $ro->core['id'])
+                                <li class="cat-item"> <a href="{{base_url('registry/registry_object/view/')}}/<?=$this->ro->id?>" title="">Registry View</a> </li>
+                            @endif
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-2">
                     <?php
 
                         $params = [
@@ -58,25 +61,22 @@
                         $params = http_build_query($params);
                     ?>
                     <div id="categories-5" class="sidebar-widget widget_categories">
-                        <h3 class="sidebar-header">Share</h3>
+                        <h5 class="sidebar-header">Share</h5>
                         <ul>
                             <li class="cat-item"><a class="noexicon social-sharing" href="{{ portal_url('page/share/facebook?'.$params) }}" target="_blank"><i class="fa fa-facebook"></i> Facebook</a></li>
                             <li class="cat-item"><a class="noexicon social-sharing" href="{{ portal_url('page/share/twitter/?'.$params) }}" target="_blank"><i class="fa fa-twitter"></i> Twitter</a></li>
                         </ul>
                     </div>
                 </div>
-                <div class="col-md-3">
-                    <div id="categories-4" class="sidebar-widget  widget_categories">
-                        <h3 class="sidebar-header">External Resources</h3>
-                        <ul>
-                            <li class="cat-item"> <a href="https://ardc.edu.au" title="" target="_blank">ARDC Website</a> </li>
-                            <li class="cat-item"> <a href="http://developers.ands.org.au" title="" target="_blank">Developers</a> </li>
-                            <li class="cat-item"> <a href="{{base_url('registry/')}}" title="">ARDC Online Services</a> </li>
-                            @if(isset($ro) && $ro->core['id'])
-                                <li class="cat-item"> <a href="{{base_url('registry/registry_object/view/')}}/<?=$this->ro->id?>" title="">Registry View</a> </li>
-                            @endif
-                        </ul>
-                    </div>
+
+
+                <div class="col-md-2">
+                    <p class="footer-text">
+                        The Australian Research Data Commons (ARDC) is enabled by NCRIS.
+                    </p>
+                    <a href="https://education.gov.au/national-collaborative-research-infrastructure-strategy-ncris" target="_blank" class="gov_logo">
+                    <img src="<?php echo asset_url('images/NCRIS_PROVIDER_rev.png','core');?>" alt="National Collaborative Research Infrastructure Strategy (NCRIS)" height="100px"/>
+                    </a>
                 </div>
             </div>
         </div>
