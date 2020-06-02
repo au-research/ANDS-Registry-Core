7d28c10da5947d16550819c42a00b753ed1e0bcf
Liz Woods <lwoods@ANDSs-MacBook-Pro.local>
2020-06-02 15:51:38 +1000

    changed collection_banner background position RDA-65

--- applications/portal/templates/ands-green/includes/banner-image.blade.php
+++ applications/portal/templates/ands-green/includes/banner-image.blade.php
@@ -1,6 +1,6 @@
 <?php 
 	$banner = isset($banner) ? $banner : asset_url('images/collection_banner.jpg', 'core');
 ?>
-<div id="banner-image" class="background-media" style="background-image: url({{$banner}}); background-size: cover; background-position: 50% 53%; background-repeat: no-repeat;  box-shadow: none;">
+<div id="banner-image" class="background-media" style="background-image: url({{$banner}}); background-size: cover; background-position: 50% 65%; background-repeat: no-repeat;  box-shadow: none;">
 </div>
 <div class="background-overlay grid-overlay-30" style="background-color: rgba(0,0,0,0.1); box-shadow: none;"></div>
