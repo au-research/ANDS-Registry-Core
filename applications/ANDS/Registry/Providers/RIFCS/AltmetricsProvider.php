71d5e594976bd695c05dc3ec2c37a776b526f438
Liz Woods <lwoods@ANDSs-MacBook-Pro.local>
2020-12-03 10:22:14 +1100

    RDA-129 RDA-130 RDA-131 RDA-132 RDA-133 Bug fixes for altmetrics meta tags

--- applications/ANDS/Registry/Providers/RIFCS/AltmetricsProvider.php
+++ applications/ANDS/Registry/Providers/RIFCS/AltmetricsProvider.php
@@ -6,6 +6,8 @@ use ANDS\Registry\Providers\MetadataProvider;
 use ANDS\Registry\Providers\RIFCSProvider;
 use ANDS\Registry\Providers\RelationshipProvider;
 use ANDS\RegistryObject;
+use ANDS\Util\XMLUtil;
+
 
 /**
  * Class AltmetricsProvider
@@ -47,16 +49,17 @@ class AltmetricsProvider implements RIFCSProvider
 
 
         /* dc.identifier */
-        if(self::getIdentifier($record )!=null) {
+        $identifiers = self::getIdentifier($record);
+        foreach($identifiers as $identifier) {
             $altmetrics[] = [
                 'type' => "dc.identifier",
-                'value' => (string)self::getIdentifier($record)
+                'value' => (string)$identifier
             ];
         }
 
         /* citation_publisher */
         $altmetrics[] = [
-            'type' => "content_provider",
+            'type' => "citation_provider",
             'value' => (string) self::getPublisher($record )
         ];
 
@@ -70,7 +73,7 @@ class AltmetricsProvider implements RIFCSProvider
         }
 
         /* citation_online_date */
-        if($publicationDate = DatesProvider::getPublicationDate($record)) {
+        if($publicationDate = self::getPublicationDate($record)) {
             $altmetrics[] = [
                 'type' => "citation_online_date",
                 'value' => (string)$publicationDate
@@ -136,27 +139,36 @@ class AltmetricsProvider implements RIFCSProvider
      * @return string
      * @throws \Exception
      */
-    public static function getIdentifier(RegistryObject $record, $simpleXML = null)
+    public function getIdentifier(RegistryObject $record, $simpleXML = null)
     {
         /**
-         * registryObject:collection:citationInfo:citationMetadata:title
-        OR RDA display title
+         * registryObject:collection:citationInfo:citationMetadata:identifier
+         * OR registryObject:collection:identifier
+         * OR
          */
+        $identifiers = [];
+
         $simpleXML = $simpleXML ? $simpleXML : MetadataProvider::getSimpleXML($record);
 
         //registryObject:collection:citationInfo:citationMetadata:identifier
-        $identifier = $simpleXML->xpath('//ro:citationInfo/ro:citationMetadata/ro:identifier');
-        if (count($identifier) > 0 && $elem = array_pop($identifier)) {
-            return (string) $elem;
+        $ids = $simpleXML->xpath('//ro:citationInfo/ro:citationMetadata/ro:identifier');
+        foreach($ids as $identifier){
+            $identifiers[] = (string) $identifier;
+        }
+        if (count($identifiers) > 0) {
+            return $identifiers;
         }
 
         //registryObject:collection:identifier
-        $identifier = $simpleXML->xpath('//ro:identifier');
-        if (count($identifier) > 0 && $elem = array_pop($identifier)) {
-            return (string) $elem;
+        $ids = $simpleXML->xpath('//ro:collection/ro:identifier');
+        foreach($ids as $identifier){
+            $identifiers[] = (string) $identifier;
         }
-
-        return $record->key;
+        if (count($identifiers) > 0 ) {
+            return $identifiers;
+        }
+        $identifiers[] = $record->key;
+        return $identifiers;
     }
 
 
@@ -200,9 +212,13 @@ class AltmetricsProvider implements RIFCSProvider
             return $creators;
         }
 
-        $validRelationTypes = ['IsPrincipalInvestigatorOf', 'author', 'coInvestigator', 'isOwnedBy', 'hasCollector'];
-        foreach ($validRelationTypes as $relationType) {
-            $creators = array_merge($creators, RelationshipProvider::getRelationByType($record, [$relationType]));
+        $validRelationTypes[] = ['IsPrincipalInvestigatorOf', 'hasPrincipalInvestigator'];
+        $validRelationTypes[] = ['author'];
+        $validRelationTypes[] = ['coInvestigator'];
+        $validRelationTypes[] = ['isOwnedBy','IsOwnerOf'];
+        $validRelationTypes[] = ['hasCollector','IsCollectorOf'];
+        foreach ($validRelationTypes as $relationTypes) {
+            $creators = array_merge($creators, RelationshipProvider::getRelationByType($record, $relationTypes));
             if (count($creators)) {
                 return $creators;
             }
@@ -215,6 +231,105 @@ class AltmetricsProvider implements RIFCSProvider
         }
 
         return $creators;
+    }
+
+
+
+    public static function getPublicationDate(
+        RegistryObject $record,
+        $data = null,
+        $format = 'Y-m-d'
+    ) {
+        if (!$data) {
+            $data = MetadataProvider::getSelective($record, ['recordData']);
+        }
+
+        $publicationDate = null;
+
+        $citationMedataDates = XMLUtil::getElementsByXPath(
+            $data['recordData'],
+            'ro:registryObject/ro:' . $record->class . '/ro:citationInfo/ro:citationMetadata/ro:date'
+        );
+
+        // registryObject/collection/citationInfo/citationMetadata/date[@type=’publication date’]
+        foreach ($citationMedataDates AS $date) {
+            if ((string) $date['type'] == 'publicationDate') {
+                $publicationDate = DatesProvider::formatDate((string) $date, $format);
+            }
+        }
+        if ($publicationDate) return $publicationDate;
+
+        // registryObject/collection/citationInfo/citationMetadata/date[@type=’issued date’]
+        foreach ($citationMedataDates AS $date) {
+            if ((string) $date['type'] == 'issued') {
+                $publicationDate = DatesProvider::formatDate((string) $date, $format);
+            }
+        }
+        if ($publicationDate) return $publicationDate;
+
+        // registryObject/collection/citationInfo/citationMetadata/date[@type=’created’]
+        foreach ($citationMedataDates AS $date) {
+            if ((string) $date['type'] == 'created') {
+                $publicationDate = DatesProvider::formatDate((string) $date, $format);
+            }
+        }
+        if ($publicationDate) return $publicationDate;
+
+        $roDates = XMLUtil::getElementsByXPath(
+            $data['recordData'],
+            'ro:registryObject/ro:' . $record->class . '/ro:dates'
+        );
+
+        // registryObject/collection/dates[@type=’issued’]
+        foreach ($roDates AS $date) {
+            if ((string) $date['type'] == 'dc.issued') {
+                $publicationDate = DatesProvider::formatDate((string) $date->date, $format);
+            }
+        }
+        if ($publicationDate) return $publicationDate;
+
+        // registryObject/collection/dates[@type=’available’]
+        foreach ($roDates AS $date) {
+            if ((string) $date['type'] == 'dc.available') {
+                $publicationDate = DatesProvider::formatDate((string) $date->date, $format);
+            }
+        }
+        if ($publicationDate) return $publicationDate;
+
+        // registryObject/collection/dates[@type=’created’]
+        foreach ($roDates AS $date) {
+            if ((string) $date['type'] == 'dc.created') {
+                $publicationDate = DatesProvider::formatDate((string) $date->date, $format);
+            }
+        }
+        if ($publicationDate) return $publicationDate;
+
+        /**
+         * registryObject/collection@dateModified
+         */
+        foreach (XMLUtil::getElementsByXPath($data['recordData'],
+            'ro:registryObject/ro:' . $record->class) AS $object) {
+
+            if ($dateModified = (string) $object['dateModified']) {
+                $publicationDate = DatesProvider::formatDate($dateModified, $format);
+            }
+        }
+        if ($publicationDate) return $publicationDate;
+
+
+        /**
+         * registryObject/Collection@dateAccessioned
+         */
+        foreach (XMLUtil::getElementsByXPath($data['recordData'],
+            'ro:registryObject/ro:' . $record->class) AS $object) {
+
+            if ($dateAccessioned = (string) $object['dateAccessioned']) {
+                $publicationDate = DatesProvider::formatDate($dateAccessioned, $format);
+            }
+        }
+        if ($publicationDate) return $publicationDate;
+
+        return DatesProvider::getCreatedDate($record, $format);
     }
 
     //function to concatenate name values based on the name part type
