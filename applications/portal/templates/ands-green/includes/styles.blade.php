d231917ee9872b59ec935f92a7e5a244572b596e
Liz Woods <lwoods@andss-mbp.uds.anu.edu.au>
2021-02-22 08:34:37 +1100

    Updated styles for surveyjs

--- applications/portal/templates/ands-green/includes/styles.blade.php
+++ applications/portal/templates/ands-green/includes/styles.blade.php
@@ -1,7 +1,3 @@
 <link rel="stylesheet" type="text/css" href="{{ dist_url('portal.combine.css') }}" media="screen">
 <link rel="stylesheet" type="text/css" href="{{ asset_url('css/print.css', 'core') }}" media="print">
 <link href="https://unpkg.com/survey-knockout@1.8.31/modern.css" type="text/css" rel="stylesheet"/>
-
-<style>
-
-</style>
