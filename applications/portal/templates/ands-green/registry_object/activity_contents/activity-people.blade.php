d5776ee08a5e3b29f58a328d918963bb747e1d49
Liz Woods <lwoods@MacBook-Pro-2.local>
2021-11-18 14:48:09 +1100

    RDA-235 catered for bug when the related info title is not available

--- applications/portal/templates/ands-green/registry_object/activity_contents/activity-people.blade.php
+++ applications/portal/templates/ands-green/registry_object/activity_contents/activity-people.blade.php
@@ -7,26 +7,41 @@
             $col['relations'][0]['relation_type_text'] = str_replace(", Participant","", $col['relations'][0]['relation_type_text']);
             $researchers_array[$col['relations'][0]['relation_type_text']][] = $col;
         }
+       // var_dump($researchers_array );
         $displayNum = 0;
     ?>
 <p>
     <strong>Researchers: </strong>
     @foreach($researchers_array as $relation=>$researchers)
+
         @foreach($researchers as $key=>$col)
+
             <?php $displayNum++; ?>
-            <a href="{{$col["to_url"]}}"
-               title="{{ $col['to_title'] }}"
-               class="ro_preview"
-               tip="{{ $col['relations'][0]['relation_type_text'] }}"
-               ro_id="{{ $col['to_identifier'] }}">
-                {{$col['to_title']}}
-            </a>
+            @if($col["to_identifier_type"]=="ro:id")
+                <a href="{{$col['to_url']}}"
+                title="{{ $col['to_title'] }}"
+                class="ro_preview"
+                ro_id="{{$col['to_identifier']}}">
+                    {{$col['to_title']}} </a>
+            @elseif($col["to_identifier_type"]!="ro:id")
+                <?php  $col_json = urlencode(json_encode($col));
+                if(!isset($col['to_title'])) $col['to_title'] = $col['to_identifier'];
+                ?>
+                <a href="false"
+                title="{{ $col['to_title'] }}"
+                class="ro_preview"
+                identifier_relation_id="{{$col_json}}">
+               {{$col['to_title']}}
+                </a>
+            @endif
+
             @if($relation!="Participant")
                 ({{ $relation }})
             @endif
             @if($displayNum<$related['researchers']['total'])
                 ,&nbsp;
             @endif
+                    <?php echo " we are here"; ?>
         @endforeach
 
     @endforeach
--- applications/portal/templates/ands-green/registry_object/contents/related-researchers.blade.php
+++ applications/portal/templates/ands-green/registry_object/contents/related-researchers.blade.php
@@ -5,7 +5,7 @@
             @foreach($related['researchers']['contents'] as $col)
                    <li>
                     <i class="fa fa-user icon-portal"></i>
-                    <?php $col_json = urlencode(json_encode($col));?>
+
                     <small>{{ $col['relations'][0]['relation_type_text'] }}</small>
                @if($col["to_identifier_type"]=="ro:id")
                     <a href="{{$col['to_url']}}"
@@ -13,12 +13,15 @@
                        class="ro_preview"
                         ro_id="{{$col['to_identifier']}}">
                         {{$col['to_title']}}</a>
-                @elseif($col["to_identifier_type"]!="uri")
-                           <a href="{{$col['to_identifier']}}"
+                @elseif($col["to_identifier_type"]!="ro:id")
+                           <?php $col_json = urlencode(json_encode($col));
+                           if(!isset($col['to_title'])) $col['to_title'] = $col['to_identifier'];?>
+                           <a href="false"
                               title="{{ $col['to_title'] }}"
                               class="ro_preview"
                               identifier_relation_id="{{$col_json}}">
-                               {{$col['to_title']}}</a>
+                               {{$col['to_title']}}
+                              </a>
                  @endif
 
                 </li>
