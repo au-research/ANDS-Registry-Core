af4c85346a7f61bf6773f338ba76c19ca4ae5e54
Liz Woods <lwoods@MacBook-Pro-2.local>
2022-01-18 12:22:28 +1100

    RDA-587 fixed bug so that if a duplicate party from the same group exists, then display that record

--- applications/portal/templates/ands-green/registry_object/contents/related-researchers.blade.php
+++ applications/portal/templates/ands-green/registry_object/contents/related-researchers.blade.php
@@ -3,8 +3,9 @@
 
     <ul class="list-unstyled">
         @if(is_array($related['researchers']) && $related['researchers']['total'] >0)
-            <? $relation_to_title = [];
-                $dupes = 0;?>
+            <?  $relation_to_title = [];
+                $relation_dupe_title = []
+;               $dupes = 0;?>
             @foreach($related['researchers']['contents'] as $col)
                 <?php
                 $result = array();
@@ -16,30 +17,91 @@
                 $relation_types = array_unique($relation_types);
                 $relation_type_text =  implode($relation_types,", ");
                 $relation_to_title[$col['to_title'].$relation_type_text][] = $col['to_title'];
-                $dupes = count($relation_to_title[$col['to_title'].$relation_type_text]);
+                $relation_dupe_title[$col['to_title']][] = $col;
+                $relation_dupe_title[$col['to_title'].'same_group_found'] = 'false';
+                $dupe_type_text[$col['to_title'].'dupe_type_text'] = $relation_type_text;
                  ?>
-                @if($dupes<2)
-                <li>
-                    <i class="fa fa-user icon-portal"></i>
-                    <small>{{ $relation_type_text }}</small>
-                   <?php
-                    if (!isset($col['to_url'])){
-                        $col['to_url']="";
-                    } ?>
-                    <a href="{{ $col['to_url'] }}"
-                       title="{{ $col['to_title'] }}"
-                       class="ro_preview"
-                       tip="{{ $col['to_title']}}"
-                       @if($col['to_identifier_type'] == "ro:id")
-                           ro_id="{{ $col['to_identifier'] }}"
-                       @else
+            @endforeach
 
-                            <?php $col_json = urlencode(json_encode($col)); ?>
-                            identifier_relation_id = "{{ $col_json }}"
-                        @endif>
-                         {{$col['to_title']}}</a>
-                </li>
-                @endif
+            @foreach($related['researchers']['contents'] as $col)
+
+             <?php $dupes = count($relation_dupe_title[$col['to_title']]);
+            //if we have duplicates - check for a record from the same group
+            ?>
+             @if($dupes>1 && $relation_dupe_title[$col['to_title'].'same_group_found'] == 'false')
+                    @foreach($relation_dupe_title[$col['to_title']] as $col2)
+                        @if($col['from_group'] == $col2['to_group'] && $col['to_title'] == $col2['to_title'])
+                         <?php //if record from the same group is found set found to true display the record
+                             $relation_dupe_title[$col['to_title'].'same_group_found'] = 'true';?>
+                             <li>
+                                 <i class="fa fa-user icon-portal"></i>
+                                 <small>{{ $dupe_type_text[$col['to_title'].'dupe_type_text'] }}</small>
+                                 <?php
+                                 if (!isset($col2['to_url'])){
+                                     $col['to_url']="";
+                                 } ?>
+                                 <a href="{{ $col2['to_url'] }}"
+                                    title="{{ $col2['to_title'] }}"
+                                    class="ro_preview"
+                                    tip="{{ $col2['to_title']}}"
+                                    @if($col2['to_identifier_type'] == "ro:id")
+                                        ro_id="{{ $col2['to_identifier'] }}"
+                                    @else
+                                    <?php $col_json = urlencode(json_encode($col2)); ?>
+                                    identifier_relation_id = "{{ $col_json }}"
+                                         @endif>
+                                     {{$col2['to_title']}}</a>
+                             </li>
+                         @endif
+                    @endforeach
+                    @if(  $relation_dupe_title[$col['to_title'].'same_group_found'] == 'false')
+                         <?php
+                            //if no record from the same group is found display 1st record
+                            $col2 = $relation_dupe_title[$col['to_title']][0] ?>
+                         <li>
+                             <i class="fa fa-user icon-portal"></i>
+                             <small>{{$dupe_type_text[$col['to_title'].'dupe_type_text'] }}</small>
+                             <?php
+                             if (!isset($col2['to_url'])){
+                                 $col['to_url']="";
+                             } ?>
+                             <a href="{{ $col2['to_url'] }}"
+                                title="{{ $col2['to_title'] }}"
+                                class="ro_preview"
+                                tip="{{ $col2['to_title']}}"
+                                @if($col2['to_identifier_type'] == "ro:id")
+                                ro_id="{{ $col2['to_identifier'] }}"
+                                @else
+
+                                <?php $col_json = urlencode(json_encode($col2)); ?>
+                                identifier_relation_id = "{{ $col_json }}"
+                                     @endif>
+                                 {{$col2['to_title']}}</a>
+                         </li>
+                    @endif
+
+            @elseif($dupes<2)
+            <li>
+                <i class="fa fa-user icon-portal"></i>
+                <small>{{ $relation_type_text }}</small>
+               <?php
+                if (!isset($col['to_url'])){
+                    $col['to_url']="";
+                } ?>
+                <a href="{{ $col['to_url'] }}"
+                   title="{{ $col['to_title'] }}"
+                   class="ro_preview"
+                   tip="{{ $col['to_title']}}"
+                   @if($col['to_identifier_type'] == "ro:id")
+                       ro_id="{{ $col['to_identifier'] }}"
+                   @else
+
+                        <?php $col_json = urlencode(json_encode($col)); ?>
+                        identifier_relation_id = "{{ $col_json }}"
+                    @endif>
+                     {{$col['to_title']}}</a>
+            </li>
+            @endif
             @endforeach
             @if($related['researchers']['total'] > 5)
                 <li><a href="{{ $related['researchers']['searchUrl'] }}">View all {{ $related['researchers']['total'] }} related people</a></li>
