391a04edf25272ba7f9f54191d33f681b40d0f6b
Liz Woods <lwoods@MacBook-Pro-2.local>
2022-02-03 12:26:06 +1100

    Fixed bug in RelationshipSearchService

--- applications/ANDS/Mycelium/RelationshipSearchService.php
+++ applications/ANDS/Mycelium/RelationshipSearchService.php
@@ -100,6 +100,7 @@ class RelationshipSearchService
                 case "to_identifier":
                 case "to_identifier_type":
                 case "to_title":
+                    $fqs[] = "+$key:$value";
                     break;
                 case "to_type":
                     // supports comma separated value, and PHP array (when using PHP API)
