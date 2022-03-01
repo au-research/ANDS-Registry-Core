059c78c0f2caf5b1deae4371f146d4560bfbd066
Leo Monus <leo.monus@ardc.edu.au>
2022-03-01 20:19:54 +1100

    DatasetORCIDSuggestor

---
+++ tests/ANDS/Registry/Suggestors/DatasetORCIDSuggestorTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace ANDS\Registry\Suggestors;
+
+use ANDS\Registry\Providers\ORCID\ORCIDRecord;
+use ANDS\Registry\Suggestors\DatasetORCIDSuggestor;
+use PHPUnit\Framework\TestCase;
+use ANDS\Registry\Providers\ORCID\ORCIDRecordsRepository;
+
+class DatasetORCIDSuggestorTest extends \RegistryTestClass
+{
+
+    public function testSuggest()
+    {
+        $data = [];
+        $data['orcid'] = "https://orcid.org/0000-0002-5822-5275";
+        $data['name'] = "Sebastien Mancini";
+        $data['access_token'] = "zzz";
+        $data['refresh_token'] = "aaa";
+
+        $orcidRecord = ORCIDRecordsRepository::firstOrCreate("https://orcid.org/0000-0002-5822-5275", $data );
+        $suggestor = new DatasetORCIDSuggestor();
+        $record['person']['name']['family-name']['value'] = 'AUTParty6';
+        $suggested = $suggestor->suggest($orcidRecord);
+        var_dump($suggested);
+    }
+}
