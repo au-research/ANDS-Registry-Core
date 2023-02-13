c8674add79b6be4dda63ac2c543ab853c9e1ac22
Leo Monus <leo.monus@ardc.edu.au>
2023-02-13 22:02:58 +1100

    restructure and relationships

--- applications/ANDS/Registry/Providers/HealthData/HealthDataProvider.php
+++ applications/ANDS/Registry/Providers/HealthData/HealthDataProvider.php
@@ -2,7 +2,9 @@
 
 namespace ANDS\Registry\Providers\HealthData;
 
+use ANDS\Mycelium\RelationshipSearchService;
 use ANDS\Registry\ContentProvider\ANZCTR\ContentProvider;
+use ANDS\Registry\Providers\RelationshipProvider;
 use ANDS\Registry\Providers\RIFCS\DescriptionProvider;
 use ANDS\Registry\Providers\RIFCS\IdentifierProvider;
 use ANDS\Registry\Providers\RIFCS\JsonLDProvider;
@@ -58,7 +60,7 @@ class HealthDataProvider
 
         }
         $healthDataset["relatedDatasets"] = $relatedDatasets;
-
+        $healthDataset["relationships"] = self::getRelationships($record->id, $record->group);
 
         $anzctr_schema = Schema::get(static::$anzctr_schema_uri);
 
@@ -73,15 +75,15 @@ class HealthDataProvider
                 $identifier["value"] =  $dom->getElementsByTagName("actrn")->item(0)->nodeValue;
                 $relatedStudy["title"] = $dom->getElementsByTagName("publictitle")->item(0)->nodeValue;
                 $relatedStudy['identifiers'][] = $identifier;
+                $relatedStudy["publictitle"] = ContentProvider::getFirst($dom, array('publictitle'));
+                $relatedStudy["briefSummary"] = ContentProvider::getFirst($dom, array('briefsummary'));
+                $relatedStudy["conditions"] = ContentProvider::getContent($dom, array('healthcondition'));
+                $relatedStudy["conditionCodes"] = ContentProvider::getContent($dom, array('conditioncode'));
+                $relatedStudy["studyType"] = ContentProvider::getContent($dom, array('studytype'));
+                $relatedStudy["ethicsApproval"] = ContentProvider::getContent($dom, array('ethicsapproval'));
+                $relatedStudy["inclusiveCriteria"] = ContentProvider::getContent($dom, array('inclusivecriteria'));
+                $relatedStudy["interventionCode"] = ContentProvider::getContent($dom, array('interventioncode'));
                 $relatedStudies[] = $relatedStudy;
-                $healthDataset["anzctrPublictitle"] = ContentProvider::getFirst($dom, array('publictitle'));
-                $healthDataset["anzctrBriefsummary"] = ContentProvider::getFirst($dom, array('briefsummary'));
-                $healthDataset["anzctrConditions"] = ContentProvider::getContent($dom, array('healthcondition'));
-                $healthDataset["anzctrConditioncodes"] = ContentProvider::getContent($dom, array('conditioncode'));
-                $healthDataset["anzctrStudytype"] = ContentProvider::getContent($dom, array('studytype'));
-                $healthDataset["anzctrEthicsapproval"] = ContentProvider::getContent($dom, array('ethicsapproval'));
-                $healthDataset["anzctrInclusivecriteria"] = ContentProvider::getContent($dom, array('inclusivecriteria'));
-                $healthDataset["anzctrInterventioncode"] = ContentProvider::getContent($dom, array('interventioncode'));
             }
         }
 
@@ -99,5 +101,212 @@ class HealthDataProvider
         $publishers = $record->group;
         return $publishers;
     }
+
+    public static function getRelationships($recordId, $group)
+    {
+        return [
+            'data' => self::getRelatedData($recordId, $group),
+            'software' => self::getRelatedSoftware($recordId, $group),
+            'publications' => self::getRelatedPublication($recordId, $group),
+            'programs' => self::getRelatedPrograms($recordId, $group),
+            'grants_projects' => self::getRelatedGrantsProjects($recordId, $group),
+            'services' => self::getRelatedService($recordId, $group),
+            'websites' => self::getRelatedWebsites($recordId, $group),
+            'researchers' => self::getRelatedResearchers($recordId, $group),
+            'organisations' => self::getRelatedOrganisations($recordId)
+        ];
+    }
+
+    /**
+     * Obtain related data from SOLR
+     * @return array
+     */
+    private static function getRelatedData($recordId, $group) {
+
+        $result = RelationshipSearchService::search([
+            'from_id' => $recordId,
+            'to_class' => 'collection',
+            'not_to_type' => 'software',
+            'to_title' => '*'
+        ], ['boost_to_group' => $group ,'rows' => 5]);
+        return $result->toArray();
+    }
+
+    /**
+     * Obtain related software from SOLR
+     * @return array
+     */
+    private static function getRelatedSoftware($recordId, $group) {
+
+        $result = RelationshipSearchService::search([
+            'from_id' => $recordId,
+            'to_class' => 'collection',
+            'to_type' => 'software',
+            'to_title' => '*'
+        ], ['boost_to_group' => $group , 'rows' => 5]);
+        return $result->toArray();
+    }
+
+    /**
+     * Obtain related programs from SOLR
+     * @return array
+     */
+    private static function getRelatedPrograms($recordId, $group) {
+        $result = RelationshipSearchService::search([
+            'from_id' => $recordId,
+            'to_class' => 'activity',
+            'to_type' => 'program',
+            'to_title' => '*'
+        ], ['boost_to_group' => $group , 'rows' => 5]);
+
+        $programs = $result->toArray();
+
+        //obtaining to_funder for each of the program
+        foreach($programs['contents'] as $key=>$grant){
+            // if the grant is not a related Object
+            if ($grant['to_identifier_type'] === "ro:id") {
+                $result2 = RelationshipSearchService::search([
+                    'from_id' => $grant["to_identifier"],
+                    'to_class' => 'party',
+                    'relation_type' =>  ['isFunderOf', 'isFundedBy']
+                ], ['rows' => 1]);
+                $funded_by = $result2->toArray();
+                // the funder's title is the to_title
+                if (array_key_exists('contents', $funded_by) && count($funded_by['contents']) > 0) {
+                    $programs['contents'][$key]["to_funder"] = $funded_by['contents'][0]["to_title"];
+                }
+            }else{ // RDA-758 it should still have a funder but we need to search from their end
+                $result2 = RelationshipSearchService::search([
+                    'to_identifier' => $grant["to_identifier"],
+                    'from_class' => 'party',
+                    'relation_type' =>  ['isFunderOf', 'isFundedBy']
+                ], ['rows' => 1]);
+                $funded_by = $result2->toArray();
+                // the funder's title is the from_title
+                if (array_key_exists('contents', $funded_by) && count($funded_by['contents']) > 0) {
+                    $programs['contents'][$key]["to_funder"] = $funded_by['contents'][0]["from_title"];
+                }
+            }
+        }
+        return $programs ;
+    }
+
+    /**
+     * Obtain related activity that are grants or projects from SOLR
+     * @return array
+     */
+    private static function getRelatedGrantsProjects($recordId, $group) {
+
+        $result = RelationshipSearchService::search([
+            'from_id' => $recordId,
+            'to_class' => 'activity',
+            'to_title' => '*',
+            'not_to_type' => 'program'
+        ], ['boost_to_group' => $group, 'rows' => 5]);
+        $grants_projects = $result->toArray();
+
+        foreach($grants_projects['contents'] as $key=>$grant){
+            if($grant["to_identifier_type"] === "ro:id"){
+                $result2 = RelationshipSearchService::search([
+                    'from_id' => $grant["to_identifier"],
+                    'to_class' => 'party',
+                    'relation_type' =>  ['isFunderOf', 'isFundedBy']
+                ], ['rows' => 1]);
+                $funded_by = $result2->toArray();
+                // the funder's title is the to_title
+                if(isset($funded_by['contents']) && count($funded_by['contents'])>0){
+                    $grants_projects['contents'][$key]["to_funder"] = $funded_by['contents'][0]["to_title"];
+                }
+            }else{// RDA-758 it should still have a funder but we need to search from their end
+                $result2 = RelationshipSearchService::search([
+                    'to_identifier' => $grant["to_identifier"],
+                    'from_class' => 'party',
+                    'relation_type' =>  ['isFunderOf', 'isFundedBy']
+                ], ['rows' => 1]);
+                $funded_by = $result2->toArray();
+                // the funder's title is the from_title
+                if(isset($funded_by['contents']) && count($funded_by['contents'])>0){
+                    $grants_projects['contents'][$key]["to_funder"] = $funded_by['contents'][0]["from_title"];
+                }
+            }
+        }
+        return $grants_projects ;
+    }
+
+    /**
+     * Obtain related publications from SOLR
+     * @return array
+     */
+    private static function getRelatedPublication($recordId, $group) {
+
+        $result = RelationshipSearchService::search([
+            'from_id' => $recordId,
+            'to_class' => 'publication'
+        ], ['boost_to_group' => $group, 'rows' =>100]);
+        return $result->toArray();
+    }
+
+    /**
+     * Obtain related services from SOLR
+     * @return array
+     */
+    private static function getRelatedService($recordId, $group) {
+
+        $result = RelationshipSearchService::search([
+            'from_id' => $recordId,
+            'to_class' => 'service',
+            'to_title' => '*'
+        ], ['boost_to_group' => $group, 'rows' => 5]);
+        return $result->toArray();
+    }
+
+    /**
+     * Obtain related websites from SOLR
+     * @return array
+     */
+    private static function getRelatedWebsites($recordId, $group) {
+
+        $result = RelationshipSearchService::search([
+            'from_id' => $recordId,
+            'to_class' => 'website'
+        ], ['boost_to_group' => $group ,'rows' =>100]);
+        return $result->toArray();
+    }
+
+    /**
+     * Obtain related researchers from SOLR
+     * relationships where there's a hasPrincipalInvestigator edge is ranked higher via boosted query
+     * @return array
+     */
+    // RDA-627 make boost relation_type an array and boost decrease by the order in the array
+    private static function getRelatedResearchers($recordId, $group) {
+
+        $result = RelationshipSearchService::search([
+            'from_id' => $recordId,
+            'to_class' => 'party',
+            'not_to_type' => 'group',
+            'to_title' => '*',
+        ], ['boost_to_group' => $group ,'boost_relation_type' =>
+            ['Principal Investigator','hasPrincipalInvestigator','Chief Investigator'] ,
+            'rows' => 5, 'sort' => 'score desc, to_title asc']);
+        return $result->toArray();
+
+    }
+
+    /**
+     * Obtain related organisations from SOLR
+     * @return array
+     */
+    private static function getRelatedOrganisations($recordId) {
+
+        $result = RelationshipSearchService::search([
+            'from_id' => $recordId,
+            'to_class' => 'party',
+            'to_type' => 'group',
+            'to_title' => '*'
+        ], ['rows' => 5]);
+        return $result->toArray();
+    }
+
 
 }
